4a6c941808a8e50a69633dc094f4d763c78462ea
andimarek <andimarek@fastmail.fm>
2015-07-08 07:14:56 +0200

    schema: add inputObjectFields

---
+++ src/main/java/graphql/schema/GraphQLInputObjectField.java
@@ -0,0 +1,27 @@
+package graphql.schema;
+
+
+public class GraphQLInputObjectField {
+
+    private final String name;
+    private final GraphQLInputType type;
+    private final Object defaultValue;
+
+    public GraphQLInputObjectField(String name, GraphQLInputType type, Object defaultValue) {
+        this.name = name;
+        this.type = type;
+        this.defaultValue = defaultValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GraphQLInputType getType() {
+        return type;
+    }
+
+    public Object getDefaultValue() {
+        return defaultValue;
+    }
+}
--- src/main/java/graphql/schema/GraphQLInputObjectType.java
+++ src/main/java/graphql/schema/GraphQLInputObjectType.java
@@ -1,7 +1,25 @@
 package graphql.schema;
 
 
-public class GraphQLInputObjectType implements GraphQLType,GraphQLInputType{
+import java.util.ArrayList;
+import java.util.List;
 
-    String name;
+public class GraphQLInputObjectType implements GraphQLType, GraphQLInputType {
+
+    private final String name;
+
+    private final List<GraphQLInputObjectField> fields = new ArrayList<>();
+
+    public GraphQLInputObjectType(String name, List<GraphQLInputObjectField> fields) {
+        this.name = name;
+        this.fields.addAll(fields);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<GraphQLInputObjectField> getFields() {
+        return fields;
+    }
 }
