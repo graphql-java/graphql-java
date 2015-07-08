cd3142742e791b085edbd33a0a70564b7fe6541e
andimarek <andimarek@fastmail.fm>
2015-07-08 04:49:58 +0200

    add types to union

--- src/main/java/graphql/schema/GraphQLUnionType.java
+++ src/main/java/graphql/schema/GraphQLUnionType.java
@@ -1,5 +1,18 @@
 package graphql.schema;
 
 
-public class GraphQLUnionType implements GraphQLType,GraphQLOutputType{
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphQLUnionType implements GraphQLType, GraphQLOutputType {
+
+    private final List<GraphQLType> types = new ArrayList<>();
+
+    public GraphQLUnionType(List<GraphQLType> possibleTypes) {
+        this.types.addAll(possibleTypes);
+    }
+
+    public List<GraphQLType> getTypes() {
+        return types;
+    }
 }
