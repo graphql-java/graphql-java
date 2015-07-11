2bbfff52c9ae188e42aa5402b8a9d2f50953f827
andimarek <andimarek@fastmail.fm>
2015-07-11 14:18:32 +0200

    nested query test

--- src/test/groovy/graphql/StarWarsQueryTest.groovy
+++ src/test/groovy/graphql/StarWarsQueryTest.groovy
@@ -65,4 +65,89 @@ class StarWarsQueryTest extends Specification {
         result == expected
     }
 
+    def 'Allows us to query for the friends of friends of R2-D2'() {
+        given:
+
+        def query = """
+        query NestedQuery {
+            hero {
+                name
+                friends {
+                    name
+                    appearsIn
+                    friends {
+                        name
+                    }
+                }
+            }
+        }
+        """
+        def expected = [
+                hero: [name   : 'R2-D2',
+                       friends: [
+                               [
+                                       name     :
+                                               'Luke Skywalker',
+                                       appearsIn: ['NEWHOPE', 'EMPIRE', 'JEDI'],
+                                       friends  : [
+                                               [
+                                                       name: 'Han Solo',
+                                               ],
+                                               [
+                                                       name: 'Leia Organa',
+                                               ],
+                                               [
+                                                       name: 'C-3PO',
+                                               ],
+                                               [
+                                                       name: 'R2-D2',
+                                               ],
+                                       ]
+                               ],
+                               [
+                                       name     : 'Han Solo',
+                                       appearsIn: ['NEWHOPE', 'EMPIRE', 'JEDI'],
+                                       friends  : [
+                                               [
+                                                       name: 'Luke Skywalker',
+                                               ],
+                                               [
+                                                       name: 'Leia Organa',
+                                               ],
+                                               [
+                                                       name: 'R2-D2',
+                                               ],
+                                       ]
+                               ],
+                               [
+                                       name     : 'Leia Organa',
+                                       appearsIn: ['NEWHOPE', 'EMPIRE', 'JEDI'],
+                                       friends  : [
+                                               [
+                                                       name: 'Luke Skywalker',
+                                               ],
+                                               [
+                                                       name: 'Han Solo',
+                                               ],
+                                               [
+                                                       name: 'C-3PO',
+                                               ],
+                                               [
+                                                       name: 'R2-D2',
+                                               ],
+                                       ]
+                               ],
+                       ]
+                ]
+        ]
+
+        when:
+        def result = new GraphQL(StarWarsSchema.starWarsSchema, query).execute()
+
+        then:
+        result == expected
+
+
+    }
+
 }
