bfdbeecbd47aeca51a9357db0258162bbeda410a
andimarek <andimarek@fastmail.fm>
2015-07-15 20:53:19 +0200

    validation

---
+++ src/main/java/graphql/validation/rules/FieldsOnCorrectType.java
@@ -0,0 +1,27 @@
+package graphql.validation.rules;
+
+
+import graphql.language.Field;
+import graphql.schema.GraphQLCompositeType;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.validation.*;
+
+public class FieldsOnCorrectType extends AbstractRule {
+
+
+    public FieldsOnCorrectType(ValidationContext validationContext, ErrorCollector errorCollector) {
+        super(validationContext, errorCollector);
+    }
+
+
+    @Override
+    public void checkField(Field field) {
+        GraphQLCompositeType parentType = getValidationContext().getParentType();
+        if (parentType == null) return;
+        GraphQLFieldDefinition fieldDef = getValidationContext().getFieldDef();
+        if (fieldDef == null) {
+            addError(new ValidationError(ValidationErrorType.FieldUndefined));
+        }
+
+    }
+}
---
+++ src/test/groovy/graphql/validation/rules/FieldsOnCorrectTypeTest.groovy
@@ -0,0 +1,30 @@
+package graphql.validation.rules
+
+import graphql.language.Field
+import graphql.schema.GraphQLObjectType
+import graphql.validation.ErrorCollector
+import graphql.validation.ValidationContext
+import graphql.validation.ValidationErrorType
+import spock.lang.Specification
+
+class FieldsOnCorrectTypeTest extends Specification {
+
+    ErrorCollector errorCollector = new ErrorCollector()
+    ValidationContext validationContext = Mock(ValidationContext)
+    FieldsOnCorrectType fieldsOnCorrectType = new FieldsOnCorrectType(validationContext, errorCollector)
+
+
+    def "field undefined"() {
+        given:
+        def parentType = GraphQLObjectType.newObject().build()
+        validationContext.getParentType() >> parentType
+        validationContext.getFieldDef() >> null
+        def field = new Field("name")
+        when:
+        fieldsOnCorrectType.checkField(field)
+
+        then:
+        errorCollector.containsError(ValidationErrorType.FieldUndefined)
+
+    }
+}
