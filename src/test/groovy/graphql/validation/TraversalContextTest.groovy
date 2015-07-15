54e5de72b215a8bc97c34df9fcf3c8daf05a36ff
andimarek <andimarek@fastmail.fm>
2015-07-16 01:47:18 +0200

    traversal context tests

--- src/test/groovy/graphql/validation/TraversalContextTest.groovy
+++ src/test/groovy/graphql/validation/TraversalContextTest.groovy
@@ -1,13 +1,40 @@
 package graphql.validation
 
+import graphql.language.OperationDefinition
+import graphql.language.SelectionSet
 import spock.lang.Specification
+
+import static graphql.StarWarsSchema.queryType
+import static graphql.StarWarsSchema.starWarsSchema
+import static graphql.language.OperationDefinition.Operation.QUERY
 
 
 class TraversalContextTest extends Specification {
 
-    TraversalContext typeInfo = new TraversalContext()
+    TraversalContext traversalContext = new TraversalContext(starWarsSchema)
 
-    def ""(){
+    def "enter operation definition"(){
+        given:
+        SelectionSet selectionSet = new SelectionSet([])
+        OperationDefinition operationDefinition = new OperationDefinition(queryType.getName(),QUERY,selectionSet)
 
+        when:
+        traversalContext.enter(operationDefinition)
+
+        then:
+        traversalContext.getType() == queryType
+    }
+
+    def "leave operation definition"(){
+        given:
+        SelectionSet selectionSet = new SelectionSet([])
+        OperationDefinition operationDefinition = new OperationDefinition(queryType.getName(),QUERY,selectionSet)
+
+        when:
+        traversalContext.enter(operationDefinition)
+        traversalContext.leave(operationDefinition)
+
+        then:
+        traversalContext.getType() == null
     }
 }
