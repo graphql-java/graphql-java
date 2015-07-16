5b7b21dbdf82b7070c3979955657f0939597e366
andimarek <andimarek@fastmail.fm>
2015-07-16 08:49:34 +0200

    doc

--- src/main/java/graphql/schema/GraphQLTypeReference.java
+++ src/main/java/graphql/schema/GraphQLTypeReference.java
@@ -1,6 +1,10 @@
 package graphql.schema;
 
 
+/**
+ * A special type to allow a object/interface types to reference itself. It's replaced with the real type
+ * object when the schema is build.
+ */
 public class GraphQLTypeReference implements GraphQLType, GraphQLOutputType {
 
     private final String name;
