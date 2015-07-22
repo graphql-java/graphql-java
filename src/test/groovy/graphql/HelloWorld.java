c63ae029f0626205034f28d84581dd861d6eaca8
andimarek <andimarek@fastmail.fm>
2015-07-22 03:18:15 +0200

    helloWorld test

--- src/test/groovy/graphql/HelloWorld.java
+++ src/test/groovy/graphql/HelloWorld.java
@@ -3,15 +3,20 @@ package graphql;
 
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
+import org.junit.Test;
+
+import java.util.Map;
 
 import static graphql.Scalars.GraphQLString;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static graphql.schema.GraphQLObjectType.newObject;
+import static org.junit.Assert.assertEquals;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         GraphQLObjectType queryType = newObject()
+                .name("helloWorldQuery")
                 .field(newFieldDefinition()
                         .type(GraphQLString)
                         .name("hello")
@@ -22,7 +27,25 @@ public class HelloWorld {
         GraphQLSchema schema = GraphQLSchema.newSchema()
                 .query(queryType)
                 .build();
-        Object result = new GraphQL(schema).execute("{hello}").getResult();
+        Map<String, Object> result = new GraphQL(schema).execute("{hello}").getResult();
         System.out.println(result);
+    }
+
+    @Test
+    public void helloWorldTest() {
+        GraphQLObjectType queryType = newObject()
+                .name("helloWorldQuery")
+                .field(newFieldDefinition()
+                        .type(GraphQLString)
+                        .name("hello")
+                        .staticValue("world")
+                        .build())
+                .build();
+
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+                .query(queryType)
+                .build();
+        Map<String, Object> result = new GraphQL(schema).execute("{hello}").getResult();
+        assertEquals("world", result.get("hello"));
     }
 }
