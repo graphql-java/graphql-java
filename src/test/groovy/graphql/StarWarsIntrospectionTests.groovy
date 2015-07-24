30a1ad3602da91b21fc4a69ccc8c54edd780b3c3
andimarek <andimarek@fastmail.fm>
2015-07-24 09:15:02 +0200

    star wars introspection test

--- src/test/groovy/graphql/StarWarsIntrospectionTests.groovy
+++ src/test/groovy/graphql/StarWarsIntrospectionTests.groovy
@@ -69,4 +69,73 @@ class StarWarsIntrospectionTests extends Specification {
         result == expected
     }
 
+
+    def "Allows querying the schema for a specific type"() {
+        given:
+        def query = """
+        query IntrospectionDroidTypeQuery {
+            __type(name: "Droid") {
+                name
+            }
+        }
+        """
+        def expected = [
+                __type: [
+                        name: 'Droid'
+                ]
+        ]
+
+        when:
+        def result = new GraphQL(StarWarsSchema.starWarsSchema).execute(query).result
+
+        then:
+        result == expected
+    }
+
+    def 'Allows querying the schema for an object kind'(){
+        given:
+        def query = """
+        query IntrospectionDroidKindQuery {
+            __type(name: "Droid") {
+                name
+                kind
+            }
+        }
+        """
+        def expected = [
+            __type: [
+                name: 'Droid',
+                kind: 'OBJECT'
+            ]
+        ];
+        when:
+        def result = new GraphQL(StarWarsSchema.starWarsSchema).execute(query).result
+
+        then:
+        result == expected
+    }
+
+    def 'Allows querying the schema for an interface kind'(){
+        given:
+        def query = """
+        query IntrospectionCharacterKindQuery {
+            __type(name: "Character") {
+                name
+                kind
+            }
+        }
+        """
+        def expected = [
+            __type: [
+                name: 'Character',
+                kind: 'INTERFACE'
+            ]
+        ];
+        when:
+        def result = new GraphQL(StarWarsSchema.starWarsSchema).execute(query).result
+
+        then:
+        result == expected
+    }
+
 }
