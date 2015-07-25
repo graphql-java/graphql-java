4c0c3e3dffacdfeae87aba796562a391b7ce0bc3
andimarek <andimarek@fastmail.fm>
2015-07-25 11:45:13 +0200

    known directive test

--- src/test/groovy/graphql/validation/rules/KnownDirectivesTest.groovy
+++ src/test/groovy/graphql/validation/rules/KnownDirectivesTest.groovy
@@ -1,11 +1,63 @@
 package graphql.validation.rules
 
+import graphql.StarWarsSchema
+import graphql.language.Document
+import graphql.parser.Parser
+import graphql.validation.*
 import spock.lang.Specification
-
 
 class KnownDirectivesTest extends Specification {
 
-    KnownDirectives knownDirectives = new KnownDirectives()
+    ValidationContext validationContext = Mock(ValidationContext)
+    ValidationErrorCollector errorCollector = new ValidationErrorCollector()
+    KnownDirectives knownDirectives = new KnownDirectives(validationContext,errorCollector)
+
+    def setup() {
+        def traversalContext = Mock(TraversalContext)
+        validationContext.getSchema() >> StarWarsSchema.starWarsSchema
+        validationContext.getTraversalContext() >> traversalContext
+    }
+
+
+    def "misplaced directive"(){
+        given:
+        def query = """
+          query Foo @include(if: true) {
+                name
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal();
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]));
+
+        then:
+        errorCollector.containsValidationError(ValidationErrorType.MisplacedDirective)
+
+    }
+
+    def "well placed directive"(){
+        given:
+        def query = """
+          query Foo  {
+                name @include(if: true)
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal();
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]));
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
+
 
 
 }
