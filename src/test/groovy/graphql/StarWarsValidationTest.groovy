e977534d7a8cd784ea8429a44a85800a7aa4040f
andimarek <andimarek@fastmail.fm>
2015-07-28 08:22:35 +0200

    star wars validation tests

--- src/test/groovy/graphql/StarWarsValidationTest.groovy
+++ src/test/groovy/graphql/StarWarsValidationTest.groovy
@@ -3,11 +3,9 @@ package graphql
 import graphql.parser.Parser
 import graphql.validation.ValidationError
 import graphql.validation.Validator
-import spock.lang.Ignore
 import spock.lang.Specification
 
-
-class StarWarsValidationTest extends Specification{
+class StarWarsValidationTest extends Specification {
 
 
     List<ValidationError> validate(String query) {
@@ -15,9 +13,33 @@ class StarWarsValidationTest extends Specification{
         return new Validator().validateDocument(StarWarsSchema.starWarsSchema, document)
     }
 
-    @Ignore // not yet
+    def 'Validates a complex but valid query'() {
+        def query = """
+        query NestedQueryWithFragment {
+            hero {
+                ...NameAndAppearances
+                friends {
+                    ...NameAndAppearances
+                    friends {
+                        ...NameAndAppearances
+                    }
+                }
+            }
+        }
+
+        fragment NameAndAppearances on Character {
+            name
+            appearsIn
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.isEmpty()
+    }
+
     def 'Notes that non-existent fields are invalid'() {
-        given:
         def query = """
         query HeroSpaceshipQuery {
             hero {
@@ -25,11 +47,95 @@ class StarWarsValidationTest extends Specification{
             }
         }
         """
-
         when:
         def validationErrors = validate(query)
 
         then:
         validationErrors.size() > 0
     }
+
+    def 'Requires fields on objects'() {
+        def query = """
+        query HeroNoFieldsQuery {
+            hero
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.size() > 0
+    }
+
+    def 'Disallows fields on scalars'() {
+        def query = """
+        query HeroFieldsOnScalarQuery {
+            hero {
+                name {
+                    firstCharacterOfName
+                }
+            }
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.size() > 0
+    }
+
+    def 'Disallows object fields on interfaces'() {
+        def query = """
+        query DroidFieldOnCharacter {
+            hero {
+                name
+                primaryFunction
+            }
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.size() > 0
+    }
+
+    def 'Allows object fields in fragments'() {
+        def query = """
+        query DroidFieldInFragment {
+            hero {
+                name
+                ...DroidFields
+            }
+        }
+
+        fragment DroidFields on Droid {
+            primaryFunction
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.isEmpty()
+    }
+
+    def 'Allows object fields in inline fragments'() {
+        def query = """
+        query DroidFieldInFragment {
+            hero {
+                name
+                ... on Droid {
+                    primaryFunction
+                }
+            }
+        }
+        """
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.isEmpty()
+    }
+
 }
