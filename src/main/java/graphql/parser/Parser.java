d817b1bf949adf7c93944bad91b61bfa6737563f
andimarek <andimarek@fastmail.fm>
2015-07-31 07:07:37 +0200

    replace antlr default error listener (which prints to console) with error listener which uses our logger

--- src/main/java/graphql/parser/Parser.java
+++ src/main/java/graphql/parser/Parser.java
@@ -3,13 +3,14 @@ package graphql.parser;
 import graphql.language.Document;
 import graphql.parser.antlr.GraphqlLexer;
 import graphql.parser.antlr.GraphqlParser;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.DefaultErrorStrategy;
-import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.PredictionMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Parser {
+
+    private static final Logger log = LoggerFactory.getLogger(Parser.class);
 
 
     public Document parseDocument(String input) {
@@ -19,6 +20,8 @@ public class Parser {
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         GraphqlParser parser = new GraphqlParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(new ErrorListener());
         parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
         parser.setErrorHandler(new ErrorStrategy());
         GraphqlParser.DocumentContext document = parser.document();
@@ -34,6 +37,13 @@ public class Parser {
         @Override
         public void recover(org.antlr.v4.runtime.Parser recognizer, RecognitionException e) {
             throw e;
+        }
+    }
+
+    private class ErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+            log.error("graphql syntax error: line " + line + ":" + charPositionInLine + " " + msg);
         }
     }
 
