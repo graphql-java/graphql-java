1bc181089b5d8933bbb19cb6e1e09fd7576a7bb5
andimarek <andimarek@fastmail.fm>
2015-09-20 02:17:42 +0200

    add test for non-null argument

--- src/test/groovy/graphql/GraphQLTest.groovy
+++ src/test/groovy/graphql/GraphQLTest.groovy
@@ -1,13 +1,15 @@
 package graphql
 
 import graphql.language.SourceLocation
-import graphql.schema.GraphQLArgument
 import graphql.schema.GraphQLFieldDefinition
+import graphql.schema.GraphQLNonNull
 import graphql.schema.GraphQLObjectType
 import graphql.schema.GraphQLSchema
+import graphql.validation.ValidationErrorType
 import spock.lang.Specification
 
 import static graphql.Scalars.GraphQLString
+import static graphql.schema.GraphQLArgument.newArgument
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
 import static graphql.schema.GraphQLObjectType.newObject
 import static graphql.schema.GraphQLSchema.newSchema
@@ -77,7 +79,7 @@ class GraphQLTest extends Specification {
         GraphQLFieldDefinition fieldDefinition = newFieldDefinition()
                 .name("hello")
                 .type(GraphQLString)
-                .argument(GraphQLArgument.newArgument().name("arg").type(GraphQLString).build())
+                .argument(newArgument().name("arg").type(GraphQLString).build())
                 .staticValue("world")
                 .build()
         GraphQLSchema schema = newSchema().query(
@@ -126,5 +128,31 @@ class GraphQLTest extends Specification {
         errors.size() == 1
         errors[0].errorType == ErrorType.InvalidSyntax
         errors[0].sourceLocations == [new SourceLocation(1, 7)]
+    }
+
+    def "non null argument is missing"() {
+        given:
+        GraphQLSchema schema = newSchema().query(
+                newObject()
+                        .name("RootQueryType")
+                        .field(newFieldDefinition()
+                        .name("field")
+                        .type(GraphQLString)
+                        .argument(newArgument()
+                        .name("arg")
+                        .type(new GraphQLNonNull(GraphQLString))
+                        .build())
+                        .build())
+                        .build()
+        ).build()
+
+        when:
+        def errors = new GraphQL(schema).execute('{ field }').errors
+
+        then:
+        errors.size() == 1
+        errors[0].errorType == ErrorType.ValidationError
+        errors[0].validationErrorType == ValidationErrorType.MissingFieldArgument
+        errors[0].sourceLocations == [new SourceLocation(1, 3)]
     }
 }
