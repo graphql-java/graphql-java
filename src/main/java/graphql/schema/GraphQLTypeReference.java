5cd9768f81d1c5615640e77fb2eb5de3d0e05ce0
Theo Zourzouvillys <theo@jive.com>
2015-09-24 19:47:04 -0600

    GraphQLTypeReference is also a GraphQLInputType.

--- src/main/java/graphql/schema/GraphQLTypeReference.java
+++ src/main/java/graphql/schema/GraphQLTypeReference.java
@@ -7,7 +7,7 @@ import static graphql.Assert.assertNotNull;
  * A special type to allow a object/interface types to reference itself. It's replaced with the real type
  * object when the schema is build.
  */
-public class GraphQLTypeReference implements GraphQLType, GraphQLOutputType {
+public class GraphQLTypeReference implements GraphQLType, GraphQLOutputType, GraphQLInputType {
 
     private final String name;
 
