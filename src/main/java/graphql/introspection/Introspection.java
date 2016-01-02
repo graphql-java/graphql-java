3a96047a25c46db27f4c1743755215c0982b35ab
andimarek <andimarek@fastmail.fm>
2016-01-02 09:48:32 +0100

    quick bugfix: add 'subscriptionType'  field to support newer introspection queries

--- src/main/java/graphql/introspection/Introspection.java
+++ src/main/java/graphql/introspection/Introspection.java
@@ -352,7 +352,7 @@ public class Introspection {
             .name("__Schema")
             .description("A GraphQL Introspection defines the capabilities" +
                     " of a GraphQL server. It exposes all available types and directives on " +
-                    "'the server, as well as the entry points for query and  'mutation operations.")
+                    "the server, the entry points for query, mutation, and subscription operations.")
             .field(newFieldDefinition()
                     .name("types")
                     .description("A list of all types supported by this server.")
@@ -397,6 +397,18 @@ public class Introspection {
                         @Override
                         public Object get(DataFetchingEnvironment environment) {
                             return environment.getGraphQLSchema().getDirectives();
+                        }
+                    })
+                    .build())
+            .field(newFieldDefinition()
+                    .name("subscriptionType")
+                    .description("'If this server support subscription, the type that subscription operations will be rooted at.")
+                    .type(__Type)
+                    .dataFetcher(new DataFetcher() {
+                        @Override
+                        public Object get(DataFetchingEnvironment environment) {
+                            // Not yet supported
+                            return null;
                         }
                     })
                     .build())
