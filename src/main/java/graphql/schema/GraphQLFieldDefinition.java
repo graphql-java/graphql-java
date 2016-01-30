5bf6386db9d12dc47d3d4cfdf6af831f02f6d6fd
max <max@10.99.94.194>
2016-01-30 15:03:06 -0500

    adjust formatting

--- src/main/java/graphql/schema/GraphQLFieldDefinition.java
+++ src/main/java/graphql/schema/GraphQLFieldDefinition.java
@@ -1,10 +1,11 @@
 package graphql.schema;
 
-import static graphql.Assert.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static graphql.Assert.assertNotNull;
 
 public class GraphQLFieldDefinition {
 
@@ -15,14 +16,8 @@ public class GraphQLFieldDefinition {
     private final String deprecationReason;
     private final List<GraphQLArgument> arguments = new ArrayList<>();
 
-    public GraphQLFieldDefinition(
-        String name,
-        String description,
-        GraphQLOutputType type,
-        DataFetcher dataFetcher,
-        List<GraphQLArgument> arguments,
-        String deprecationReason) {
 
+    public GraphQLFieldDefinition(String name, String description, GraphQLOutputType type, DataFetcher dataFetcher, List<GraphQLArgument> arguments, String deprecationReason) {
         assertNotNull(name, "name can't be null");
         assertNotNull(dataFetcher, "dataFetcher can't be null");
         assertNotNull(type, "type can't be null");
@@ -35,59 +30,48 @@ public class GraphQLFieldDefinition {
         this.deprecationReason = deprecationReason;
     }
 
-    void replaceTypeReferences(
-        Map<String, GraphQLType> typeMap) {
 
+    void replaceTypeReferences(Map<String, GraphQLType> typeMap) {
         type = (GraphQLOutputType) new SchemaUtil().resolveTypeReference(type, typeMap);
     }
 
     public String getName() {
-
         return name;
     }
 
-    public GraphQLOutputType getType() {
 
+    public GraphQLOutputType getType() {
         return type;
     }
 
     public DataFetcher getDataFetcher() {
-
         return dataFetcher;
     }
 
-    public GraphQLArgument getArgument(
-        String name) {
-
+    public GraphQLArgument getArgument(String name) {
         for (GraphQLArgument argument : arguments) {
-            if (argument.getName().equals(name))
-                return argument;
+            if (argument.getName().equals(name)) return argument;
         }
         return null;
     }
 
     public List<GraphQLArgument> getArguments() {
-
         return new ArrayList<>(arguments);
     }
 
     public String getDescription() {
-
         return description;
     }
 
     public String getDeprecationReason() {
-
         return deprecationReason;
     }
 
     public boolean isDeprecated() {
-
         return deprecationReason != null;
     }
 
     public static Builder newFieldDefinition() {
-
         return new Builder();
     }
 
@@ -102,53 +86,38 @@ public class GraphQLFieldDefinition {
         private boolean isField;
         private String fieldName;
 
-        public Builder name(
-            String name) {
 
+        public Builder name(String name) {
             this.name = name;
             this.fieldName = name;
             return this;
         }
-
-        public Builder nameAndFieldName(
-            String name,
-            String fieldName) {
-
+        
+        public Builder nameAndFieldName(String name,String fieldName) {
             this.name = name;
             this.fieldName = fieldName;
             return this;
         }
 
-        public Builder description(
-            String description) {
-
+        public Builder description(String description) {
             this.description = description;
             return this;
         }
 
-        public Builder type(
-            GraphQLOutputType type) {
-
+        public Builder type(GraphQLOutputType type) {
             this.type = type;
             return this;
         }
 
-        public Builder dataFetcher(
-            DataFetcher dataFetcher) {
-
+        public Builder dataFetcher(DataFetcher dataFetcher) {
             this.dataFetcher = dataFetcher;
             return this;
         }
 
-        public Builder staticValue(
-            final Object value) {
-
+        public Builder staticValue(final Object value) {
             this.dataFetcher = new DataFetcher() {
-
                 @Override
-                public Object get(
-                    DataFetchingEnvironment environment) {
-
+                public Object get(DataFetchingEnvironment environment) {
                     return value;
                 }
             };
@@ -160,34 +129,26 @@ public class GraphQLFieldDefinition {
          * @return
          */
         public Builder fetchField() {
-
             this.isField = true;
             return this;
         }
 
-        public Builder argument(
-            GraphQLArgument argument) {
-
+        public Builder argument(GraphQLArgument argument) {
             this.arguments.add(argument);
             return this;
         }
 
-        public Builder argument(
-            List<GraphQLArgument> arguments) {
-
+        public Builder argument(List<GraphQLArgument> arguments) {
             this.arguments.addAll(arguments);
             return this;
         }
 
-        public Builder deprecate(
-            String deprecationReason) {
-
+        public Builder deprecate(String deprecationReason) {
             this.deprecationReason = deprecationReason;
             return this;
         }
 
         public GraphQLFieldDefinition build() {
-
             if (dataFetcher == null) {
                 if (isField) {
                     dataFetcher = new FieldDataFetcher(fieldName);
@@ -197,6 +158,7 @@ public class GraphQLFieldDefinition {
             }
             return new GraphQLFieldDefinition(name, description, type, dataFetcher, arguments, deprecationReason);
         }
+
 
     }
 }
