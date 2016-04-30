c3fbc2d594766b445f975a63154ce2867d03c93d
Winsor, Daniel <daniel.winsor@etrade.com>
2016-04-29 18:12:05 -0700

    Union types must have at least 1 member, and members can be only of Object types, not scalars etc.

--- src/main/java/graphql/Assert.java
+++ src/main/java/graphql/Assert.java
@@ -1,5 +1,6 @@
 package graphql;
 
+import java.util.Collection;
 
 public class Assert {
 
@@ -7,6 +8,11 @@ public class Assert {
     public static void assertNotNull(Object object, String errorMessage) {
         if (object != null) return;
         throw new AssertException(errorMessage);
+    }
+
+    public static void assertNotEmpty(Collection<?> c, String errorMessage) {
+        if (c == null || c.isEmpty()) throw new AssertException(errorMessage);
+        return;
     }
 
 }
