9ccf1e5d7b631c60f3b5e1d200a7efb45118f0f4
Jiayu Liu <jiayu@madadata.com>
2017-04-07 10:55:56 +0800

    base64-encoding

--- src/main/java/graphql/relay/Base64.java
+++ src/main/java/graphql/relay/Base64.java
@@ -1,24 +1,25 @@
 package graphql.relay;
 
 import java.nio.charset.Charset;
-import javax.xml.bind.DatatypeConverter;
-import java.io.UnsupportedEncodingException;
 
-
+/**
+ * @deprecated use {@link java.util.Base64} instead.
+ */
+@Deprecated
 public class Base64 {
 
     private Base64() {
     }
 
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+    private static final java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
+
     public static String toBase64(String string) {
-        try {
-            return DatatypeConverter.printBase64Binary(string.getBytes("utf-8"));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        return encoder.encodeToString(string.getBytes(UTF8));
     }
 
     public static String fromBase64(String string) {
-        return new String(DatatypeConverter.parseBase64Binary(string), Charset.forName("UTF-8"));
+        return new String(decoder.decode(string), UTF8);
     }
 }
--- src/main/java/graphql/relay/Relay.java
+++ src/main/java/graphql/relay/Relay.java
@@ -1,12 +1,24 @@
 package graphql.relay;
 
-
-import graphql.schema.*;
-
+import graphql.schema.DataFetcher;
+import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLInputObjectField;
+import graphql.schema.GraphQLInputObjectType;
+import graphql.schema.GraphQLInterfaceType;
+import graphql.schema.GraphQLList;
+import graphql.schema.GraphQLNonNull;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLOutputType;
+import graphql.schema.TypeResolver;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static graphql.Scalars.*;
+import static graphql.Scalars.GraphQLBoolean;
+import static graphql.Scalars.GraphQLID;
+import static graphql.Scalars.GraphQLInt;
+import static graphql.Scalars.GraphQLString;
 import static graphql.schema.GraphQLArgument.newArgument;
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static graphql.schema.GraphQLInputObjectField.newInputObjectField;
@@ -18,50 +30,50 @@ public class Relay {
 
     public static final String NODE = "Node";
     private GraphQLObjectType pageInfoType = newObject()
-            .name("PageInfo")
-            .description("Information about pagination in a connection.")
-            .field(newFieldDefinition()
-                    .name("hasNextPage")
-                    .type(new GraphQLNonNull(GraphQLBoolean))
-                    .description("When paginating forwards, are there more items?"))
-            .field(newFieldDefinition()
-                    .name("hasPreviousPage")
-                    .type(new GraphQLNonNull(GraphQLBoolean))
-                    .description("When paginating backwards, are there more items?"))
-            .field(newFieldDefinition()
-                    .name("startCursor")
-                    .type(GraphQLString)
-                    .description("When paginating backwards, the cursor to continue."))
-            .field(newFieldDefinition()
-                    .name("endCursor")
-                    .type(GraphQLString)
-                    .description("When paginating forwards, the cursor to continue."))
-            .build();
+        .name("PageInfo")
+        .description("Information about pagination in a connection.")
+        .field(newFieldDefinition()
+            .name("hasNextPage")
+            .type(new GraphQLNonNull(GraphQLBoolean))
+            .description("When paginating forwards, are there more items?"))
+        .field(newFieldDefinition()
+            .name("hasPreviousPage")
+            .type(new GraphQLNonNull(GraphQLBoolean))
+            .description("When paginating backwards, are there more items?"))
+        .field(newFieldDefinition()
+            .name("startCursor")
+            .type(GraphQLString)
+            .description("When paginating backwards, the cursor to continue."))
+        .field(newFieldDefinition()
+            .name("endCursor")
+            .type(GraphQLString)
+            .description("When paginating forwards, the cursor to continue."))
+        .build();
 
     public GraphQLInterfaceType nodeInterface(TypeResolver typeResolver) {
         GraphQLInterfaceType node = newInterface()
-                .name(NODE)
-                .description("An object with an ID")
-                .typeResolver(typeResolver)
-                .field(newFieldDefinition()
-                        .name("id")
-                        .description("The ID of an object")
-                        .type(new GraphQLNonNull(GraphQLID)))
-                .build();
+            .name(NODE)
+            .description("An object with an ID")
+            .typeResolver(typeResolver)
+            .field(newFieldDefinition()
+                .name("id")
+                .description("The ID of an object")
+                .type(new GraphQLNonNull(GraphQLID)))
+            .build();
         return node;
     }
 
     public GraphQLFieldDefinition nodeField(GraphQLInterfaceType nodeInterface, DataFetcher nodeDataFetcher) {
         GraphQLFieldDefinition fieldDefinition = newFieldDefinition()
-                .name("node")
-                .description("Fetches an object given its ID")
-                .type(nodeInterface)
-                .dataFetcher(nodeDataFetcher)
-                .argument(newArgument()
-                        .name("id")
-                        .description("The ID of an object")
-                        .type(new GraphQLNonNull(GraphQLID)))
-                .build();
+            .name("node")
+            .description("Fetches an object given its ID")
+            .type(nodeInterface)
+            .dataFetcher(nodeDataFetcher)
+            .argument(newArgument()
+                .name("id")
+                .description("The ID of an object")
+                .type(new GraphQLNonNull(GraphQLID)))
+            .build();
         return fieldDefinition;
     }
 
@@ -69,21 +81,21 @@ public class Relay {
         List<GraphQLArgument> args = new ArrayList<>();
 
         args.add(newArgument()
-                .name("before")
-                .type(GraphQLString)
-                .build());
+            .name("before")
+            .type(GraphQLString)
+            .build());
         args.add(newArgument()
-                .name("after")
-                .type(GraphQLString)
-                .build());
+            .name("after")
+            .type(GraphQLString)
+            .build());
         args.add(newArgument()
-                .name("first")
-                .type(GraphQLInt)
-                .build());
+            .name("first")
+            .type(GraphQLInt)
+            .build());
         args.add(newArgument()
-                .name("last")
-                .type(GraphQLInt)
-                .build());
+            .name("last")
+            .type(GraphQLInt)
+            .build());
         return args;
     }
 
@@ -91,13 +103,13 @@ public class Relay {
         List<GraphQLArgument> args = new ArrayList<>();
 
         args.add(newArgument()
-                .name("before")
-                .type(GraphQLString)
-                .build());
+            .name("before")
+            .type(GraphQLString)
+            .build());
         args.add(newArgument()
-                .name("last")
-                .type(GraphQLInt)
-                .build());
+            .name("last")
+            .type(GraphQLInt)
+            .build());
         return args;
     }
 
@@ -105,78 +117,77 @@ public class Relay {
         List<GraphQLArgument> args = new ArrayList<>();
 
         args.add(newArgument()
-                .name("after")
-                .type(GraphQLString)
-                .build());
+            .name("after")
+            .type(GraphQLString)
+            .build());
         args.add(newArgument()
-                .name("first")
-                .type(GraphQLInt)
-                .build());
+            .name("first")
+            .type(GraphQLInt)
+            .build());
         return args;
     }
 
     public GraphQLObjectType edgeType(String name, GraphQLOutputType nodeType, GraphQLInterfaceType nodeInterface, List<GraphQLFieldDefinition> edgeFields) {
 
         GraphQLObjectType edgeType = newObject()
-                .name(name + "Edge")
-                .description("An edge in a connection.")
-                .field(newFieldDefinition()
-                        .name("node")
-                        .type(nodeType)
-                        .description("The item at the end of the edge"))
-                .field(newFieldDefinition()
-                        .name("cursor")
-                        .type(new GraphQLNonNull(GraphQLString))
-                        .description(""))
-                .fields(edgeFields)
-                .build();
+            .name(name + "Edge")
+            .description("An edge in a connection.")
+            .field(newFieldDefinition()
+                .name("node")
+                .type(nodeType)
+                .description("The item at the end of the edge"))
+            .field(newFieldDefinition()
+                .name("cursor")
+                .type(new GraphQLNonNull(GraphQLString))
+                .description(""))
+            .fields(edgeFields)
+            .build();
         return edgeType;
     }
 
     public GraphQLObjectType connectionType(String name, GraphQLObjectType edgeType, List<GraphQLFieldDefinition> connectionFields) {
 
         GraphQLObjectType connectionType = newObject()
-                .name(name + "Connection")
-                .description("A connection to a list of items.")
-                .field(newFieldDefinition()
-                        .name("edges")
-                        .type(new GraphQLList(edgeType)))
-                .field(newFieldDefinition()
-                        .name("pageInfo")
-                        .type(new GraphQLNonNull(pageInfoType)))
-                .fields(connectionFields)
-                .build();
+            .name(name + "Connection")
+            .description("A connection to a list of items.")
+            .field(newFieldDefinition()
+                .name("edges")
+                .type(new GraphQLList(edgeType)))
+            .field(newFieldDefinition()
+                .name("pageInfo")
+                .type(new GraphQLNonNull(pageInfoType)))
+            .fields(connectionFields)
+            .build();
         return connectionType;
     }
-
 
     public GraphQLFieldDefinition mutationWithClientMutationId(String name, String fieldName,
                                                                List<GraphQLInputObjectField> inputFields,
                                                                List<GraphQLFieldDefinition> outputFields,
                                                                DataFetcher dataFetcher) {
         GraphQLInputObjectType inputObjectType = newInputObject()
-                .name(name + "Input")
-                .field(newInputObjectField()
-                        .name("clientMutationId")
-                        .type(new GraphQLNonNull(GraphQLString)))
-                .fields(inputFields)
-                .build();
+            .name(name + "Input")
+            .field(newInputObjectField()
+                .name("clientMutationId")
+                .type(new GraphQLNonNull(GraphQLString)))
+            .fields(inputFields)
+            .build();
         GraphQLObjectType outputType = newObject()
-                .name(name + "Payload")
-                .field(newFieldDefinition()
-                        .name("clientMutationId")
-                        .type(new GraphQLNonNull(GraphQLString)))
-                .fields(outputFields)
-                .build();
+            .name(name + "Payload")
+            .field(newFieldDefinition()
+                .name("clientMutationId")
+                .type(new GraphQLNonNull(GraphQLString)))
+            .fields(outputFields)
+            .build();
 
         return newFieldDefinition()
-                .name(fieldName)
-                .type(outputType)
-                .argument(newArgument()
-                        .name("input")
-                        .type(new GraphQLNonNull(inputObjectType)))
-                .dataFetcher(dataFetcher)
-                .build();
+            .name(fieldName)
+            .type(outputType)
+            .argument(newArgument()
+                .name("input")
+                .type(new GraphQLNonNull(inputObjectType)))
+            .dataFetcher(dataFetcher)
+            .build();
     }
 
     public static class ResolvedGlobalId {
@@ -206,12 +217,16 @@ public class Relay {
         }
     }
 
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+    private static final java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
+
     public String toGlobalId(String type, String id) {
-        return Base64.toBase64(type + ":" + id);
+        return encoder.encodeToString((type + ":" + id).getBytes(UTF8));
     }
 
     public ResolvedGlobalId fromGlobalId(String globalId) {
-        String[] split = Base64.fromBase64(globalId).split(":", 2);
+        String[] split = new String(decoder.decode(globalId), UTF8).split(":", 2);
         if (split.length != 2) {
             throw new IllegalArgumentException(String.format("expecting a valid global id, got %s", globalId));
         }
