096da1c1f1c2d8aff32b6b2602c3ef35bdd4ecbe
Brad Baker <bbaker@atlassian.com>
2017-04-17 10:54:09 +1000

    #352 - fixed the bug where too many tokens are allowed as valid

--- src/main/java/graphql/parser/Parser.java
+++ src/main/java/graphql/parser/Parser.java
@@ -6,7 +6,11 @@ import graphql.parser.antlr.GraphqlParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.atn.PredictionMode;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
+import java.util.List;
 
 public class Parser {
 
@@ -25,6 +29,21 @@ public class Parser {
 
         GraphqlAntlrToLanguage antlrToLanguage = new GraphqlAntlrToLanguage(tokens);
         antlrToLanguage.visitDocument(document);
+
+        Token stop = document.getStop();
+        List<Token> allTokens = tokens.getTokens();
+        if (stop != null && allTokens != null && !allTokens.isEmpty()) {
+            Token last = allTokens.get(allTokens.size() - 1);
+            //
+            // do we have more tokens in the stream than we consumed in the parse?
+            // if yes then its invalid.  We make sure its the same channel
+            boolean notEOF = last.getType() != Token.EOF;
+            boolean lastGreaterThanDocument = last.getTokenIndex() > stop.getTokenIndex();
+            boolean sameChannel = last.getChannel() == stop.getChannel();
+            if (notEOF && lastGreaterThanDocument && sameChannel) {
+                throw new ParseCancellationException("There are more tokens in the query that have not been consumed");
+            }
+        }
         return antlrToLanguage.result;
     }
 }
