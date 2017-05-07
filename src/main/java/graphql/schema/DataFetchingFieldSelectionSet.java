a75cdea6172c5096a49e34874a22592cb61f18eb
Brad Baker <bbaker@atlassian.com>
2017-05-07 14:29:19 +1000

    Made DataFetchingFieldSelectionSet a supplier since it really is

--- src/main/java/graphql/schema/DataFetchingFieldSelectionSet.java
+++ src/main/java/graphql/schema/DataFetchingFieldSelectionSet.java
@@ -4,12 +4,13 @@ import graphql.language.Field;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * This allows you to retrieve the selection set of fields that have been asked for when the
  * {@link DataFetcher} was invoked
  */
-public interface DataFetchingFieldSelectionSet {
+public interface DataFetchingFieldSelectionSet extends Supplier<Map<String, List<Field>>> {
 
     /**
      * @return a map of the fields that represent the selection set
