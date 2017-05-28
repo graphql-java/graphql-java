0be159f4a3e200d0d2153cf4ee509ce2be55d3db
Brad Baker <bbaker@atlassian.com>
2017-05-29 06:56:00 +1000

    #438 - unit test for NonNullableFieldValidator

---
+++ src/test/groovy/graphql/execution/NonNullableFieldValidatorTest.groovy
@@ -0,0 +1,36 @@
+package graphql.execution
+
+import spock.lang.Specification
+
+import static graphql.Scalars.GraphQLString
+import static graphql.schema.GraphQLNonNull.nonNull
+
+class NonNullableFieldValidatorTest extends Specification {
+
+    ExecutionContext context = new ExecutionContext(null, null, null, null, null, null, null, null, null, null)
+
+    def "non nullable field throws exception"() {
+        TypeInfo typeInfo = TypeInfo.newTypeInfo().type(nonNull(GraphQLString)).build()
+
+        NonNullableFieldValidator validator = new NonNullableFieldValidator(context, typeInfo)
+
+        when:
+        validator.validate(null)
+
+        then:
+        thrown(NonNullableFieldWasNullException)
+
+    }
+
+    def "nullable field does not throw exception"() {
+        TypeInfo typeInfo = TypeInfo.newTypeInfo().type(GraphQLString).build()
+
+        NonNullableFieldValidator validator = new NonNullableFieldValidator(context, typeInfo)
+
+        when:
+        def result = validator.validate(null)
+
+        then:
+        result == null
+    }
+}
