9addb0219831a39c9ffc3cc96ab445c4e15b700f
Brad Baker <bbaker@atlassian.com>
2017-05-29 08:03:50 +1000

    #467 generate additional types into schema

--- src/test/groovy/graphql/TestUtil.groovy
+++ src/test/groovy/graphql/TestUtil.groovy
@@ -1,10 +1,22 @@
 package graphql
 
-import graphql.schema.*
+import graphql.language.FieldDefinition
+import graphql.language.InterfaceTypeDefinition
+import graphql.language.UnionTypeDefinition
+import graphql.schema.DataFetcher
+import graphql.schema.GraphQLArgument
+import graphql.schema.GraphQLFieldDefinition
+import graphql.schema.GraphQLInputType
+import graphql.schema.GraphQLObjectType
+import graphql.schema.GraphQLSchema
+import graphql.schema.PropertyDataFetcher
+import graphql.schema.TypeResolver
 import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.SchemaGenerator
 import graphql.schema.idl.SchemaParser
+import graphql.schema.idl.TypeDefinitionRegistry
 import graphql.schema.idl.TypeRuntimeWiring
+import graphql.schema.idl.WiringFactory
 import graphql.schema.idl.errors.SchemaProblem
 
 import static graphql.Scalars.GraphQLString
@@ -40,6 +52,11 @@ class TestUtil {
         schema(spec, runtimeWiring.build())
     }
 
+    static GraphQLSchema schema(String spec) {
+        schema(spec, RuntimeWiring.newRuntimeWiring().wiringFactory(mockWiringFactory).build())
+    }
+
+    @SuppressWarnings("GroovyMissingReturnStatement")
     static GraphQLSchema schema(String spec, RuntimeWiring runtimeWiring) {
         try {
             def registry = new SchemaParser().parse(spec)
@@ -48,4 +65,47 @@ class TestUtil {
             assert false: "The schema could not be compiled : ${e}"
         }
     }
+
+    static WiringFactory mockWiringFactory = new WiringFactory() {
+        @Override
+        boolean providesTypeResolver(TypeDefinitionRegistry registry, InterfaceTypeDefinition interfaceType) {
+            return true
+        }
+
+        @Override
+        boolean providesTypeResolver(TypeDefinitionRegistry registry, UnionTypeDefinition unionType) {
+            return true
+        }
+
+        @Override
+        TypeResolver getTypeResolver(TypeDefinitionRegistry registry, InterfaceTypeDefinition interfaceType) {
+            new TypeResolver() {
+                @Override
+                GraphQLObjectType getType(TypeResolutionEnvironment env) {
+                    throw new UnsupportedOperationException("Not implemented")
+                }
+            }
+        }
+
+        @Override
+        TypeResolver getTypeResolver(TypeDefinitionRegistry registry, UnionTypeDefinition unionType) {
+            new TypeResolver() {
+                @Override
+                GraphQLObjectType getType(TypeResolutionEnvironment env) {
+                    throw new UnsupportedOperationException("Not implemented")
+                }
+            }
+        }
+
+        @Override
+        boolean providesDataFetcher(TypeDefinitionRegistry registry, FieldDefinition definition) {
+            return true
+        }
+
+        @Override
+        DataFetcher getDataFetcher(TypeDefinitionRegistry registry, FieldDefinition definition) {
+            return new PropertyDataFetcher(definition.getName())
+        }
+    }
+
 }
