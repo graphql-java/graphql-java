a4c073c45958830cef63844cbb17d4101e38c820
Brad Baker <bbaker@atlassian.com>
2017-06-24 09:40:45 +1000

    Better error handling via composition
    #523 - this addresses the ability to compose execution strategies better
    #525 - this also addresses the need to have a richer error reporting capability

---
+++ src/main/java/graphql/execution/DataFetcherExceptionHandler.java
@@ -0,0 +1,23 @@
+package graphql.execution;
+
+import graphql.GraphQLError;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import java.util.function.Consumer;
+
+/**
+ * This is called when an exception is thrown during {@link graphql.schema.DataFetcher#get(DataFetchingEnvironment)} execution
+ */
+public interface DataFetcherExceptionHandler extends Consumer<DataFetcherExceptionHandlerParameters> {
+
+    /**
+     * When an exception during a call to a {@link DataFetcher} then this handler
+     * is called back to shape the error that should be placed in the list of errors
+     * via {@link ExecutionContext#addError(GraphQLError)}
+     *
+     * @param handlerParameters the parameters to this callback
+     */
+    @Override
+    void accept(DataFetcherExceptionHandlerParameters handlerParameters);
+}
