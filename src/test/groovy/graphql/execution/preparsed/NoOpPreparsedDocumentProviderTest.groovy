06e52c35ecc0d88effa984aa0634ece0732d0cf2
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-06-25 08:55:08 +0200

    Renamed test PreparsedTest -> PreparsedDocumentProviderTest and removed some comments. Implemented unit test on NoOpPreparsedDocumentProvider

---
+++ src/test/groovy/graphql/execution/preparsed/NoOpPreparsedDocumentProviderTest.groovy
@@ -0,0 +1,20 @@
+package graphql.execution.preparsed
+
+import graphql.language.Document
+import spock.lang.Specification
+
+
+class NoOpPreparsedDocumentProviderTest extends Specification {
+    def "NoOp always returns result of compute function"() {
+        given:
+        def provider = NoOpPreparsedDocumentProvider.INSTANCE
+        def documentEntry = new PreparsedDocumentEntry(new Document())
+
+        when:
+        def actual = provider.get("{}", { return documentEntry})
+
+        then:
+        actual == documentEntry
+
+    }
+}
