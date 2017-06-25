a4647087490a9ba95911b8c3490723d8df5178d8
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-06-25 09:43:27 +0200

    ParsedDocumentEntry's fields are made final and added some guards to prevent null inputs. Added unit test for ParsedDocumentEntry as well.

---
+++ src/test/groovy/graphql/execution/preparsed/PreparsedDocumentEntryTest.groovy
@@ -0,0 +1,62 @@
+package graphql.execution.preparsed
+
+import graphql.GraphQLError
+import graphql.InvalidSyntaxError
+import graphql.language.Document
+import graphql.language.SourceLocation
+import graphql.validation.ValidationError
+import graphql.validation.ValidationErrorType
+import spock.lang.Specification
+
+class PreparsedDocumentEntryTest extends Specification {
+    def "Ensure a non-null document returns"() {
+        given:
+        def document = new Document()
+
+        when:
+        def docEntry = new PreparsedDocumentEntry(document)
+
+        then:
+        docEntry.document == document
+        docEntry.errors == null
+    }
+
+    def "Ensure a null document throws NPE"() {
+        when:
+        new PreparsedDocumentEntry((Document) null)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+    def "Ensure a non-null errors returns"() {
+        given:
+        def errors = [new InvalidSyntaxError(new SourceLocation(0, 0)),
+                      new ValidationError(ValidationErrorType.InvalidSyntax)]
+
+        when:
+        def docEntry = new PreparsedDocumentEntry(errors)
+
+        then:
+        docEntry.document == null
+        docEntry.errors == errors
+    }
+
+    def "Ensure a null errors throws NPE"() {
+        when:
+        new PreparsedDocumentEntry((List<GraphQLError>) null)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+    def "Ensure a null error throws NPE"() {
+        when:
+        new PreparsedDocumentEntry((GraphQLError) null)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+
+}
