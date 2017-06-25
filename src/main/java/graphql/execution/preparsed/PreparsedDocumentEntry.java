bc388c48972b4e90e86d1bb6540efbf93d1bc0a9
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-06-25 09:43:27 +0200

    ParsedDocumentEntry's fields are made final and added some guards to prevent null inputs. Added unit test for ParsedDocumentEntry as well.

--- src/main/java/graphql/execution/preparsed/PreparsedDocumentEntry.java
+++ src/main/java/graphql/execution/preparsed/PreparsedDocumentEntry.java
@@ -5,25 +5,30 @@ import graphql.language.Document;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * An instance of a preparsed doucument entry represents the result of a query parse and validation, like
  * an either implementation it contains either the correct result in th document property or the errors.
  */
 public class PreparsedDocumentEntry {
-    private Document document;
-    private List<? extends GraphQLError> errors;
+    private final Document document;
+    private final List<? extends GraphQLError> errors;
 
     public PreparsedDocumentEntry(Document document) {
+        Objects.requireNonNull(document);
         this.document = document;
+        this.errors = null;
     }
 
     public PreparsedDocumentEntry(List<? extends GraphQLError> errors) {
+        Objects.requireNonNull(errors);
+        this.document = null;
         this.errors = errors;
     }
 
     public PreparsedDocumentEntry(GraphQLError error) {
-        this.errors = Collections.singletonList(error);
+        this(Collections.singletonList(Objects.requireNonNull(error)));
     }
 
     public Document getDocument() {
@@ -33,8 +38,7 @@ public class PreparsedDocumentEntry {
     public List<? extends GraphQLError> getErrors() {
         return errors;
     }
-
-
+    
     public boolean hasErrors() {
         return errors != null && !errors.isEmpty();
     }
