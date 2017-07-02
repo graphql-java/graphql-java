fe24dfe422189c1be743fee7adabaa22381074b1
Brad Baker <bbaker@atlassian.com>
2017-07-03 09:21:03 +1000

    Fixed javadoc warning

--- src/main/java/graphql/execution/preparsed/PreparsedDocumentProvider.java
+++ src/main/java/graphql/execution/preparsed/PreparsedDocumentProvider.java
@@ -4,17 +4,20 @@ package graphql.execution.preparsed;
 import java.util.function.Function;
 
 /**
- * Interface that allows clients to hook in Document caching and/or whitelisting of queries
+ * Interface that allows clients to hook in Document caching and/or the whitelisting of queries
  */
 @FunctionalInterface
 public interface PreparsedDocumentProvider {
     /**
-     * Get existing instance of a preparsed query
+     * This is called to get a "cached" pre-parsed query and if its not present, then the computeFunction
+     * can be called to parse the query
      *
-     * @param query The graphql query
-     * @return Null of missing or an instance of {@link PreparsedDocumentEntry}
+     * @param query           The graphql query
+     * @param computeFunction If the query has not be pre-parsed, this function can be called to parse it
+     *
+     * @return an instance of {@link PreparsedDocumentEntry}
      */
-    PreparsedDocumentEntry get(String query, Function<String, PreparsedDocumentEntry> compute);
+    PreparsedDocumentEntry get(String query, Function<String, PreparsedDocumentEntry> computeFunction);
 }
 
 
