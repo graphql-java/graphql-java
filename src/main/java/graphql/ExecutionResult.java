28b22812eb575b45cca66d8096622f4268201db6
Brad Baker <bbaker@atlassian.com>
2017-07-06 18:58:31 +1000

    javadoc fix

--- src/main/java/graphql/ExecutionResult.java
+++ src/main/java/graphql/ExecutionResult.java
@@ -27,7 +27,7 @@ public interface ExecutionResult {
      * should be present.  Certain JSON serializers may or may interpret {@link ExecutionResult} to spec, so this method
      * is provided to produce a map that strictly follows the specification.
      *
-     * See : <a href="http://facebook.github.io/graphql/#sec-Response-Format">http://facebook.github.io/graphql/#sec-Response-Format">http://facebook.github.io/graphql/#sec-Response-Format</a>
+     * See : <a href="http://facebook.github.io/graphql/#sec-Response-Format">http://facebook.github.io/graphql/#sec-Response-Format</a>
      *
      * @return a map of the result that strictly follows the spec
      */
