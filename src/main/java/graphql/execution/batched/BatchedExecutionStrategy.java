3d2e6881ebabfd08630bb8abd5d47cb2d97b5907
huangdong <huangdong@dayuwuxian.com>
2017-07-20 11:19:14 +0800

    Fix BatchedExectionStrategy - IndexOutOfBoundException

--- src/main/java/graphql/execution/batched/BatchedExecutionStrategy.java
+++ src/main/java/graphql/execution/batched/BatchedExecutionStrategy.java
@@ -328,9 +328,8 @@ public class BatchedExecutionStrategy extends ExecutionStrategy {
         try {
             values = (List<Object>) getDataFetcher(fieldDef).get(environment);
         } catch (Exception e) {
-            values = new ArrayList<>(nodeData.size());
+            values = Collections.nCopies(nodeData.size(), null);
             log.warn("Exception while fetching data", e);
-
             DataFetcherExceptionHandlerParameters handlerParameters = DataFetcherExceptionHandlerParameters.newExceptionParameters()
                     .executionContext(executionContext)
                     .dataFetchingEnvironment(environment)
