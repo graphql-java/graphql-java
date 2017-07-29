d618ac8b559373b39b266f57138911977dc403ed
Anastasios Basoukos <abasoukos@groupon.com>
2017-07-29 17:44:12 +0200

    Allow schemas to contain custom directives for queries.

---
+++ src/test/groovy/graphql/validation/ValidateCustomDirectives.groovy
@@ -0,0 +1,61 @@
+package graphql.validation
+
+import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLSchema;
+import graphql.introspection.Introspection.DirectiveLocation;
+import graphql.parser.Parser
+import graphql.validation.ValidationError
+import graphql.validation.Validator
+import spock.lang.Specification
+/**
+ * Test that custom directives are validated properly.
+ */
+class ValidateCustomDirectives extends Specification {
+
+    GraphQLDirective customDirective = GraphQLDirective.newDirective()
+            .name("customDirective")
+            .description("Dummy Custom Directive")
+            .validLocations(DirectiveLocation.FIELD)
+            .build();
+
+    GraphQLSchema customDirectiveSchema = GraphQLSchema.newSchema()
+            .query(SpecValidationSchema.queryRoot)
+            .build(SpecValidationSchema.specValidationDictionary, [customDirective].toSet());
+
+    def 'Schema with custom directive validates query with same directive'() {
+        def query = """
+query {
+  dog {
+    name @customDirective
+  }
+}
+"""
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.empty
+    }
+
+    def 'Schema with custom directive validates query parameters'() {
+        def query = """
+query {
+  dog {
+    name @customDirective(dummy: true)
+  }
+}
+"""
+        when:
+        def validationErrors = validate(query)
+
+        then:
+        validationErrors.size() == 1
+        validationErrors.get(0).getValidationErrorType() == ValidationErrorType.UnknownDirective
+        validationErrors.get(0).getDescription() =='Unknown directive argument dummy'
+    }
+
+    List<ValidationError> validate(String query) {
+        def document = new Parser().parseDocument(query)
+        return new Validator().validateDocument(customDirectiveSchema, document)
+    }
+}
