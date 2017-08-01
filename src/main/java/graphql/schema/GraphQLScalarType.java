4315735171ddae0813307728389a52dea184e285
Brad Baker <bbaker@atlassian.com>
2017-08-02 08:11:45 +1000

    Better coercing Javadoc

--- src/main/java/graphql/schema/GraphQLScalarType.java
+++ src/main/java/graphql/schema/GraphQLScalarType.java
@@ -6,6 +6,17 @@ import graphql.language.ScalarTypeDefinition;
 import static graphql.Assert.assertNotNull;
 import static graphql.Assert.assertValidName;
 
+/**
+ * This allows you to define new scalar types.
+ * <p>
+ * <blockquote>
+ * GraphQL provides a number of built‐in scalars, but type systems can add additional scalars with semantic meaning,
+ * for example, a GraphQL system could define a scalar called Time which, while serialized as a string, promises to
+ * conform to ISO‐8601. When querying a field of type Time, you can then rely on the ability to parse the result with an ISO‐8601 parser and use a client‐specific primitive for time.
+ * <p>
+ * From the spec : http://facebook.github.io/graphql/#sec-Scalars
+ * </blockquote>
+ */
 public class GraphQLScalarType implements GraphQLType, GraphQLInputType, GraphQLOutputType, GraphQLUnmodifiedType, GraphQLNullableType {
 
     private final String name;
@@ -14,7 +25,7 @@ public class GraphQLScalarType implements GraphQLType, GraphQLInputType, GraphQL
     private final ScalarTypeDefinition definition;
 
     public GraphQLScalarType(String name, String description, Coercing coercing) {
-        this(name,description,coercing,null);
+        this(name, description, coercing, null);
     }
 
     public GraphQLScalarType(String name, String description, Coercing coercing, ScalarTypeDefinition definition) {
