13f127353f26ad1969435b7dc27ff1f05d0fb6fa
Brad Baker <bbaker@atlassian.com>
2017-08-04 00:09:58 +1000

    Added chained instrumentation

--- src/main/java/graphql/execution/instrumentation/parameters/InstrumentationFieldFetchParameters.java
+++ src/main/java/graphql/execution/instrumentation/parameters/InstrumentationFieldFetchParameters.java
@@ -2,6 +2,7 @@ package graphql.execution.instrumentation.parameters;
 
 import graphql.execution.ExecutionContext;
 import graphql.execution.instrumentation.Instrumentation;
+import graphql.execution.instrumentation.InstrumentationState;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLFieldDefinition;
 
@@ -15,6 +16,25 @@ public class InstrumentationFieldFetchParameters extends InstrumentationFieldPar
         super(getExecutionContext, fieldDef, environment.getFieldTypeInfo());
         this.environment = environment;
     }
+
+    private InstrumentationFieldFetchParameters(ExecutionContext getExecutionContext, GraphQLFieldDefinition fieldDef, DataFetchingEnvironment environment, InstrumentationState instrumentationState) {
+        super(getExecutionContext, fieldDef, environment.getFieldTypeInfo(), instrumentationState);
+        this.environment = environment;
+    }
+
+    /**
+     * Returns a cloned parameters object with the new state
+     *
+     * @param instrumentationState the new state for this parameters object
+     *
+     * @return a new parameters object with the new state
+     */
+    public InstrumentationFieldFetchParameters withNewState(InstrumentationState instrumentationState) {
+        return new InstrumentationFieldFetchParameters(
+                this.getExecutionContext(), this.getField(), this.getEnvironment(),
+                instrumentationState);
+    }
+
 
     public DataFetchingEnvironment getEnvironment() {
         return environment;
