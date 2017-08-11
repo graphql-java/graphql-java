0a5fabf51700d2b902ea8def4af223333828c4d0
jameskleeh <james.kleeh@gmail.com>
2017-08-11 01:48:39 -0400

    Improve error messages for arguments that fail validation

--- src/main/java/graphql/validation/ValidationUtil.java
+++ src/main/java/graphql/validation/ValidationUtil.java
@@ -6,7 +6,11 @@ import graphql.language.*;
 import graphql.schema.*;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ValidationUtil {
 
@@ -21,12 +25,29 @@ public class ValidationUtil {
         throw new ShouldNotHappenException();
     }
 
+    protected void handleNullError(Value value, GraphQLType type) {}
+
+    protected void handleScalarError(Value value, GraphQLScalarType type) {}
+
+    protected void handleEnumError(Value value, GraphQLEnumType type) {}
+
+    protected void handleNotObjectError(Value value, GraphQLInputObjectType type) {}
+
+    protected void handleMissingFieldsError(Value value, GraphQLInputObjectType type, Set<String> missingFields) {}
+
+    protected void handleExtraFieldError(Value value, GraphQLInputObjectType type, ObjectField objectField) {}
+
+    protected void handleFieldNotValidError(ObjectField objectField, GraphQLInputObjectType type) {}
+
+    protected void handleFieldNotValidError(Value value, GraphQLType type, int index) {}
+
     public boolean isValidLiteralValue(Value value, GraphQLType type) {
-        if (value == null) {
-            return !(type instanceof GraphQLNonNull);
-        }
-        if (value instanceof NullValue) {
-            return !(type instanceof GraphQLNonNull);
+        if (value == null || value instanceof NullValue) {
+            boolean valid = !(type instanceof GraphQLNonNull);
+            if (!valid) {
+                handleNullError(value, type);
+            }
+            return valid;
         }
         if (value instanceof VariableReference) {
             return true;
@@ -36,10 +57,18 @@ public class ValidationUtil {
         }
 
         if (type instanceof GraphQLScalarType) {
-            return ((GraphQLScalarType) type).getCoercing().parseLiteral(value) != null;
+            boolean valid = ((GraphQLScalarType) type).getCoercing().parseLiteral(value) != null;
+            if (!valid) {
+                handleScalarError(value, (GraphQLScalarType) type);
+            }
+            return valid;
         }
         if (type instanceof GraphQLEnumType) {
-            return ((GraphQLEnumType) type).getCoercing().parseLiteral(value) != null;
+            boolean valid = ((GraphQLEnumType) type).getCoercing().parseLiteral(value) != null;
+            if (!valid) {
+                handleEnumError(value, (GraphQLEnumType) type);
+            }
+            return valid;
         }
 
         if (type instanceof GraphQLList) {
@@ -53,27 +82,40 @@ public class ValidationUtil {
     }
 
     private boolean isValidLiteralValue(Value value, GraphQLInputObjectType type) {
-        if (!(value instanceof ObjectValue)) return false;
+        if (!(value instanceof ObjectValue)) {
+            handleNotObjectError(value, type);
+            return false;
+        }
         ObjectValue objectValue = (ObjectValue) value;
         Map<String, ObjectField> objectFieldMap = fieldMap(objectValue);
 
-        if (isFieldMissing(type, objectFieldMap)) return false;
+        Set<String> missingFields = getMissingFields(type, objectFieldMap);
+        if (!missingFields.isEmpty()) {
+            handleMissingFieldsError(value, type, missingFields);
+            return false;
+        }
 
         for (ObjectField objectField : objectValue.getObjectFields()) {
             GraphQLInputObjectField inputObjectField = type.getField(objectField.getName());
-            if (inputObjectField == null) return false;
-            if (!isValidLiteralValue(objectField.getValue(), inputObjectField.getType())) return false;
+            if (inputObjectField == null) {
+                handleExtraFieldError(value, type, objectField);
+                return false;
+            }
+            if (!isValidLiteralValue(objectField.getValue(), inputObjectField.getType())) {
+                handleFieldNotValidError(objectField, type);
+                return false;
+            }
 
         }
         return true;
     }
 
-    private boolean isFieldMissing(GraphQLInputObjectType type, Map<String, ObjectField> objectFieldMap) {
-        for (GraphQLInputObjectField inputObjectField : type.getFields()) {
-            if (!objectFieldMap.containsKey(inputObjectField.getName()) &&
-                    (inputObjectField.getType() instanceof GraphQLNonNull)) return true;
-        }
-        return false;
+    private Set<String> getMissingFields(GraphQLInputObjectType type, Map<String, ObjectField> objectFieldMap) {
+        return type.getFields().stream()
+                .filter(field -> field.getType() instanceof GraphQLNonNull)
+                .map(GraphQLInputObjectField::getName)
+                .filter(((Predicate<String>) objectFieldMap::containsKey).negate())
+                .collect(Collectors.toSet());
     }
 
     private Map<String, ObjectField> fieldMap(ObjectValue objectValue) {
@@ -87,8 +129,12 @@ public class ValidationUtil {
     private boolean isValidLiteralValue(Value value, GraphQLList type) {
         GraphQLType wrappedType = type.getWrappedType();
         if (value instanceof ArrayValue) {
-            for (Value innerValue : ((ArrayValue) value).getValues()) {
-                if (!isValidLiteralValue(innerValue, wrappedType)) return false;
+            List<Value> values = ((ArrayValue) value).getValues();
+            for (int i = 0; i < values.size(); i++) {
+                if (!isValidLiteralValue(values.get(i), wrappedType)) {
+                    handleFieldNotValidError(values.get(i), wrappedType, i);
+                    return false;
+                }
             }
             return true;
         } else {
