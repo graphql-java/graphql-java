02f34e6f870395332bb3c1819b0f3914f76e0a3a
Andreas Marek <andimarek@fastmail.fm>
2017-08-27 23:52:53 +0200

    rename exception (#653)

---
+++ src/main/java/graphql/execution/batched/BatchAssertionFailed.java
@@ -0,0 +1,22 @@
+package graphql.execution.batched;
+
+import graphql.GraphQLException;
+
+
+public class BatchAssertionFailed extends GraphQLException {
+    public BatchAssertionFailed() {
+        super();
+    }
+
+    public BatchAssertionFailed(String message) {
+        super(message);
+    }
+
+    public BatchAssertionFailed(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BatchAssertionFailed(Throwable cause) {
+        super(cause);
+    }
+}
