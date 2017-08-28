be3d9a8a2e3ffef27eea0024f0765aa639af497c
andimarek <andimarek@fastmail.fm>
2017-08-28 03:48:25 +0200

    add max query complexity instrumentation poc

---
+++ src/test/groovy/graphql/analysis/MaxQueryComplexityInstrumentationTest.groovy
@@ -0,0 +1,47 @@
+package graphql.analysis
+
+import graphql.ExecutionInput
+import graphql.TestUtil
+import graphql.execution.AbortExecutionException
+import graphql.execution.instrumentation.InstrumentationContext
+import graphql.execution.instrumentation.parameters.InstrumentationValidationParameters
+import graphql.language.Document
+import graphql.parser.Parser
+import spock.lang.Specification
+
+class MaxQueryComplexityInstrumentationTest extends Specification {
+
+    Document createQuery(String query) {
+        Parser parser = new Parser()
+        parser.parseDocument(query)
+    }
+
+
+    def "throws exception"() {
+        given:
+        def schema = TestUtil.schema("""
+            type Query{
+                foo: Foo
+                bar: String
+            }
+            type Foo {
+                scalar: String  
+                foo: Foo
+            }
+        """)
+        def query = createQuery("""
+            {f2: foo {scalar foo{scalar}} f1: foo { foo {foo {foo {foo{foo{scalar}}}}}} }
+            """)
+        MaxQueryComplexityInstrumentation queryComplexityInstrumentation = new MaxQueryComplexityInstrumentation(10)
+        ExecutionInput executionInput = Mock(ExecutionInput)
+        InstrumentationValidationParameters validationParameters = new InstrumentationValidationParameters(executionInput, query, schema, null);
+        InstrumentationContext instrumentationContext = queryComplexityInstrumentation.beginValidation(validationParameters)
+        when:
+        instrumentationContext.onEnd(null, null)
+        then:
+        def e = thrown(AbortExecutionException)
+        e.message == "maximum query complexity exceeded 11 > 10"
+
+    }
+}
+
