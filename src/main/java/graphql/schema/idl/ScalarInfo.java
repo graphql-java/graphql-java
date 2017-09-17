2d35fca536e8db6e4109028319d42efd89fd74ec
Brad Baker <bbakerman@gmail.com>
2017-09-16 21:39:54 -0700

    Henryqdineen built in scalars (#711)
    * omit only built-in Scalars from GraphQL spec from SchemaPrinter
    
    * Updated the PR so that the name is more explicit
    
    * Updated the PR so that the name is more explicit and also added more tests

--- src/main/java/graphql/schema/idl/ScalarInfo.java
+++ src/main/java/graphql/schema/idl/ScalarInfo.java
@@ -14,16 +14,27 @@ import java.util.Map;
  */
 public class ScalarInfo {
     /**
-     * A list of the scalar types provided by graphql
+     * A list of the scalar types provided by graphql-java
      */
     public static final List<GraphQLScalarType> STANDARD_SCALARS = new ArrayList<>();
 
     /**
-     * A map of scalar type definitions provided by graphql
+     * A list of the built-in scalar types as defined by the graphql specification
+     */
+    public static final List<GraphQLScalarType> GRAPHQL_SPECIFICATION_SCALARS = new ArrayList<>();
+
+    /**
+     * A map of scalar type definitions provided by graphql-java
      */
     public static final Map<String, ScalarTypeDefinition> STANDARD_SCALAR_DEFINITIONS = new LinkedHashMap<>();
 
     static {
+        GRAPHQL_SPECIFICATION_SCALARS.add(Scalars.GraphQLInt);
+        GRAPHQL_SPECIFICATION_SCALARS.add(Scalars.GraphQLFloat);
+        GRAPHQL_SPECIFICATION_SCALARS.add(Scalars.GraphQLString);
+        GRAPHQL_SPECIFICATION_SCALARS.add(Scalars.GraphQLBoolean);
+        GRAPHQL_SPECIFICATION_SCALARS.add(Scalars.GraphQLID);
+
         STANDARD_SCALARS.add(Scalars.GraphQLInt);
         STANDARD_SCALARS.add(Scalars.GraphQLFloat);
         STANDARD_SCALARS.add(Scalars.GraphQLString);
@@ -55,16 +66,26 @@ public class ScalarInfo {
 
     }
 
-
     /**
-     * Returns true if the scalar type is a standard one provided by graphql
+     * Returns true if the scalar type is a standard one provided by graphql-java
      *
      * @param scalarType the type in question
      *
-     * @return true if the scalar type is a standard one provided by graphql
+     * @return true if the scalar type is a graphql-java provided scalar
      */
     public static boolean isStandardScalar(GraphQLScalarType scalarType) {
         return STANDARD_SCALARS.stream().anyMatch(sc -> sc.getName().equals(scalarType.getName()));
+    }
+
+    /**
+     * Returns true if the scalar type is a scalar that is specified by the graphql specification
+     *
+     * @param scalarType the type in question
+     *
+     * @return true if the scalar type is is specified by the graphql specification
+     */
+    public static boolean isGraphqlSpecifiedScalar(GraphQLScalarType scalarType) {
+        return GRAPHQL_SPECIFICATION_SCALARS.stream().anyMatch(sc -> sc.getName().equals(scalarType.getName()));
     }
 
 }
