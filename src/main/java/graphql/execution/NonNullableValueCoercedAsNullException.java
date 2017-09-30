7edff4907d348f2a679477db00cca5c4607e19f3
Brad Baker <bbakerman@gmail.com>
2017-09-30 11:27:27 +1000

    Fixed the naming of the coerced exceptions (#732)

--- src/main/java/graphql/execution/NonNullableValueCoercedAsNullException.java
+++ src/main/java/graphql/execution/NonNullableValueCoercedAsNullException.java
@@ -1,16 +1,26 @@
 package graphql.execution;
 
 import graphql.GraphQLException;
+import graphql.language.VariableDefinition;
+import graphql.schema.GraphQLInputObjectField;
 import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeUtil;
+
+import static java.lang.String.format;
 
 /**
  * This is thrown if a non nullable value is coerced to a null value
  */
 public class NonNullableValueCoercedAsNullException extends GraphQLException {
 
-    public NonNullableValueCoercedAsNullException(GraphQLType graphQLType) {
-        super(String.format("Null value for NonNull type '%s", GraphQLTypeUtil.getUnwrappedTypeName(graphQLType)));
+    public NonNullableValueCoercedAsNullException(VariableDefinition variableDefinition, GraphQLType graphQLType) {
+        super(format("Variable '%s' has coerced Null value for NonNull type '%s'",
+                variableDefinition.getName(), GraphQLTypeUtil.getUnwrappedTypeName(graphQLType)));
+    }
+
+    public NonNullableValueCoercedAsNullException(GraphQLInputObjectField inputTypeField) {
+        super(format("Input field '%s' has coerced Null value for NonNull type '%s'",
+                inputTypeField.getName(), GraphQLTypeUtil.getUnwrappedTypeName(inputTypeField.getType())));
     }
 
 }
