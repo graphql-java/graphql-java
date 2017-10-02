92468df35c1bf2d71630c1677867fa9451b01a5e
Brad Baker <bbakerman@gmail.com>
2017-10-03 07:43:39 +1100

    Added schema / introspection diff support (#745)
    * Added schema / introspection diff support
    
    * Moved package to a more sensible one

---
+++ src/main/java/graphql/schema/diff/reporting/CapturingReporter.java
@@ -0,0 +1,54 @@
+package graphql.schema.diff.reporting;
+
+import graphql.PublicApi;
+import graphql.schema.diff.DiffEvent;
+import graphql.schema.diff.DiffLevel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A reporter that captures all the difference events as they occur
+ */
+@PublicApi
+public class CapturingReporter implements DifferenceReporter {
+    private final List<DiffEvent> events = new ArrayList<>();
+    private final List<DiffEvent> breakages = new ArrayList<>();
+    private final List<DiffEvent> dangers = new ArrayList<>();
+
+    @Override
+    public void report(DiffEvent differenceEvent) {
+        events.add(differenceEvent);
+        if (differenceEvent.getLevel() == DiffLevel.BREAKING) {
+            breakages.add(differenceEvent);
+        }
+        if (differenceEvent.getLevel() == DiffLevel.DANGEROUS) {
+            dangers.add(differenceEvent);
+        }
+    }
+
+    @Override
+    public void onEnd() {
+    }
+
+    public List<DiffEvent> getEvents() {
+        return new ArrayList<>(events);
+    }
+
+    public List<DiffEvent> getBreakages() {
+        return new ArrayList<>(breakages);
+    }
+
+    public List<DiffEvent> getDangers() {
+        return new ArrayList<>(dangers);
+    }
+
+    public int getBreakageCount() {
+        return breakages.size();
+    }
+
+    public int getDangerCount() {
+        return dangers.size();
+    }
+
+}
