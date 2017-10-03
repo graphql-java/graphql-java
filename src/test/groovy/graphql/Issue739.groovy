158c48d1a5881c79c1a53e62c0d9fe1fc0a3aab1
Brad Baker <bbaker@atlassian.com>
2017-10-03 18:24:12 +1100

    This is the test ported into groovy - it is indeed failing

---
+++ src/test/groovy/graphql/Issue739.groovy
@@ -0,0 +1,90 @@
+package graphql
+
+import graphql.schema.GraphQLObjectType
+import graphql.schema.idl.RuntimeWiring
+import spock.lang.Specification
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring
+
+class Issue739 extends Specification {
+
+    def "#739 test"() {
+
+        when:
+
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                .dataFetcher("foo",
+                { env ->
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", "abc")
+                    return map;
+
+                })
+                .dataFetcher("bar",
+                { env ->
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", "def")
+                    return map;
+                })
+        )
+                .type(newTypeWiring("Node")
+                .typeResolver({ env -> (GraphQLObjectType) env.getSchema().getType("Foo") }))
+                .build()
+
+
+        def schema = TestUtil.schema("""
+            schema {
+              query: Query
+            }
+            
+            type Query {
+              foo: Node
+              bar(input: BarInput!): Node
+            }
+            
+            input BarInput {
+              baz: String!
+            }
+            
+            interface Node {
+              id: String
+            }
+            
+            type Foo implements Node {
+              id: String
+            }
+        """, runtimeWiring)
+
+
+        GraphQL graphQL = GraphQL.newGraphQL(schema)
+                .build()
+
+        ExecutionInput noVarInput = ExecutionInput.newExecutionInput()
+                .query('{ bar(input: 123) { id } } ')
+                .build()
+
+        ExecutionResult noVarResult = graphQL
+                .executeAsync(noVarInput)
+                .join()
+
+        then:
+
+        1 == noVarResult.getErrors().size()
+
+        when:
+        def variables = ["input": 123]
+
+        ExecutionInput varInput = ExecutionInput.newExecutionInput()
+                .query('query Bar($input: BarInput!) {bar(input: $input) {id}}')
+                .variables(variables)
+                .build();
+
+        ExecutionResult varResult = graphQL
+                .executeAsync(varInput)
+                .join()
+
+        then:
+        1 == varResult.getErrors().size()
+    }
+}
