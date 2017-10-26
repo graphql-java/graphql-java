3fc7f4f88caa5af8cac1d3019d0aab23c97bb225
James Baker <j_d_baker@hotmail.com>
2017-10-27 00:34:15 +0100

    Make the fields of AsynchronousDataFetcher final

--- src/main/java/graphql/schema/AsynchronousDataFetcher.java
+++ src/main/java/graphql/schema/AsynchronousDataFetcher.java
@@ -17,38 +17,48 @@ public class AsynchronousDataFetcher<T> implements DataFetcher<CompletableFuture
     /**
      * A factory method for creating asynchronous data fetchers so that when used with 
      * static imports allows more readable code such as:
+     * <p>
      * {@code .dataFetcher(async(fooDataFetcher))}
-     * <br><br>
+     * <p>
      * By default this will run in the {@link ForkJoinPool#commonPool()}. You can set 
-     * your own {@link Executor} with {@link #executeIn(Executor)}
+     * your own {@link Executor} with {@link #asyncWithExecutor(DataFetcher, Executor)}
      *
      * @param wrappedDataFetcher the data fetcher to run asynchronously
      *
-     * @return a DataFetcher that will run the wrappedDataFetcher asynchronously
+     * @return a {@link DataFetcher} that will run the wrappedDataFetcher asynchronously
      */
     public static <T> AsynchronousDataFetcher<T> async(DataFetcher<T> wrappedDataFetcher) {
         return new AsynchronousDataFetcher<>(wrappedDataFetcher);
     }
     
-    private final DataFetcher<T> wrappedDataFetcher;
-    private Executor executor = ForkJoinPool.commonPool();
-
-    public AsynchronousDataFetcher(DataFetcher<T> wrappedDataFetcher) {
-        assertNotNull(wrappedDataFetcher, "wrappedDataFetcher can't be null");
-        this.wrappedDataFetcher = wrappedDataFetcher;
+    /**
+     * A factory method for creating asynchronous data fetchers and setting the 
+     * {@link Executor} they run in so that when used with static imports allows 
+     * more readable code such as:
+     * <p>
+     * {@code .dataFetcher(asyncWithExecutor(fooDataFetcher, fooPool))}
+     *
+     * @param wrappedDataFetcher the data fetcher to run asynchronously
+     * @param executor to run the asynchronous data fetcher in
+     *
+     * @return a {@link DataFetcher} that will run the wrappedDataFetcher asynchronously in 
+     * the given {@link Executor}
+     */
+    public static <T> AsynchronousDataFetcher<T> asyncWithExecutor(DataFetcher<T> wrappedDataFetcher,
+            Executor executor) {
+        return new AsynchronousDataFetcher<>(wrappedDataFetcher, executor);
     }
     
-    /**
-     * This allows you to set the {@link Executor} that this {@link DataFetcher}
-     * will run in
-     * 
-     * @param executor the {@link Executor} to run the asynchronous data fetcher in
-     * 
-     * @return a reference to this object
-     */
-    public AsynchronousDataFetcher<T> executeIn(Executor executor) {
+    private final DataFetcher<T> wrappedDataFetcher;
+    private final Executor executor;
+
+    public AsynchronousDataFetcher(DataFetcher<T> wrappedDataFetcher) {
+        this(wrappedDataFetcher, ForkJoinPool.commonPool());
+    }
+
+    public AsynchronousDataFetcher(DataFetcher<T> wrappedDataFetcher, Executor executor) {
+        this.wrappedDataFetcher = assertNotNull(wrappedDataFetcher, "wrappedDataFetcher can't be null");
         this.executor = assertNotNull(executor, "executor can't be null");
-        return this;
     }
 
     @Override
