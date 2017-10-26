0a399dadd7e2f51bc8478cb42b289f31e79da6f1
James Baker <j_d_baker@hotmail.com>
2017-10-26 09:33:04 +0100

    Add the ability to set the executor an async data fetcher will run in

--- src/main/java/graphql/schema/AsynchronousDataFetcher.java
+++ src/main/java/graphql/schema/AsynchronousDataFetcher.java
@@ -3,19 +3,24 @@ package graphql.schema;
 import static graphql.Assert.assertNotNull;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
 
 import graphql.PublicApi;
 
 /**
- * A modified type for that indicates the underlying data fetcher is run asynchronously.
+ * A modifier type that indicates the underlying data fetcher is run asynchronously
  */
 @PublicApi
 public class AsynchronousDataFetcher<T> implements DataFetcher<CompletableFuture<T>> {
     
     /**
      * A factory method for creating asynchronous data fetchers so that when used with 
-     * static imports allows more readable code such as
-     * {@code .dataFetcher(async(fooDataFetcher)) }
+     * static imports allows more readable code such as:
+     * {@code .dataFetcher(async(fooDataFetcher))}
+     * <br><br>
+     * By default this will run in the {@link ForkJoinPool#commonPool()}. You can set 
+     * your own {@link Executor} with {@link #executeIn(Executor)}
      *
      * @param wrappedDataFetcher the data fetcher to run asynchronously
      *
@@ -26,15 +31,29 @@ public class AsynchronousDataFetcher<T> implements DataFetcher<CompletableFuture
     }
     
     private final DataFetcher<T> wrappedDataFetcher;
+    private Executor executor = ForkJoinPool.commonPool();
 
     public AsynchronousDataFetcher(DataFetcher<T> wrappedDataFetcher) {
         assertNotNull(wrappedDataFetcher, "wrappedDataFetcher can't be null");
         this.wrappedDataFetcher = wrappedDataFetcher;
     }
+    
+    /**
+     * This allows you to set the {@link Executor} that this {@link DataFetcher}
+     * will run in
+     * 
+     * @param executor the {@link Executor} to run the asynchronous data fetcher in
+     * 
+     * @return a reference to this object
+     */
+    public AsynchronousDataFetcher<T> executeIn(Executor executor) {
+        this.executor = assertNotNull(executor, "executor can't be null");
+        return this;
+    }
 
     @Override
     public CompletableFuture<T> get(DataFetchingEnvironment environment) {
-        return CompletableFuture.supplyAsync(() -> wrappedDataFetcher.get(environment));
+        return CompletableFuture.supplyAsync(() -> wrappedDataFetcher.get(environment), executor);
     }
 
 }
