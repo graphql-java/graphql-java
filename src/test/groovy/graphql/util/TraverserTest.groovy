c602e1d82405bb1d87dc2e48afac9bf12804182f
andimarek <andimarek@fastmail.fm>
2018-03-06 07:01:44 +1100

    make test more groovy

--- src/test/groovy/graphql/util/TraverserTest.groovy
+++ src/test/groovy/graphql/util/TraverserTest.groovy
@@ -3,35 +3,34 @@ package graphql.util
 import spock.lang.Specification
 
 class TraverserTest extends Specification {
+
     class Node {
-        int number;
-        List<Node> children = Collections.emptyList();
+        int number
+        List<Node> children = Collections.emptyList()
     }
 
     def "test pre-order depth-first traversal"() {
         given:
-            Node root = new Node(number: 0, children: [
+        Node root = new Node(number: 0, children: [
                 new Node(number: 1, children: [
-                    new Node(number: 3)
+                        new Node(number: 3)
                 ]),
                 new Node(number: 2, children: [
-                    new Node(number: 4),
-                    new Node(number: 5)
+                        new Node(number: 4),
+                        new Node(number: 5)
                 ])
-            ])
-        def initialData = new ArrayList();
+        ])
+        def initialData = []
+        def visitor = [
+                enter: { TraverserContext<Node> context ->
+                    context.getInitialData().add(context.thisNode().number)
+                    TraversalControl.CONTINUE
+                },
+                leave: { TraversalControl.CONTINUE }
+        ] as TraverserVisitor<Node>
         when:
-        Traverser.depthFirst({ Node n -> n.children }, initialData)
-                .traverse(root, new TraverserVisitor<Node>() {
-            public TraversalControl enter(TraverserContext<? super Node> context) {
-                context.getInitialData().add(context.thisNode().number);
-                return TraversalControl.CONTINUE;
-                    }
+        Traverser.depthFirst({ Node n -> n.children }, initialData).traverse(root, visitor)
 
-            public TraversalControl leave(TraverserContext<? super Node> context) {
-                return TraversalControl.CONTINUE;
-                    }
-                })
 
         then:
         initialData == [0, 1, 3, 2, 4, 5]
@@ -39,28 +38,27 @@ class TraverserTest extends Specification {
 
     def "test post-order depth-first traversal"() {
         given:
-            Node root = new Node(number: 0, children: [
+        Node root = new Node(number: 0, children: [
                 new Node(number: 1, children: [
-                    new Node(number: 3)
+                        new Node(number: 3)
                 ]),
                 new Node(number: 2, children: [
-                    new Node(number: 4),
-                    new Node(number: 5)
+                        new Node(number: 4),
+                        new Node(number: 5)
                 ])
-            ])
+        ])
         def initialData = new ArrayList();
+        def visitor = [
+                enter: { TraverserContext<Node> context ->
+                    TraversalControl.CONTINUE
+                },
+                leave: { TraverserContext<Node> context ->
+                    context.getInitialData().add(context.thisNode().number)
+                    TraversalControl.CONTINUE
+                }
+        ] as TraverserVisitor<Node>
         when:
-        Traverser.depthFirst({ Node n -> n.children }, initialData)
-                .traverse(root, new TraverserVisitor<Node>() {
-            public TraversalControl enter(TraverserContext<? super Node> context) {
-                return TraversalControl.CONTINUE
-                    }
-
-            public TraversalControl leave(TraverserContext<? super Node> context) {
-                context.getInitialData().add(context.thisNode().number);
-                return TraversalControl.CONTINUE;
-                    }
-                })
+        Traverser.depthFirst({ Node n -> n.children }, initialData).traverse(root, visitor)
 
         then:
         initialData == [3, 1, 4, 5, 2, 0]
@@ -68,31 +66,31 @@ class TraverserTest extends Specification {
 
     def "test breadth-first traversal"() {
         given:
-            Node root = new Node(number: 0, children: [
+        Node root = new Node(number: 0, children: [
                 new Node(number: 1, children: [
-                    new Node(number: 3)
+                        new Node(number: 3)
                 ]),
                 new Node(number: 2, children: [
-                    new Node(number: 4),
-                    new Node(number: 5)
+                        new Node(number: 4),
+                        new Node(number: 5)
                 ])
-            ])
+        ])
         def initialData = new ArrayList()
+        def visitor = [
+                enter: { TraverserContext<Node> context ->
+                    context.getInitialData().add(context.thisNode().number)
+                    TraversalControl.CONTINUE
+                },
+                leave: {
+                    TraversalControl.CONTINUE
+                }
+        ] as TraverserVisitor<Node>
         when:
-        Traverser.breadthFirst({ Node n -> n.children }, initialData)
-                .traverse(root, new TraverserVisitor<Node>() {
-            public TraversalControl enter(TraverserContext<? super Node> context) {
-                context.getInitialData().add(context.thisNode().number);
-                return TraversalControl.CONTINUE;
-                    }
-
-            public TraversalControl leave(TraverserContext<? super Node> context) {
-                return TraversalControl.CONTINUE;
-                    }
-                })
+        Traverser.breadthFirst({ Node n -> n.children }, initialData).traverse(root, visitor)
 
         then:
         initialData == [0, 1, 2, 3, 4, 5]
     }
+
 }
 
