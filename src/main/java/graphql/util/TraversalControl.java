4628a7d7ee56485f91a01a9490ecaf1cfd80a3a6
andimarek <andimarek@fastmail.fm>
2018-03-10 07:53:12 +1100

    doc

--- src/main/java/graphql/util/TraversalControl.java
+++ src/main/java/graphql/util/TraversalControl.java
@@ -14,7 +14,9 @@ public enum TraversalControl {
      */
     QUIT,
     /**
-     * When returned from a Visitor's method, indicates skipping traversal of a subtree
+     * When returned from a Visitor's method, indicates skipping traversal of a subtree.
+     * When turned from the 'leave' method it is the same as CONTINUE, because the subtree is already
+     * visited
      */
-    ABORT,
+    ABORT
 }
