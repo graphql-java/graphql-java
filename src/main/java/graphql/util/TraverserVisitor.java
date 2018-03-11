818abd359704ddd4f5aa1c52a4fcbe952b9615c3
andimarek <andimarek@fastmail.fm>
2018-03-11 11:12:31 +1100

    introduced TraverserResult, more testing

--- src/main/java/graphql/util/TraverserVisitor.java
+++ src/main/java/graphql/util/TraverserVisitor.java
@@ -2,42 +2,15 @@ package graphql.util;
 
 import graphql.Internal;
 
-/**
- * Visitor interface that get's notified as the Traverser traverses a tree
- *
- * @param <T> type of tree nodes
- */
 @Internal
 public interface TraverserVisitor<T> {
-    /**
-     * Notification that a traverser starts "visiting" a tree node
-     *
-     * @param context traverser context
-     *
-     * @see TraverserContext
-     */
+
     TraversalControl enter(TraverserContext<T> context);
 
-    /**
-     * Notification that a traverser finishes "visiting" a tree node
-     *
-     * @param context traverser context
-     *
-     * @see TraverserContext
-     */
     TraversalControl leave(TraverserContext<T> context);
 
-    /**
-     * Notification that a traverser visits a node it has already visited
-     * This happens in cyclic graphs and the traversal does not traverse this
-     * node again to prevent infinite recursion
-     *
-     * @param context traverser context
-     *
-     * @see TraverserContext
-     */
-    default void backRef(TraverserContext<T> context) {
-
+    default TraversalControl backRef(TraverserContext<T> context) {
+        return TraversalControl.CONTINUE;
     }
 
 }
