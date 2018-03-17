8867d3a9333735a481c481a086e83298ccdf1182
andimarek <andimarek@fastmail.fm>
2018-03-17 20:48:39 +1100

    tests

--- src/test/groovy/graphql/language/NodeVisitorStubTest.groovy
+++ src/test/groovy/graphql/language/NodeVisitorStubTest.groovy
@@ -10,103 +10,48 @@ import static java.util.Collections.emptyList
 class NodeVisitorStubTest extends Specification {
 
 
-    def "selection nodes call visitSelection by default"() {
+    @Unroll
+    def "#visitMethod call visitSelection by default"() {
         given:
         NodeVisitorStub nodeVisitorStub = Spy(NodeVisitorStub, constructorArgs: [])
-        Field field = new Field()
-        FragmentSpread fragmentSpread = new FragmentSpread()
-        InlineFragment inlineFragment = new InlineFragment()
         TraverserContext context = Mock(TraverserContext)
 
         when:
-        def control = nodeVisitorStub.visitField(field, context)
-
+        def control = nodeVisitorStub."$visitMethod"(node, context)
         then:
-        1 * nodeVisitorStub.visitSelection(field, context) >> TraversalControl.QUIT
+        1 * nodeVisitorStub.visitSelection(node, context) >> TraversalControl.QUIT
         control == TraversalControl.QUIT
 
-        when:
-        control = nodeVisitorStub.visitInlineFragment(inlineFragment, context)
+        where:
+        node                 | visitMethod
+        new Field()          | 'visitField'
+        new FragmentSpread() | 'visitFragmentSpread'
+        new InlineFragment() | 'visitInlineFragment'
 
-        then:
-        1 * nodeVisitorStub.visitSelection(inlineFragment, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitFragmentSpread(fragmentSpread, context)
-
-        then:
-        1 * nodeVisitorStub.visitSelection(fragmentSpread, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
     }
 
     def "values nodes call visitValue by default"() {
         given:
         NodeVisitorStub nodeVisitorStub = Spy(NodeVisitorStub, constructorArgs: [])
-        FloatValue floatValue = new FloatValue()
-        ArrayValue arrayValue = new ArrayValue()
-        IntValue intValue = new IntValue()
-        BooleanValue booleanValue = new BooleanValue(true)
-        NullValue nullValue = NullValue.Null
-        ObjectValue objectValue = new ObjectValue()
-        VariableReference variableReference = new VariableReference()
-        EnumValue enumValue = new EnumValue()
-        StringValue stringValue = new StringValue()
         TraverserContext context = Mock(TraverserContext)
 
         when:
-        def control = nodeVisitorStub.visitFloatValue(floatValue, context)
+        def control = nodeVisitorStub."$visitMethod"(node, context)
         then:
-        1 * nodeVisitorStub.visitValue(floatValue, context) >> TraversalControl.QUIT
+        1 * nodeVisitorStub.visitValue(node, context) >> TraversalControl.QUIT
         control == TraversalControl.QUIT
 
-        when:
-        control = nodeVisitorStub.visitArrayValue(arrayValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(arrayValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitIntValue(intValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(intValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitBooleanValue(booleanValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(booleanValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitNullValue(nullValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(nullValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitObjectValue(objectValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(objectValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitVariableReference(variableReference, context)
-        then:
-        1 * nodeVisitorStub.visitValue(variableReference, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitEnumValue(enumValue, context)
-        then:
-        1 * nodeVisitorStub.visitValue(enumValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
-
-        when:
-        control = nodeVisitorStub.visitStringValue(stringValue, context)
-        then:
-        1 * nodeVisitorStub.visitStringValue(stringValue, context) >> TraversalControl.QUIT
-        control == TraversalControl.QUIT
+        where:
+        node                    | visitMethod
+        new FloatValue()        | 'visitFloatValue'
+        new ArrayValue()        | 'visitArrayValue'
+        new IntValue()          | 'visitIntValue'
+        new BooleanValue(true)  | 'visitBooleanValue'
+        NullValue.Null          | 'visitNullValue'
+        new ObjectValue()       | 'visitObjectValue'
+        new VariableReference() | 'visitVariableReference'
+        new EnumValue()         | 'visitEnumValue'
+        new StringValue()       | 'visitStringValue'
     }
 
     def "definitions call visitDefinition by default"() {
