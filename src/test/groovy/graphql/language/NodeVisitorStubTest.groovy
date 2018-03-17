f697a5ea8b8f88d1c474e915d15dd6731472a1b6
andimarek <andimarek@fastmail.fm>
2018-03-17 20:40:46 +1100

    tests

--- src/test/groovy/graphql/language/NodeVisitorStubTest.groovy
+++ src/test/groovy/graphql/language/NodeVisitorStubTest.groovy
@@ -3,6 +3,9 @@ package graphql.language
 import graphql.util.TraversalControl
 import graphql.util.TraverserContext
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static java.util.Collections.emptyList
 
 class NodeVisitorStubTest extends Specification {
 
@@ -219,6 +222,41 @@ class NodeVisitorStubTest extends Specification {
         then:
         1 * nodeVisitorStub.visitType(typeName, context) >> TraversalControl.QUIT
         control == TraversalControl.QUIT
+    }
+
+    @Unroll
+    def "default for #visitMethod is to call visitNode"() {
+        given:
+        NodeVisitorStub nodeVisitorStub = Spy(NodeVisitorStub, constructorArgs: [])
+        TraverserContext context = Mock(TraverserContext)
+
+        when:
+        def control = nodeVisitorStub."$visitMethod"(node, context)
+        then:
+        1 * nodeVisitorStub.visitNode(node, context) >> TraversalControl.QUIT
+        control == TraversalControl.QUIT
+
+        where:
+        node                           | visitMethod
+        new Argument("", null)         | 'visitArgument'
+        new Directive("", emptyList()) | 'visitDirective'
+        new DirectiveLocation("")      | 'visitDirectiveLocation'
+        new Document()                 | 'visitDocument'
+        new EnumValueDefinition("")    | 'visitEnumValueDefinition'
+        new FieldDefinition("")        | 'visitFieldDefinition'
+        new InputValueDefinition("")   | 'visitInputValueDefinition'
+        new InputValueDefinition("")   | 'visitInputValueDefinition'
+        new ObjectField("", null)      | 'visitObjectField'
+        new OperationTypeDefinition()  | 'visitOperationTypeDefinition'
+        new OperationTypeDefinition()  | 'visitOperationTypeDefinition'
+        new SelectionSet()             | 'visitSelectionSet'
+        new VariableDefinition()       | 'visitVariableDefinition'
+        new StringValue("")            | 'visitValue'
+        new OperationDefinition()      | 'visitDefinition'
+        new UnionTypeDefinition("")    | 'visitTypeDefinition'
+        new Field()                    | 'visitSelection'
+        new NonNullType()              | 'visitType'
+
     }
 
 }
