399f3bfedeaf62a25adff1debea469b7bb62e10d
Brad Baker <bbaker@atlassian.com>
2018-03-21 11:53:15 +1100

    using idiomatic Java getters on ES parameters

--- src/main/java/graphql/util/TraverserVisitor.java
+++ src/main/java/graphql/util/TraverserVisitor.java
@@ -5,14 +5,23 @@ import graphql.Internal;
 @Internal
 public interface TraverserVisitor<T> {
 
+    /**
+     * @param context the context in place
+     *
+     * @return Any allowed control value
+     */
     TraversalControl enter(TraverserContext<T> context);
 
     /**
+     * @param context the context in place
+     *
      * @return Only Continue or Quit allowed
      */
     TraversalControl leave(TraverserContext<T> context);
 
     /**
+     * @param context the context in place
+     *
      * @return Only Continue or Quit allowed
      */
     default TraversalControl backRef(TraverserContext<T> context) {
