6828145e7e853a040f98a36edf064ffb18b5dde5
Samuel Le Berrigaud <sam@leberrigaud.org>
2018-04-04 10:48:38 +0200

    Add data loader dispatcher instrumentation copied from Braid, and update dataloader performance test to use it.
    Refactor according to PR feedback.

--- src/main/java/graphql/execution/instrumentation/dataloader/BraidDataLoaderDispatcherInstrumentation.java
+++ src/main/java/graphql/execution/instrumentation/dataloader/BraidDataLoaderDispatcherInstrumentation.java
@@ -16,10 +16,9 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static graphql.execution.instrumentation.SimpleInstrumentationContext.whenDispatched;
+import static graphql.util.FpKit.concat;
 import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
-import static java.util.concurrent.CompletableFuture.completedFuture;
 
 public final class BraidDataLoaderDispatcherInstrumentation extends SimpleInstrumentation {
     private static final Logger log = LoggerFactory.getLogger(BraidDataLoaderDispatcherInstrumentation.class);
@@ -37,76 +36,57 @@ public final class BraidDataLoaderDispatcherInstrumentation extends SimpleInstru
 
     private void dispatch() {
         log.debug("Dispatching all data loaders ({})", dataLoaderRegistry.getKeys());
-        final DispatchedBatchLoaders allDispatched = dataLoaderRegistry.getKeys().stream()
+        final CombinedDataLoaderDispatchCalls allDispatched = dataLoaderRegistry.getKeys().stream()
                 .map(key -> dispatchBatchLoader(dataLoaderRegistry, key))
-                .reduce(new DispatchedBatchLoaders(), DispatchedBatchLoaders::add, DispatchedBatchLoaders::combine);
+                .reduce(new CombinedDataLoaderDispatchCalls(), CombinedDataLoaderDispatchCalls::add, CombinedDataLoaderDispatchCalls::combine);
 
         if (allDispatched.depth > 0) {
             allDispatched.whenComplete(this::dispatch);
         }
     }
 
-    private DispatchedBatchLoader dispatchBatchLoader(DataLoaderRegistry dataLoaderRegistry, String key) {
+    private DataLoaderDispatchCall dispatchBatchLoader(DataLoaderRegistry dataLoaderRegistry, String key) {
         final DataLoader<Object, DataFetcherResult> dataLoader = dataLoaderRegistry.getDataLoader(key);
-        final int dispatchDepth = dataLoader.dispatchDepth();
-
-        return new DispatchedBatchLoader<>(key,
-                dispatchDepth,
-                dispatchDepth > 0 ? dataLoader.dispatch() : completedFuture(emptyList()));
+        return new DataLoaderDispatchCall<>(dataLoader.dispatchDepth(), dataLoader.dispatch());
     }
 
-    private static class DispatchedBatchLoader<V> {
-        private final String key;
+    private static class DataLoaderDispatchCall<V> {
         private final int depth;
         private final CompletableFuture<List<V>> futures;
 
-        private DispatchedBatchLoader(String key, int depth, CompletableFuture<List<V>> futures) {
-            this.key = key;
+        private DataLoaderDispatchCall(int depth, CompletableFuture<List<V>> futures) {
             this.depth = depth;
             this.futures = futures;
         }
     }
 
-    private static class DispatchedBatchLoaders {
+    private static class CombinedDataLoaderDispatchCalls {
         private final int depth;
         private final List<CompletableFuture<List<?>>> futures;
 
-        private DispatchedBatchLoaders() {
-            this(0);
+        private CombinedDataLoaderDispatchCalls() {
+            this(0, emptyList());
         }
 
-        private DispatchedBatchLoaders(int depth) {
-            this.depth = depth;
-            this.futures = new ArrayList<>();
-        }
-
-        private DispatchedBatchLoaders(int depth, List<CompletableFuture<List<?>>> futures) {
+        private CombinedDataLoaderDispatchCalls(int depth, List<CompletableFuture<List<?>>> futures) {
             this.depth = depth;
             this.futures = new ArrayList<>(futures);
         }
 
-        private DispatchedBatchLoaders add(DispatchedBatchLoader dbl) {
-            return new DispatchedBatchLoaders(this.depth + dbl.depth, concat(this.futures, dbl.futures));
+        @SuppressWarnings("unchecked")
+        private CombinedDataLoaderDispatchCalls add(DataLoaderDispatchCall ddc) {
+            return new CombinedDataLoaderDispatchCalls(this.depth + ddc.depth, concat(this.futures, ddc.futures));
         }
 
-        private static DispatchedBatchLoaders combine(DispatchedBatchLoaders ds1, DispatchedBatchLoaders ds2) {
-            return new DispatchedBatchLoaders(ds1.depth + ds2.depth, concat(ds1.futures, ds2.futures));
+        private static CombinedDataLoaderDispatchCalls combine(CombinedDataLoaderDispatchCalls cdldc1,
+                                                               CombinedDataLoaderDispatchCalls cdldc2) {
+            return new CombinedDataLoaderDispatchCalls(
+                    cdldc1.depth + cdldc2.depth,
+                    concat(cdldc1.futures, cdldc2.futures));
         }
 
         void whenComplete(Runnable run) {
             CompletableFutureKit.allOf(futures).whenComplete((__, ___) -> run.run());
         }
     }
-
-    static <T> List<T> concat(List<T> l, T t) {
-        return concat(l, singletonList(t));
-    }
-
-    static <T> List<T> concat(List<T> l1, List<T> l2) {
-        ArrayList<T> l = new ArrayList<>(l1);
-        l.addAll(l2);
-        l.trimToSize();
-        return l;
-    }
-
 }
