1648b4a96b0d9c44a68b4e46db4789f7a61a6fb8
Brad Baker <bbaker@atlassian.com>
2018-04-05 15:07:10 +1000

    Hmm it seems to be failing on travis - why??

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
-BUILD_COMMAND="./gradlew assemble && ./gradlew check"
+BUILD_COMMAND="./gradlew assemble && ./gradlew check --info"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "Building on master"
-    BUILD_COMMAND="./gradlew clean assemble && ./gradlew check && ./gradlew bintrayUpload -x check --info"
+    BUILD_COMMAND="./gradlew clean assemble && ./gradlew check  --info && ./gradlew bintrayUpload -x check --info"
 fi
 docker run -it --rm -v `pwd .`:/build  -e BINTRAY_USER=$BINTRAY_USER -e BINTRAY_API_KEY=$BINTRAY_API_KEY -w /build openjdk:8u131-jdk bash -c "${BUILD_COMMAND}"
