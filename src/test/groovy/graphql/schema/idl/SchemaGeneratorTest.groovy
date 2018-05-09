28e3d95b8d33b432b7c0e7ed76b20087c5b28e38
Brad Baker <bbaker@atlassian.com>
2018-05-09 10:36:43 +1000

    Fixed tests and found a bug in Introspection

--- src/test/groovy/graphql/schema/idl/SchemaGeneratorTest.groovy
+++ src/test/groovy/graphql/schema/idl/SchemaGeneratorTest.groovy
@@ -1481,14 +1481,15 @@ class SchemaGeneratorTest extends Specification {
         directive.getArgument("knownArg").defaultValue == "defaultValue"
     }
 
-    def "directives don't have to provide default values"() {
+    def "directive definitions don't have to provide default values"() {
         def spec = """
             directive @test1(include: Boolean!) on FIELD_DEFINITION
             
             directive @test2(include: Boolean!  = true) on FIELD_DEFINITION
             
             type Query {
-                f : String @test1
+                f1 : String @test1(include : false)
+                f2 : String @test2
             }
         """
 
@@ -1505,7 +1506,7 @@ class SchemaGeneratorTest extends Specification {
 
         def directiveTest2 = schema.getDirective("test2")
         directiveTest2.getArgument("include").type == GraphQLNonNull.nonNull(GraphQLBoolean)
-        directiveTest2.getArgument("include").value == null
+        directiveTest2.getArgument("include").value == true
         directiveTest2.getArgument("include").defaultValue == true
 
     }
