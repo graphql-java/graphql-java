ebdf796387ba177c9ce24523716db222b7e395df
Alex Matiushkin <exbeinfo@gmail.com>
2018-05-23 14:30:31 -0700

    Tests for Known directives validation rule (#1008)
    * Switch known directives to use visitor and add tests
    
    * Restore original directives

--- src/test/groovy/graphql/validation/rules/KnownDirectivesTest.groovy
+++ src/test/groovy/graphql/validation/rules/KnownDirectivesTest.groovy
@@ -62,5 +62,177 @@ class KnownDirectivesTest extends Specification {
 
     }
 
+    def "correct placed directive on mutation"() {
+        given:
+        def query = """
+          mutation Foo  {
+                name @include(if: true)
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
+
+    def "misplaced directive on mutation"() {
+        given:
+        def query = """
+          mutation Foo @include(if: true) {
+                name
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.containsValidationError(ValidationErrorType.MisplacedDirective)
+
+    }
+
+
+
+    def "bogus directive"() {
+        given:
+        def query = """
+          query Foo @helloWorld {
+                name
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.containsValidationError(ValidationErrorType.UnknownDirective)
+
+    }
+
+
+    def "misplaced directive on fragment definition"() {
+        given:
+        def query = """
+          fragment getName on Foo @include(if: true) {
+            name  
+          }
+          query Foo {
+                ...getName
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.containsValidationError(ValidationErrorType.MisplacedDirective)
+
+    }
+
+
+    def "directive on external fragment spread"() {
+        given:
+        def query = """
+          fragment getName on Foo {
+             ... @include(if: true) {
+                    name
+             }
+          }
+          query Foo {
+                ...getName
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
+    def "directive on inline fragment spread"() {
+        given:
+        def query = """
+          query Foo {
+                 ... @include(if: true) {
+                    name
+                  }
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
+    def "directive on inline fragment spread and type"() {
+        given:
+        def query = """
+          query Foo {
+                 ... on Foo @include(if: true) {
+                    name
+                  }
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
+    def "directive on fragment spread"() {
+        given:
+        def query = """
+          fragment getName on Foo {
+            name
+          }
+          query Foo {
+                ...getName @include(if: true)
+              }
+        """
+
+        Document document = new Parser().parseDocument(query)
+        LanguageTraversal languageTraversal = new LanguageTraversal()
+
+        when:
+        languageTraversal.traverse(document, new RulesVisitor(validationContext, [knownDirectives]))
+
+        then:
+        errorCollector.errors.isEmpty()
+
+    }
+
 
 }
