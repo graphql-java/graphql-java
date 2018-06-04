9e01eaa543ae93ecd5080768f477443469eb1dbb
andimarek <andimarek@fastmail.fm>
2018-06-04 16:08:49 +1000

    pass RELEASE_VERSION env to build

--- travis-build.sh
+++ travis-build.sh
@@ -5,4 +5,4 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     echo "Building on master"
     BUILD_COMMAND="./gradlew clean assemble && ./gradlew check && ./gradlew bintrayUpload -x check --info"
 fi
-docker run -it --rm -v `pwd .`:/build  -e BINTRAY_USER=$BINTRAY_USER -e BINTRAY_API_KEY=$BINTRAY_API_KEY -e MAVEN_CENTRAL_USER=$MAVEN_CENTRAL_USER -e MAVEN_CENTRAL_PASSWORD=$MAVEN_CENTRAL_PASSWORD -w /build openjdk:8u131-jdk bash -c "${BUILD_COMMAND}"
+docker run -it --rm -v `pwd .`:/build -e RELEASE_VERSION=$RELEASE_VERSION -e BINTRAY_USER=$BINTRAY_USER -e BINTRAY_API_KEY=$BINTRAY_API_KEY -e MAVEN_CENTRAL_USER=$MAVEN_CENTRAL_USER -e MAVEN_CENTRAL_PASSWORD=$MAVEN_CENTRAL_PASSWORD -w /build openjdk:8u131-jdk bash -c "${BUILD_COMMAND}"
