4d3a9d3f3d44e098a05927225d581f894cb9b41b
tinnou <antoineb19@gmail.com>
2018-07-27 11:32:21 -0700

    Add clearDirectives to remove built-in directives.

--- src/test/groovy/graphql/schema/GraphQLSchemaTest.groovy
+++ src/test/groovy/graphql/schema/GraphQLSchemaTest.groovy
@@ -3,6 +3,8 @@ package graphql.schema
 import graphql.AssertException
 import graphql.ExecutionInput
 import graphql.GraphQL
+import graphql.Scalars
+import graphql.StarWarsData
 import graphql.TestUtil
 import graphql.schema.idl.RuntimeWiring
 import spock.lang.Specification
@@ -11,6 +13,8 @@ import static graphql.StarWarsSchema.characterInterface
 import static graphql.StarWarsSchema.droidType
 import static graphql.StarWarsSchema.humanType
 import static graphql.StarWarsSchema.starWarsSchema
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import static graphql.schema.GraphQLObjectType.newObject
 
 class GraphQLSchemaTest extends Specification {
 
@@ -75,6 +79,35 @@ class GraphQLSchemaTest extends Specification {
         expect:
         assert 0 == runQuery(existingSchema).getErrors().size()
         assert 0 == runQuery(schema).getErrors().size()
+    }
+
+    def "clear directives works as expected"() {
+        setup:
+        def schemaBuilder = GraphQLSchema.newSchema()
+                .query(newObject()
+                    .name("QueryType")
+                    .field(newFieldDefinition()
+                        .name("hero")
+                        .type(Scalars.GraphQLString)
+                        .dataFetcher(new StaticDataFetcher(StarWarsData.getArtoo()))))
+
+        when: "no additional directives have been specified"
+        def schema = schemaBuilder.build()
+
+        then: "built-in directives should be present"
+        schema.directives.size() == 3
+
+        when: "clear directives is called"
+        schema = schemaBuilder.clearDirectives().build()
+
+        then: "all directives should be cleared"
+        schema.directives.empty
+
+        when: "clear directives is called with additional types"
+        schema = schemaBuilder.clearDirectives().build([] as Set)
+
+        then: "all directives should be cleared"
+        schema.directives.empty
     }
 
 
