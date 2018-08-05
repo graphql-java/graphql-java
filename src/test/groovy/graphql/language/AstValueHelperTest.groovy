6b60c3ece334bb359b2f85590fc97fed6837f5e1
Brad Baker <bbaker@atlassian.com>
2018-08-06 04:34:18 +1000

    #1105 - fixed json encoding of AST string values
    (cherry picked from commit 639f366)

--- src/test/groovy/graphql/language/AstValueHelperTest.groovy
+++ src/test/groovy/graphql/language/AstValueHelperTest.groovy
@@ -178,4 +178,29 @@ class AstValueHelperTest extends Specification {
         '["A", "B", "C"]'                             | ArrayValue.class
         '{string : "s", integer : 1, boolean : true}' | ObjectValue.class
     }
+
+    def "1105 - encoding of json strings"() {
+
+        when:
+        def json = AstValueHelper.jsonStringify(strValue)
+
+        then:
+        json == expected
+
+        where:
+        strValue                                  | expected
+        ''                                        | ''
+        'json'                                    | 'json'
+        'quotation-"'                             | 'quotation-\\"'
+        'reverse-solidus-\\'                      | 'reverse-solidus-\\\\'
+        'solidus-/'                               | 'solidus-\\/'
+        'backspace-\b'                            | 'backspace-\\b'
+        'formfeed-\f'                             | 'formfeed-\\f'
+        'newline-\n'                              | 'newline-\\n'
+        'carriage-return-\r'                      | 'carriage-return-\\r'
+        'horizontal-tab-\t'                       | 'horizontal-tab-\\t'
+
+        // this is some AST from issue 1105
+        '''"{"operator":"eq", "operands": []}"''' | '''\\"{\\"operator\\":\\"eq\\", \\"operands\\": []}\\"'''
+    }
 }
