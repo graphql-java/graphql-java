b031cfcd806c434e053a4169b8a053b2f95eff47
Brad Baker <bbakerman@gmail.com>
2018-09-28 13:43:06 +1000

    Deprecate all the batched code (#1243)
    * Added @Override as part of errorprone code health check
    
    * Revert "Added @Override as part of errorprone code health check"
    
    This reverts commit 38dfab1
    
    * Deprecating all the batched related code

--- src/main/java/graphql/execution/batched/BatchedDataFetcherFactory.java
+++ src/main/java/graphql/execution/batched/BatchedDataFetcherFactory.java
@@ -12,8 +12,10 @@ import java.lang.reflect.Method;
  * Otherwise we wrap the fetcher in a BatchedDataFetcher that iterates over the sources and invokes the delegate
  * on each source. Note that this forgoes any performance benefits of batching,
  * so regular DataFetchers should normally only be used if they are in-memory.
+ *
+ * @deprecated This has been deprecated in favour of using {@link graphql.execution.AsyncExecutionStrategy} and {@link graphql.execution.instrumentation.dataloader.DataLoaderDispatcherInstrumentation}
  */
-
+@Deprecated
 public class BatchedDataFetcherFactory {
     public BatchedDataFetcher create(final DataFetcher supplied) {
         if (supplied instanceof BatchedDataFetcher) {
