de33b8fd0399a8ba9ceddbd2290b427e705a2294
Brad Baker <bbaker@atlassian.com>
2018-11-04 08:48:02 +1100

    Added a regression test on re-use of type references

---
+++ src/test/groovy/graphql/schema/GraphQLTypeReferenceTest.groovy
@@ -0,0 +1,37 @@
+package graphql.schema
+
+import graphql.Scalars
+import spock.lang.Specification
+
+class GraphQLTypeReferenceTest extends Specification {
+
+    def "the same reference can be used multiple times"() {
+        when:
+        GraphQLTypeReference ref = new GraphQLTypeReference("String");
+        def inputObject = GraphQLInputObjectType.newInputObject()
+                .name("ObjInput")
+                .field(GraphQLInputObjectField.newInputObjectField()
+                .name("value")
+                .type(ref)) //Will get replaced, as expected
+                .field(GraphQLInputObjectField.newInputObjectField()
+                .name("value2")
+                .type(ref)) //Will *not* get replaced the 2nd time it's used
+                .build()
+
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+                .query(GraphQLObjectType.newObject()
+                .name("Query")
+                .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("test")
+                .type(Scalars.GraphQLString)
+                .dataFetcher({ env -> "test" })
+                .argument(GraphQLArgument.newArgument()
+                .name("in")
+                .type(inputObject))
+        )).build()
+
+        then:
+        // issue 1216 - reuse of type reference caused problems
+        schema != null
+    }
+}
