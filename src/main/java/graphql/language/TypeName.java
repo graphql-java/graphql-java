efdaa48189f354c9892b313e7827e1ae8d76b696
andimarek <andimarek@fastmail.fm>
2018-11-16 16:58:19 +1100

    TypeName is also a NamedNode

--- src/main/java/graphql/language/TypeName.java
+++ src/main/java/graphql/language/TypeName.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 @PublicApi
-public class TypeName extends AbstractNode<TypeName> implements Type<TypeName> {
+public class TypeName extends AbstractNode<TypeName> implements Type<TypeName>, NamedNode<TypeName> {
 
     private final String name;
 
