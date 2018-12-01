9a145f41603077ad3d8d6592575dde63b64abdcc
andimarek <andimarek@fastmail.fm>
2018-12-01 22:20:18 +1100

    AstMultiZipper tests

--- src/test/groovy/graphql/language/AstMultiZipperTest.groovy
+++ src/test/groovy/graphql/language/AstMultiZipperTest.groovy
@@ -8,7 +8,7 @@ import static graphql.language.AstBreadcrumb.Location
 class AstMultiZipperTest extends Specification {
 
 
-    def "toRootNode"() {
+    def "toRootNode with one zipper"() {
         def rootNode = TestUtil.parseQuery("{root{midA{leafA leafB} midB{leafC leafD}}}")
         def operationDefinition = rootNode.children[0] as OperationDefinition
 
@@ -36,16 +36,64 @@ class AstMultiZipperTest extends Specification {
 
         when:
         def newRoot = multiZipper.toRootNode()
-        def newMidA = getMidANode(newRoot) as Field
+        def newMidA = getMidA(newRoot) as Field
 
         then:
         newMidA.name == "midA-changed"
     }
 
-    Node getMidANode(Node rootNode) {
+    Node rootField(Document document) {
+        def operationDefinition = document.children[0] as OperationDefinition
+        operationDefinition.selectionSet.children[0] as Field
+    }
+
+    Node getMidA(Document document) {
+        rootField(document).selectionSet.children[0] as Field
+    }
+
+    Node getMidB(Document document) {
+        rootField(document).selectionSet.children[1] as Field
+    }
+
+    def "toRootNode with two zippers"() {
+        def rootNode = TestUtil.parseQuery("{root{midA{leafA leafB} midB{leafC leafD}}}")
         def operationDefinition = rootNode.children[0] as OperationDefinition
+
         def rootField = operationDefinition.selectionSet.children[0] as Field
         def midA = rootField.selectionSet.children[0] as Field
-        return midA
+        def leafA = midA.selectionSet.children[0] as Field
+        def leafB = midA.selectionSet.children[0] as Field
+
+        def midB = rootField.selectionSet.children[1] as Field
+        def leafC = midB.selectionSet.children[0] as Field
+        def leafD = midB.selectionSet.children[0] as Field
+
+
+        def b1 = new AstBreadcrumb(rootNode, new Location(Document.CHILD_DEFINITIONS, 0))
+        def b2 = new AstBreadcrumb(operationDefinition, new Location(OperationDefinition.CHILD_SELECTION_SET, 0))
+        def b3 = new AstBreadcrumb(operationDefinition.selectionSet, new Location(SelectionSet.CHILD_SELECTIONS, 0))
+        def b4 = new AstBreadcrumb(rootField, new Location(Field.CHILD_SELECTION_SET, 0))
+        def bMidA = new AstBreadcrumb(rootField.selectionSet, new Location(SelectionSet.CHILD_SELECTIONS, 0))
+        def bMidB = new AstBreadcrumb(rootField.selectionSet, new Location(SelectionSet.CHILD_SELECTIONS, 1))
+
+        def breadCrumbsFromMidA = [bMidA, b4, b3, b2, b1]
+        def breadCrumbsFromMidB = [bMidB, b4, b3, b2, b1]
+
+        def midAChanged = midA.transform({ builder -> builder.name("midA-changed") })
+        def midBChanged = midB.transform({ builder -> builder.name("midB-changed") })
+
+        def midAZipperChanged = new AstZipper(midAChanged, breadCrumbsFromMidA)
+        def midBZipperChanged = new AstZipper(midBChanged, breadCrumbsFromMidB)
+        def multiZipper = new AstMultiZipper(rootNode, [midAZipperChanged, midBZipperChanged])
+
+        when:
+        def newRoot = multiZipper.toRootNode()
+        def newMidA = getMidA(newRoot) as Field
+        def newMidB = getMidB(newRoot) as Field
+
+        then:
+        newMidA.name == "midA-changed"
+        newMidB.name == "midB-changed"
+
     }
 }
