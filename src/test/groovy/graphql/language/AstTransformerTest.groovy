5f11a736b4aa2abbe99826ff6de23eceeeeed03a
andimarek <andimarek@fastmail.fm>
2018-12-02 13:24:52 +1100

    make test nicer

--- src/test/groovy/graphql/language/AstTransformerTest.groovy
+++ src/test/groovy/graphql/language/AstTransformerTest.groovy
@@ -7,7 +7,7 @@ import spock.lang.Specification
 
 class AstTransformerTest extends Specification {
 
-    def "test"() {
+    def "modify multiple nodes"() {
         def document = TestUtil.parseQuery("{ root { foo { midA { leafA } midB { leafB } } bar { midC { leafC } midD { leafD } } } }")
 
         AstTransformer astTransformer = new AstTransformer()
@@ -16,23 +16,28 @@ class AstTransformerTest extends Specification {
 
             @Override
             TraversalControl visitField(Field node, TraverserContext<Node> context) {
-                AstMultiZipper multiZipper = context.getCurrentAccumulate();
-                if (node.name.startsWith("mid")) {
-                    String newName = node.name + "-modified"
-                    def zipper = context
-                            .getVar(AstZipper.class)
-                            .withNewNode(node.transform({ builder -> builder.name(newName) }))
-                    multiZipper = multiZipper.withNewZipper(zipper)
+                if (!node.name.startsWith("mid")) {
+                    return TraversalControl.CONTINUE
                 }
-                context.setAccumulate(multiZipper)
-                return super.visitField(node, context)
+                String newName = node.name + "-modified"
+
+                Field changedField = node.transform({ builder -> builder.name(newName) })
+                def zipperWithChangedNode = context.getVar(AstZipper.class).withNewNode(changedField)
+
+                AstMultiZipper multiZipper = context.getCurrentAccumulate();
+                context.setAccumulate(multiZipper.withNewZipper(zipperWithChangedNode))
+
+                return TraversalControl.CONTINUE
             }
         }
+
         when:
         def newDocument = astTransformer.transform(document, visitor)
 
         then:
-        AstPrinter.printAstCompact(newDocument) == "query { root { foo { midA-modified { leafA } midB-modified { leafB } } bar { midC-modified { leafC } midD-modified { leafD } } } }"
+        AstPrinter.printAstCompact(newDocument) ==
+                "query { root { foo { midA-modified { leafA } midB-modified { leafB } } bar { midC-modified { leafC } midD-modified { leafD } } } }"
 
     }
+
 }
