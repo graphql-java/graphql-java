171a0a5cb17d304ca9cbc5bbc3584ccbb225a16a
Brad Baker <bbaker@atlassian.com>
2018-12-12 20:07:18 +1100

    And a test

---
+++ src/test/groovy/graphql/execution/DataFetcherResultTest.groovy
@@ -0,0 +1,20 @@
+package graphql.execution
+
+import graphql.InvalidSyntaxError
+import graphql.validation.ValidationError
+import graphql.validation.ValidationErrorType
+import spock.lang.Specification
+
+class DataFetcherResultTest extends Specification {
+
+    def "basic building"() {
+        def error1 = new ValidationError(ValidationErrorType.DuplicateOperationName)
+        def error2 = new InvalidSyntaxError([], "Boo")
+        when:
+        def result = DataFetcherResult.newResult().data("hello")
+                .error(error1).errors([error2]).build()
+        then:
+        result.getData() == "hello"
+        result.errors == [error1, error2]
+    }
+}
