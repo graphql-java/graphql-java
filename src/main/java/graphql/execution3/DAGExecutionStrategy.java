2f71c45bb0c9dca224a6dc21ce2b8d1157b7c235
Greg E. Kesler <greg_kesler@intuit.com>
2019-01-25 18:33:27 -0800

    - implemented DAGExecutionStrategy based on DependencyGraph evaluation

--- src/main/java/graphql/execution3/DAGExecutionStrategy.java
+++ src/main/java/graphql/execution3/DAGExecutionStrategy.java
@@ -5,7 +5,6 @@
  */
 package graphql.execution3;
 
-import graphql.Assert;
 import graphql.ExecutionResult;
 import graphql.ExecutionResultImpl;
 import graphql.execution.Async;
@@ -23,16 +22,12 @@ import graphql.schema.GraphQLType;
 import graphql.util.DependenciesIterator;
 import graphql.util.Edge;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -48,7 +43,7 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
         this.executionContext = Objects.requireNonNull(executionContext);
         this.executionInfoFactory = new ExecutionStepInfoFactory();
         this.valueFetcher = new ValueFetcher(executionContext);
-//        this.fetchedValueAnalyzer = new FetchedValueAnalyzer(executionContext);
+        this.resultCollector = new ResultCollector();
     }
     
     @Override
@@ -113,19 +108,18 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
                 Object result = source.getResult();
                 return sink
                     .parentExecutionStepInfo(source.getExecutionStepInfo())
-                    .source(flatten((List<Object>)source.getResult()));
+                    .source(ResultCollector.flatten((List<Object>)source.getResult()));
             }
         });
     }
     
     private void joinResults (FieldVertex source, NodeVertex<Node, GraphQLType> sink) {
         LOGGER.info("afterResolve: source={}, sink={}", source, sink);
-//        resultCollector.joinOn(source.getResponseKey(), (List<Object>)source.getResult(), (List<Object>)source.getSource());
         resultCollector.joinResultsOf(source);
     }
 
-    private boolean resolveNode (NodeVertex<Node, GraphQLType> node) {
-        LOGGER.info("resolveNode: {}", node);
+    private boolean tryResolve (NodeVertex<Node, GraphQLType> node) {
+        LOGGER.info("tryResolve: node={}", node);
         return node.accept(true, new NodeVertexVisitor<Boolean>() {
             @Override
             public Boolean visit(FieldVertex node, Boolean data) {
@@ -141,39 +135,28 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
     }
 
     private CompletableFuture<NodeVertex<Node, GraphQLType>> fetchNode (NodeVertex<Node, GraphQLType> node) {  
-        LOGGER.info("fetchNode: {}", node);
+        LOGGER.info("fetchNode: node={}", node);
         
-        FieldVertex fieldNode = (FieldVertex)(NodeVertex<? extends Node, ? extends GraphQLType>)node;
-//        Object source = fieldNode.isRoot() ? executionContext.getRoot() : asObject(fieldNode.getSource());
-//        List<Object> sources = fieldNode.isRoot() ? asList(executionContext.getRoot()) : (List<Object>)fieldNode.getSource();
+        FieldVertex fieldNode = node.as(FieldVertex.class);
         List<Field> sameFields = Collections.singletonList(fieldNode.getNode());
-        ExecutionStepInfo executionStepInfo = executionInfoFactory.newExecutionStepInfoForSubField(executionContext, sameFields, node.getParentExecutionStepInfo());
-//        List<ExecutionStepInfo> executionStepInfos = Stream
-//                .generate(() -> executionStepInfo)
-//                .limit(sources.size())
-//                .collect(Collectors.toList());
+        ExecutionStepInfo executionStepInfo = executionInfoFactory
+            .newExecutionStepInfoForSubField(executionContext, sameFields, node.getParentExecutionStepInfo());
+        
         Supplier<CompletableFuture<List<FetchedValue>>> valuesSupplier = fieldNode.isRoot()
             ? () -> fetchRootValues(executionContext.getRoot(), sameFields, executionStepInfo)
             : () -> fetchBatchedValues((List<Object>)fieldNode.getSource(), sameFields, executionStepInfo);
 
 
-        // FIXME: in batch mode source object *always* must be a list
-        // extract the element for now if the list is a singleton
-        
-//        return valueFetcher
-//                .fetchValue(source, sameFields, executionStepInfo)
-//                .thenApply(fetchedValue -> asList(fetchedValue.getFetchedValue()))
-//                .thenApply(fetchedValue -> (NodeVertex<Node, GraphQLType>)node
-//                        .executionStepInfo(executionStepInfo)
-//                        .result(fetchedValue));
         return valuesSupplier.get()
-                .thenApply(fetchedValues -> fetchedValues
+                .thenApply(fetchedValues -> 
+                    fetchedValues
                         .stream()
                         .map(FetchedValue::getFetchedValue)
-                        .map(fv -> checkAndFixNILs(fv, fieldNode))
+                        .map(fv -> resultCollector.checkAndFixNILs(fv, fieldNode))
                         .collect(Collectors.toList())
                 )
-                .thenApply(fetchedValues -> (NodeVertex<Node, GraphQLType>)node
+                .thenApply(fetchedValues -> 
+                    (NodeVertex<Node, GraphQLType>)node
                         .executionStepInfo(executionStepInfo)
                         .result(fetchedValues)
                 );
@@ -194,93 +177,7 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
                     .collect(Collectors.toList())
             );
     }
-    
-    private Object checkAndFixNILs (Object fetchedValue, FieldVertex fieldNode) {
-        return (isNIL(fetchedValue) || fieldNode.getCardinality() == FieldVertex.Cardinality.OneToOne)
-            ? fixNIL(fetchedValue, fieldNode)
-            : fixNILs((List<Object>)fetchedValue, fieldNode);
-    }
-        
-    private static boolean isNIL (Object value) {
-        return value == null || value == ValueFetcher.NULL_VALUE;
-    }
-    
-    private static Object fixNIL (Object fetchedValue, FieldVertex fieldNode) {
-        if (isNIL(fetchedValue)) {
-            if (fieldNode.isNotNull()) {
-                // FIXME: report error?
-            }
-            
-            return null;
-        }
-        
-        return fetchedValue;
-    }
 
-    private static Object fixNILs (List<Object> fetchedValues, FieldVertex fieldNode) {
-        boolean hasNulls[] = {false};
-        fetchedValues = flatten(fetchedValues, o -> true)
-            .stream()
-            .map(fetchedValue -> {
-                if (isNIL(fetchedValue)) {
-                    hasNulls[0] = true;
-                    return null;
-                } else {
-                    return fetchedValue;
-                }
-            })
-            .collect(Collectors.toList());
-        
-        if (hasNulls[0]) {
-            if (fieldNode.isNotNullItems()) {
-                return null;
-            }
-        }
-        
-        return fetchedValues;
-    }
-
-    public static List<Object> flatten (List<Object> result) {
-        return flatten(result, o -> o != null);
-    }
-    
-    public static List<Object> flatten (List<Object> result, Predicate<? super Object> filter) {
-        Objects.requireNonNull(filter);
-        
-        return Optional
-            .ofNullable(result)
-            .map(res -> res
-                .stream()
-                .flatMap(DAGExecutionStrategy::asStream)
-                .filter(filter)
-                .collect(Collectors.toList())
-            )
-            .orElseGet(Collections::emptyList);
-    }
-
-    private static Stream<Object> asStream (Object o) {
-        return (o instanceof Collection)
-            ? ((Collection<Object>)o)
-                .stream()
-                .flatMap(DAGExecutionStrategy::asStream)
-            : Stream.of(o);
-    }
-
-    private static List<Object> asList (Object o) {
-        return (o instanceof List)
-            ? (List<Object>)o
-            : Collections.singletonList(o);
-    }
-    
-    private static Object asObject (Object o) {
-        List<Object> singletonList;
-        return (o instanceof List && (singletonList = (List<Object>)o).size() <= 1)
-            ? singletonList.size() == 1 
-                ? singletonList.get(0) 
-                : null
-            : o;
-    }
-    
     private class ExecutionPlanContextImpl implements ExecutionPlanContext {
         @Override
         public void prepareResolve(Edge<? extends NodeVertex<? extends Node, ? extends GraphQLType>, ?> edge) {
@@ -294,7 +191,7 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
 
         @Override
         public boolean resolve(NodeVertex<? extends Node, ? extends GraphQLType> node) {
-            return resolveNode((NodeVertex<Node, GraphQLType>)node);
+            return tryResolve((NodeVertex<Node, GraphQLType>)node);
         }
 
         public ExecutionResult getResult() {
@@ -305,9 +202,7 @@ public class DAGExecutionStrategy implements ExecutionStrategy {
     private final ExecutionContext executionContext;
     private final ExecutionStepInfoFactory executionInfoFactory;
     private final ValueFetcher valueFetcher;
-//    private final FetchedValueAnalyzer fetchedValueAnalyzer;
-    private final ResultCollector resultCollector = new ResultCollector();
+    private final ResultCollector resultCollector;
     
-    private static final CompletableFuture<?>[] EMPTY_STAGES = {};
     private static final Logger LOGGER = LoggerFactory.getLogger(DAGExecutionStrategy.class);
 }
--- src/main/java/graphql/execution3/ResultCollector.java
+++ src/main/java/graphql/execution3/ResultCollector.java
@@ -5,9 +5,8 @@
  */
 package graphql.execution3;
 
-import graphql.language.Node;
-import graphql.schema.GraphQLType;
-import java.util.ArrayList;
+import graphql.execution2.ValueFetcher;
+import static graphql.execution3.NodeVertexVisitor.whenFieldVertex;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -16,93 +15,17 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
  * @author gkesler
  */
-public class ResultCollector {
-    public void joinResultsOf (FieldVertex node) {
-        Objects.requireNonNull(node);
-        
-        String on = node.getResponseKey();
-        List<Object> result = (List<Object>)node.getResult();
-        List<Object> source = (List<Object>)node.getSource();
-        // will join relation dataset to target dataset
-        // since we don't have any keys to build a cartesian product,
-        // will be assuming that relation and target dataset s are alerady sorted by the
-        // same key, so a pair {target[i], relation[i]} represents that cartesian product
-        // we can use to join them together
-        int[] indexHolder = {0};
-        result
-            .stream()
-            .limit(Math.min(source.size(), result.size()))
-            .forEach(value -> {
-                int index = indexHolder[0]++;
-                Map<String, Object> targetMap = (Map<String, Object>)source.get(index);
-//                if (targetMap != null) {
-//                    Object value = result.get(index);
-                    targetMap.put(on, value);
-
-                    // Here, verify if the value to join is valid
-                    if (value == null && node.isNotNull()) {
-                        // need to set the paren't corresponding value to null
-                        bubbleUpNIL(node);
-                    }                    
-//                }
-            });
-    }
-    
-    private void bubbleUpNIL (FieldVertex node) {
-        String responseKey = node.getResponseKey();
-        node
-          .dependencySet()
-          .forEach(v -> {
-//                if (!v.accept(false, FieldVertex.IS_FIELD))
-//                    return;
-//                
-//                FieldVertex fieldNode = v.as(FieldVertex.class);
-                boolean hasNull = false;
-                List<Object> results = (List<Object>)v.getResult();
-                ListIterator resultsIt = results.listIterator();
-                while (resultsIt.hasNext()) {
-                    Object o = resultsIt.next();
-                    
-                    if (o == null)
-                        continue;
-                    
-                    List<Object> items = (o instanceof List)
-                          ? (List<Object>)o
-                          : Arrays.asList(o);
-
-                    ListIterator itemsIt = items.listIterator();
-                    while (itemsIt.hasNext()) {
-                        Map<String, Object> value = (Map<String, Object>)itemsIt.next();
-
-                        if (value.getOrDefault(responseKey, this) == null) {
-                            itemsIt.set(null);
-                            hasNull = true;
-                        }
-                    }
-                    
-                    if (hasNull && NodeVertexVisitor.whenFieldVertex(v, node.isNotNull(), n -> n.isNotNullItems())/*node.isNotNullItems()*/) {
-                        resultsIt.set(null);
-                    } else {
-                        hasNull = false;
-                    }
-                }
-
-                if (hasNull) {
-                    NodeVertexVisitor.whenFieldVertex(v, null, n -> {
-                        joinResultsOf(n);
-                        return null;
-                    });
-                }
-          });
-    };
-    
+public class ResultCollector {    
     public DocumentVertex prepareResult (DocumentVertex document) {
         Objects.requireNonNull(document);
         
@@ -117,6 +40,152 @@ public class ResultCollector {
     
     public Object getResult () {
         return result.get(0);
+    }
+    
+    public void joinResultsOf (FieldVertex node) {
+        Objects.requireNonNull(node);
+        
+        String on = node.getResponseKey();
+        List<Object> result = (List<Object>)node.getResult();
+        List<Object> source = (List<Object>)node.getSource();
+        
+        // will join relation dataset to target dataset
+        // since we don't have any keys to build a cartesian product,
+        // will be assuming that relation and target dataset s are alerady sorted by the
+        // same key, so a pair {target[i], relation[i]} represents that cartesian product
+        // we can use to join them together
+        int index[] = {0};
+        result
+            .stream()
+            .limit(Math.min(source.size(), result.size()))
+            .forEach(value -> {
+                Map<String, Object> targetMap = (Map<String, Object>)source.get(index[0]++);
+                targetMap.put(on, value);
+
+                // Here, verify if the value to join is valid
+                if (value == null && node.isNotNull()) {
+                    // need to set the paren't corresponding value to null
+                    bubbleUpNIL(node);
+                }                    
+            });
+    }
+    
+    private void bubbleUpNIL (FieldVertex node) {
+        String responseKey = node.getResponseKey();
+        node
+          .dependencySet()
+          .forEach(v -> {
+                boolean hasNull = false;                
+                ListIterator results = asList(v.getResult()).listIterator();
+                while (results.hasNext()) {
+                    Object o = results.next();                    
+                    if (o == null) {
+                        hasNull = true;
+                        continue;
+                    }
+                    
+                    boolean hasNullItems = false;                    
+                    ListIterator items = asList(o).listIterator();
+                    while (items.hasNext()) {
+                        Map<String, Object> value = (Map<String, Object>)items.next();
+
+                        if (value.getOrDefault(responseKey, this) == null) {
+                            items.set(null);
+                            hasNullItems = true;
+                        }
+                    }
+                    
+                    if (hasNullItems && whenFieldVertex(v, node.isNotNull(), FieldVertex::isNotNullItems)) {
+                        results.set(null);
+                        hasNull = true;
+                    }
+                }
+
+                if (hasNull) {
+                    whenFieldVertex(v, null, n -> {
+                        joinResultsOf(n);
+                        return null;
+                    });
+                }
+          });
+    };
+    
+    public Object checkAndFixNILs (Object fetchedValue, FieldVertex fieldNode) {
+        return (isNIL(fetchedValue) || isOneToOne(fieldNode))
+            ? fixNIL(fetchedValue, () -> null)
+            : fixNILs((List<Object>)fetchedValue, fieldNode);
+    }
+        
+    private static boolean isNIL (Object value) {
+        return value == null || value == ValueFetcher.NULL_VALUE;
+    }
+    
+    private static boolean isOneToOne (FieldVertex fieldNode) {
+        return fieldNode.getCardinality() == FieldVertex.Cardinality.OneToOne;
+    }
+    
+    private static Object fixNIL (Object fetchedValue, Supplier<? super Object> mapper) {
+        return (isNIL(fetchedValue))
+            ? mapper.get()
+            : fetchedValue;
+    }
+
+    private static Object fixNILs (List<Object> fetchedValues, FieldVertex fieldNode) {
+        boolean hasNulls[] = {false};
+        fetchedValues = flatten(fetchedValues, o -> true)
+            .stream()
+            .map(o -> fixNIL(o, 
+                () -> { 
+                    hasNulls[0] = true; 
+                    return null; 
+                }
+            ))
+            .collect(Collectors.toList());
+        
+        return (hasNulls[0] && fieldNode.isNotNullItems())
+            ? null
+            : fetchedValues;
+    }
+
+    public static List<Object> flatten (List<Object> result) {
+        return flatten(result, o -> o != null);
+    }
+    
+    public static List<Object> flatten (List<Object> result, Predicate<? super Object> filter) {
+        Objects.requireNonNull(filter);
+        
+        return Optional
+            .ofNullable(result)
+            .map(res -> res
+                .stream()
+                .flatMap(ResultCollector::asStream)
+                .filter(filter)
+                .collect(Collectors.toList())
+            )
+            .orElseGet(Collections::emptyList);
+    }
+
+    private static Stream<Object> asStream (Object o) {
+        return (o instanceof Collection)
+            ? ((Collection<Object>)o)
+                .stream()
+                .flatMap(ResultCollector::asStream)
+            : Stream.of(o);
+    }
+
+    private static List<Object> asList (Object o) {
+        return (o instanceof List)
+            ? (List<Object>)o
+            : Arrays.asList(o);
+    }
+    
+    private static Object asObject (Object o) {
+        List<Object> singletonList;
+        return (o instanceof List && (singletonList = (List<Object>)o).size() <= 1)
+            ? singletonList.size() == 1 
+                ? singletonList.get(0) 
+                : null
+            : o;
     }
     
     private final List<Object> result = Arrays.asList(new HashMap<String, Object>());
