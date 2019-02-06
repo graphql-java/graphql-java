5388e0ea1fc134a2407250a1d18c4aaf50261cf8
Brad Baker <bbaker@atlassian.com>
2019-02-06 11:34:56 +1100

    Because warnings annoy me

--- src/main/java/graphql/schema/idl/DefaultSchemaPrinterComparatorRegistry.java
+++ src/main/java/graphql/schema/idl/DefaultSchemaPrinterComparatorRegistry.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.function.UnaryOperator;
 
 import static graphql.Assert.assertNotNull;
-import static graphql.schema.idl.SchemaPrinterComparatorEnvironment.*;
+import static graphql.schema.idl.SchemaPrinterComparatorEnvironment.newEnvironment;
 
 /**
  * Associates a {@code Comparator} with a {@code SchemaPrinterComparatorEnvironment} to control the scope in which the {@code Comparator} can be applied.
@@ -64,10 +64,11 @@ public class DefaultSchemaPrinterComparatorRegistry implements SchemaPrinterComp
         /**
          * Registers a {@code Comparator} with an environment to control its permitted scope of operation.
          *
-         * @param environment Defines the scope to control where the {@code Comparator} can be applied.
+         * @param environment     Defines the scope to control where the {@code Comparator} can be applied.
          * @param comparatorClass The {@code Comparator} class for added type safety. It should match {@code environment.elementType}.
-         * @param comparator The {@code Comparator} of type {@code comparatorClass}.
-         * @param <T> The specific {@code GraphQLType} the {@code Comparator} should operate on.
+         * @param comparator      The {@code Comparator} of type {@code comparatorClass}.
+         * @param <T>             The specific {@code GraphQLType} the {@code Comparator} should operate on.
+         *
          * @return The {@code Builder} instance to allow chaining.
          */
         public <T extends GraphQLType> Builder addComparator(SchemaPrinterComparatorEnvironment environment, Class<T> comparatorClass, Comparator<? super T> comparator) {
@@ -80,10 +81,18 @@ public class DefaultSchemaPrinterComparatorRegistry implements SchemaPrinterComp
 
         /**
          * Convenience method which supplies an environment builder function.
+         *
+         * @param builderFunction the function which is given a builder
+         * @param comparatorClass The {@code Comparator} class for added type safety. It should match {@code environment.elementType}.
+         * @param comparator      The {@code Comparator} of type {@code comparatorClass}.
+         * @param <T>             the graphql type
+         *
+         * @return this builder
+         *
          * @see #addComparator
          */
         public <T extends GraphQLType> Builder addComparator(UnaryOperator<SchemaPrinterComparatorEnvironment.Builder> builderFunction,
-                Class<T> comparatorClass, Comparator<? super T> comparator) {
+                                                             Class<T> comparatorClass, Comparator<? super T> comparator) {
             assertNotNull(builderFunction, "builderFunction can't be null");
 
             SchemaPrinterComparatorEnvironment environment = builderFunction.apply(newEnvironment()).build();
