7652086cfe8dbdb15efdbd1a8bda6e119c7d4d95
Brad Baker <bbaker@atlassian.com>
2019-02-20 12:48:07 +1100

    Made the ErrorType implement a base interface

--- src/main/java/graphql/ErrorType.java
+++ src/main/java/graphql/ErrorType.java
@@ -5,7 +5,7 @@ package graphql;
  * All the errors in graphql belong to one of these categories
  */
 @PublicApi
-public enum ErrorType {
+public enum ErrorType implements ErrorTypeClassification {
     InvalidSyntax,
     ValidationError,
     DataFetchingException,
---
+++ src/main/java/graphql/ErrorTypeClassification.java
@@ -0,0 +1,12 @@
+package graphql;
+
+/**
+ * Errors in graphql-java can have a classification to help with the processing
+ * of errors.  Custom {@link graphql.GraphQLError} implementations could use
+ * custom classifications.
+ * <p>
+ * graphql-java ships with a standard set of error classifications via {@link graphql.ErrorType}
+ */
+@PublicApi
+public interface ErrorTypeClassification {
+}
