2610977188e624035414776968a7808082ef8ae6
andimarek <andimarek@fastmail.fm>
2019-10-08 14:53:05 +1100

    logging

--- src/test/groovy/graphql/util/TreeParallelTraverserTest.groovy
+++ src/test/groovy/graphql/util/TreeParallelTraverserTest.groovy
@@ -1,10 +1,12 @@
 package graphql.util
 
+
 import spock.lang.Specification
 
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.TimeUnit
 
 class TreeParallelTraverserTest extends Specification {
     /**
@@ -34,8 +36,9 @@ class TreeParallelTraverserTest extends Specification {
                     def number = context.thisNode().number
                     println "number: $number"
                     if (number == 1) {
-                        while (latch.getCount() > 0) {
-                        }
+//                        while (latch.getCount() > 0) {
+//                        }
+                        assert latch.await(30, TimeUnit.SECONDS)
                     }
                     nodes.add(number)
                     latch.countDown()
@@ -45,6 +48,8 @@ class TreeParallelTraverserTest extends Specification {
         ] as TraverserVisitor
         when:
         def pool = new ForkJoinPool(4)
+        println "parellism: ${pool.getParallelism()}"
+        println "processors: ${Runtime.getRuntime().availableProcessors()}"
         TreeParallelTraverser.parallelTraverser({ n -> n.children }, pool).traverse(root, visitor)
 
 
