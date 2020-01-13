6571cab0dcb671ebcbdc27b7c3d59822d11261a3
Brad Baker <bbaker@atlassian.com>
2020-01-13 16:54:08 +1100

    Fixed up the no data loader logic

--- src/main/java/graphql/execution/instrumentation/dataloader/DataLoaderDispatcherInstrumentationState.java
+++ src/main/java/graphql/execution/instrumentation/dataloader/DataLoaderDispatcherInstrumentationState.java
@@ -25,12 +25,22 @@ public class DataLoaderDispatcherInstrumentationState implements Instrumentation
     private final FieldLevelTrackingApproach approach;
     private final AtomicReference<DataLoaderRegistry> dataLoaderRegistry;
     private final InstrumentationState state;
-    private boolean aggressivelyBatching = true;
+    private volatile boolean aggressivelyBatching = true;
+    private volatile boolean hasNoDataLoaders;
 
     public DataLoaderDispatcherInstrumentationState(Logger log, DataLoaderRegistry dataLoaderRegistry) {
         this.dataLoaderRegistry = new AtomicReference<>(dataLoaderRegistry);
         this.approach = new FieldLevelTrackingApproach(log, this::getDataLoaderRegistry);
         this.state = approach.createState();
+        hasNoDataLoaders = checkForNoDataLoader(dataLoaderRegistry);
+    }
+
+    private boolean checkForNoDataLoader(DataLoaderRegistry dataLoaderRegistry) {
+        //
+        // if they have never set a dataloader into the execution input then we can optimize
+        // away the tracking code
+        //
+        return dataLoaderRegistry == EMPTY_DATALOADER_REGISTRY;
     }
 
     boolean isAggressivelyBatching() {
@@ -51,10 +61,11 @@ public class DataLoaderDispatcherInstrumentationState implements Instrumentation
 
     void setDataLoaderRegistry(DataLoaderRegistry newRegistry) {
         dataLoaderRegistry.set(newRegistry);
+        hasNoDataLoaders = checkForNoDataLoader(dataLoaderRegistry.get());
     }
 
     boolean hasNoDataLoaders() {
-        return getDataLoaderRegistry().getKeys().isEmpty();
+        return hasNoDataLoaders;
     }
 
     InstrumentationState getState() {
