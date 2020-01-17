578985f4ed81d154f1f83bbef6903aa88f3f1a59
Brad Baker <bbakerman@gmail.com>
2020-01-17 13:37:39 +1100

    Introduce a GraphqlErrorException for Kotlin users and generally. (#1743)
    * Introduce a GraphqlErrorException for Kotlin users and a as a general purpose exception that can be an error
    
    * PR feedback and now more generic

--- src/main/java/graphql/schema/CoercingParseLiteralException.java
+++ src/main/java/graphql/schema/CoercingParseLiteralException.java
@@ -1,19 +1,12 @@
 package graphql.schema;
 
 import graphql.ErrorType;
-import graphql.GraphQLError;
-import graphql.GraphQLException;
+import graphql.GraphqlErrorException;
 import graphql.PublicApi;
 import graphql.language.SourceLocation;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
 @PublicApi
-public class CoercingParseLiteralException extends GraphQLException implements GraphQLError {
-    private final List<SourceLocation> sourceLocations;
-    private final Map<String, Object> extensions;
+public class CoercingParseLiteralException extends GraphqlErrorException {
 
     public CoercingParseLiteralException() {
         this(newCoercingParseLiteralException());
@@ -36,14 +29,7 @@ public class CoercingParseLiteralException extends GraphQLException implements G
     }
 
     private CoercingParseLiteralException(Builder builder) {
-        super(builder.message, builder.cause);
-        this.sourceLocations = builder.sourceLocations;
-        this.extensions = builder.extensions;
-    }
-
-    @Override
-    public List<SourceLocation> getLocations() {
-        return sourceLocations;
+        super(builder);
     }
 
     @Override
@@ -51,41 +37,11 @@ public class CoercingParseLiteralException extends GraphQLException implements G
         return ErrorType.ValidationError;
     }
 
-    @Override
-    public Map<String, Object> getExtensions() {
-        return extensions;
-    }
-
     public static Builder newCoercingParseLiteralException() {
         return new Builder();
     }
 
-    public static class Builder {
-        private String message;
-        private Throwable cause;
-        private List<SourceLocation> sourceLocations;
-        private Map<String, Object> extensions;
-
-        public Builder message(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public Builder cause(Throwable cause) {
-            this.cause = cause;
-            return this;
-        }
-
-        public Builder sourceLocation(SourceLocation sourceLocation) {
-            this.sourceLocations = sourceLocation == null ? null : Collections.singletonList(sourceLocation);
-            return this;
-        }
-
-        public Builder extensions(Map<String, Object> extensions) {
-            this.extensions = extensions;
-            return this;
-        }
-
+    public static class Builder extends BuilderBase<Builder, CoercingParseLiteralException> {
         public CoercingParseLiteralException build() {
             return new CoercingParseLiteralException(this);
         }
