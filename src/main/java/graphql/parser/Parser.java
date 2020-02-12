7abcc0f43777e5359951882fcec0dd482631bc3b
Brad Baker <bbaker@atlassian.com>
2020-02-13 09:44:26 +1100

    Now public API

--- src/main/java/graphql/parser/Parser.java
+++ src/main/java/graphql/parser/Parser.java
@@ -1,6 +1,6 @@
 package graphql.parser;
 
-import graphql.Internal;
+import graphql.PublicApi;
 import graphql.language.Document;
 import graphql.language.SourceLocation;
 import graphql.parser.antlr.GraphqlLexer;
@@ -19,7 +19,7 @@ import java.io.Reader;
 import java.io.UncheckedIOException;
 import java.util.List;
 
-@Internal
+@PublicApi
 public class Parser {
 
 
@@ -96,6 +96,13 @@ public class Parser {
         return doc;
     }
 
+    /**
+     * Allows you to override the ANTLR to AST code.
+     *
+     * @param tokens            the toke stream
+     * @param multiSourceReader the source of the query document
+     * @return a new GraphqlAntlrToLanguage instance
+     */
     protected GraphqlAntlrToLanguage getAntlrToLanguage(CommonTokenStream tokens, MultiSourceReader multiSourceReader) {
         return new GraphqlAntlrToLanguage(tokens, multiSourceReader);
     }
