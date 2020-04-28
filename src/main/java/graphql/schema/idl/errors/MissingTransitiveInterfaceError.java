3dfdab1b12fa64c4032737d59dec792083c86bb8
Felipe Reis <felipe.gdr@gmail.com>
2020-04-29 09:13:10 +1000

    1764: Use String.join instead of realying on Collection#toString

--- src/main/java/graphql/schema/idl/errors/MissingTransitiveInterfaceError.java
+++ src/main/java/graphql/schema/idl/errors/MissingTransitiveInterfaceError.java
@@ -9,6 +9,6 @@ import static java.lang.String.format;
 public class MissingTransitiveInterfaceError extends BaseError {
     public MissingTransitiveInterfaceError(String typeOfType, ImplementingTypeDefinition typeDefinition, Collection<String> missingInterfaces) {
         super(typeDefinition, format("The %s type '%s' %s does not implement the following transitive interfaces: %s",
-                typeOfType, typeDefinition.getName(), lineCol(typeDefinition), missingInterfaces));
+                typeOfType, typeDefinition.getName(), lineCol(typeDefinition), "[" + String.join(", ", missingInterfaces) + "]"));
     }
 }
