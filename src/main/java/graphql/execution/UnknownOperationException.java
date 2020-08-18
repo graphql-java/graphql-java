655385b06a5d311e02f09930f4bf439e15782e2c
Franklin Wang <fractalemagic@gmail.com>
2020-08-18 16:39:32 +1200

    Make UnknownOperationException implement GraphQLError

--- src/main/java/graphql/execution/UnknownOperationException.java
+++ src/main/java/graphql/execution/UnknownOperationException.java
@@ -1,7 +1,13 @@
 package graphql.execution;
 
+import graphql.ErrorClassification;
+import graphql.ErrorType;
+import graphql.GraphQLError;
 import graphql.GraphQLException;
 import graphql.PublicApi;
+import graphql.language.SourceLocation;
+
+import java.util.List;
 
 /**
  * This is thrown if multiple operations are defined in the query and
@@ -9,9 +15,18 @@ import graphql.PublicApi;
  * contained in the GraphQL query.
  */
 @PublicApi
-public class UnknownOperationException extends GraphQLException {
-
+public class UnknownOperationException extends GraphQLException implements GraphQLError {
     public UnknownOperationException(String message) {
         super(message);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return ErrorType.ValidationError;
     }
 }
