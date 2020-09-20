8938edc974806bb135257eb34e5e286b9125405d
dugenkui <dugenkui@meituan.com>
2020-09-20 13:01:19 +0800

    DirectiveInfo

--- src/main/java/graphql/DirectiveInfo.java
+++ src/main/java/graphql/DirectiveInfo.java
@@ -7,10 +7,20 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Info on all the directives provided by graphql specification
+ */
+@PublicApi
 public class DirectiveInfo {
 
+    /**
+     * A set of directives which provided by graphql specification
+     */
     public static final Set<GraphQLDirective> GRAPHQL_SPECIFICATION_DIRECTIVES = new LinkedHashSet();
 
+    /**
+     * A map from directive name to directive which provided by specification
+     */
     public static final Map<String, GraphQLDirective> GRAPHQL_SPECIFICATION_DIRECTIVE_MAP = new LinkedHashMap<>();
 
     static{
@@ -28,7 +38,15 @@ public class DirectiveInfo {
     }
 
 
+    /**
+     * Returns true if a directive with provided directiveName has been defined in graphql specification
+     *
+     * @param directiveName the name of directive in question
+     *
+     * @return true if the directive provided by graphql specification, and false otherwise
+     */
     public static boolean isGraphqlSpecifiedDirective(String directiveName) {
         return GRAPHQL_SPECIFICATION_DIRECTIVE_MAP.containsKey(directiveName);
     }
+
 }
