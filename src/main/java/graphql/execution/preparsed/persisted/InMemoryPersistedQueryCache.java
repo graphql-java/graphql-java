4a893b0f55da36cd97397ceb76634644ad658e55
Brad Baker <bbakerman@gmail.com>
2020-10-09 17:00:31 +1100

    Persisted query support (#2013)

---
+++ src/main/java/graphql/execution/preparsed/persisted/InMemoryPersistedQueryCache.java
@@ -0,0 +1,59 @@
+package graphql.execution.preparsed.persisted;
+
+import graphql.Assert;
+import graphql.ExecutionInput;
+import graphql.PublicApi;
+import graphql.execution.preparsed.PreparsedDocumentEntry;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A PersistedQueryCache that is just an in memory map of known queries.
+ */
+@PublicApi
+public class InMemoryPersistedQueryCache implements PersistedQueryCache {
+
+    private final Map<Object, PreparsedDocumentEntry> cache = new ConcurrentHashMap<>();
+    private final Map<Object, String> knownQueries;
+
+    public InMemoryPersistedQueryCache(Map<Object, String> knownQueries) {
+        this.knownQueries = Assert.assertNotNull(knownQueries);
+    }
+
+    public Map<Object, String> getKnownQueries() {
+        return knownQueries;
+    }
+
+    @Override
+    public PreparsedDocumentEntry getPersistedQueryDocument(Object persistedQueryId, ExecutionInput executionInput, PersistedQueryCacheMiss onCacheMiss) throws PersistedQueryNotFound {
+        return cache.compute(persistedQueryId, (k, v) -> {
+            if (v != null) {
+                return v;
+            }
+            String queryText = knownQueries.get(persistedQueryId);
+            if (queryText == null) {
+                throw new PersistedQueryNotFound(persistedQueryId);
+            }
+            return onCacheMiss.apply(queryText);
+        });
+    }
+
+    public static Builder newInMemoryPersistedQueryCache() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final Map<Object, String> knownQueries = new HashMap<>();
+
+        public Builder addQuery(Object key, String queryText) {
+            knownQueries.put(key, queryText);
+            return this;
+        }
+
+        public InMemoryPersistedQueryCache build() {
+            return new InMemoryPersistedQueryCache(knownQueries);
+        }
+    }
+}
