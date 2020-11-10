4213a3034aa18edc36207753ea274c3824593e1b
Brad Baker <bbakerman@gmail.com>
2020-11-10 20:34:22 +1100

    Makes a default data fetcher exception handler possible (#2093)

--- src/main/java/graphql/execution/ValueUnboxer.java
+++ src/main/java/graphql/execution/ValueUnboxer.java
@@ -2,9 +2,17 @@ package graphql.execution;
 
 import graphql.PublicSpi;
 
+/**
+ * A value unboxer takes values that are wrapped in classes like {@link java.util.Optional} / {@link java.util.OptionalInt} etc..
+ * and returns value from them.  You can provide your own implementation if you have your own specific
+ * holder classes.
+ */
 @PublicSpi
 public interface ValueUnboxer {
 
+    /**
+     * The default value unboxer handles JDK classes such as {@link java.util.Optional} and {@link java.util.OptionalInt} etc..
+     */
     ValueUnboxer DEFAULT = new DefaultValueUnboxer();
 
     /**
@@ -13,7 +21,6 @@ public interface ValueUnboxer {
      * unmodified
      *
      * @param object to unbox
-     *
      * @return unboxed object, or original if cannot unbox
      */
     Object unbox(final Object object);
