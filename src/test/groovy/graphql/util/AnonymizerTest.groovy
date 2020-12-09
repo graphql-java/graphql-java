31d2503b51a3f7aeaafd3c2b341a3b103ae08840
andimarek <andimarek@fastmail.fm>
2020-12-09 18:00:44 +1100

    Add anonymizer to make anonymous version of a schema and queries for that schema

---
+++ src/test/groovy/graphql/util/AnonymizerTest.groovy
@@ -0,0 +1,79 @@
+package graphql.util
+
+import graphql.TestUtil
+import graphql.schema.idl.SchemaPrinter
+import spock.lang.Specification
+
+class AnonymizerTest extends Specification {
+
+    def "simple schema and query"() {
+        given:
+        def schema = TestUtil.schema("""
+        type Query {
+            foo: Foo
+        }
+        type Foo {
+            bar1: String
+            bar2: ID
+        }
+        """)
+        def query = "{foo{bar1 bar2}}"
+
+        when:
+        def result = Anonymizer.anonymizeSchemaAndQueries(schema, [query])
+        def newSchema = new SchemaPrinter(SchemaPrinter.Options.defaultOptions().includeDirectiveDefinitions(false)).print(result.schema)
+        def newQuery = result.queries[0]
+
+        then:
+        newSchema == """schema {
+  query: Object1
+}
+
+type Object1 {
+  field1: Object2
+}
+
+type Object2 {
+  field2: String
+  field3: ID
+}
+"""
+        newQuery == "query {field1 {field2 field3}}"
+    }
+
+    def "query with fragments"() {
+        given:
+        def schema = TestUtil.schema("""
+        type Query {
+            foo: Foo
+        }
+        type Foo {
+            bar1: String
+            bar2: ID
+        }
+        """)
+        def query = "{...MyFragment foo {bar1 bar2}} fragment MyFragment on Query{foo {bar1 bar2 }}"
+
+        when:
+        def result = Anonymizer.anonymizeSchemaAndQueries(schema, [query])
+        def newSchema = new SchemaPrinter(SchemaPrinter.Options.defaultOptions().includeDirectiveDefinitions(false)).print(result.schema)
+        def newQuery = result.queries[0]
+
+        then:
+        newSchema == """schema {
+  query: Object1
+}
+
+type Object1 {
+  field1: Object2
+}
+
+type Object2 {
+  field2: String
+  field3: ID
+}
+"""
+        newQuery == "query {...Fragment1 field1 {field2 field3}} fragment Fragment1 on Object1 {field1 {field2 field3}}"
+
+    }
+}
