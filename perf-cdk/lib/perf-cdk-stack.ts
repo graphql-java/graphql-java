e922c733a3c16023a88a2ed00337ddabac242c0c
Andreas Marek <andimarek@fastmail.fm>
2021-05-30 08:43:06 +1000

    perf-testing wip

---
+++ perf-cdk/lib/perf-cdk-stack.ts
@@ -0,0 +1,76 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import * as fs from 'fs'
+
+export class PerfCdkStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        const defaultVpc = ec2.Vpc.fromLookup(this, 'VPC', {isDefault: true})
+        const role = new iam.Role(
+            this,
+            'simple-instance-1-role', // this is a unique id that will represent this resource in a Cloudformation template
+            {assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')}
+        )
+
+        // lets create a security group for our instance
+        // A security group acts as a virtual firewall for your instance to control inbound and outbound traffic.
+        const securityGroup = new ec2.SecurityGroup(
+            this,
+            'simple-instance-1-sg',
+            {
+                vpc: defaultVpc,
+                allowAllOutbound: true, // will let your instance send outboud traffic
+                securityGroupName: 'simple-instance-1-sg',
+            }
+        )
+
+        // lets use the security group to allow inbound traffic on specific ports
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(22),
+            'Allows SSH access from Internet'
+        )
+
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(80),
+            'Allows HTTP access from Internet'
+        )
+
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(443),
+            'Allows HTTPS access from Internet'
+        )
+
+        // Finally lets provision our ec2 instance
+        const instance = new ec2.Instance(this, 'simple-instance-1', {
+            vpc: defaultVpc,
+            role: role,
+            securityGroup: securityGroup,
+            instanceName: 'simple-instance-1',
+            instanceType: ec2.InstanceType.of( // t2.micro has free tier usage in aws
+                ec2.InstanceClass.T2,
+                ec2.InstanceSize.MICRO
+            ),
+            machineImage: ec2.MachineImage.latestAmazonLinux({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            }),
+
+            keyName: 'simple-instance-1-key',
+        })
+        instance.addUserData(
+            fs.readFileSync('lib/setup.sh', 'utf8')
+        )
+
+        // cdk lets us output prperties of the resources we create after they are created
+        // we want the ip address of this new instance so we can ssh into it later
+        new cdk.CfnOutput(this, 'simple-instance-1-output', {
+            value: instance.instancePublicIp
+        })
+
+
+    }
+}
+
---
+++ perf-cdk/lib/setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+sudo yum -y update
+curl -s "https://get.sdkman.io" | bash
+sdk install java 11.0.11.hs-adpt
