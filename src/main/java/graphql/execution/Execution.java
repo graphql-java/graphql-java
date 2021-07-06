9dd108599f428451b76b0f6561996292e294d879
Romain Liautaud <romain@nabla.com>
2021-07-06 18:33:59 +0200

    Introduce ExecutionContext.getStrategy.

--- src/main/java/graphql/execution/Execution.java
+++ src/main/java/graphql/execution/Execution.java
@@ -152,14 +152,7 @@ public class Execution {
 
         CompletableFuture<ExecutionResult> result;
         try {
-            ExecutionStrategy executionStrategy;
-            if (operation == OperationDefinition.Operation.MUTATION) {
-                executionStrategy = executionContext.getMutationStrategy();
-            } else if (operation == SUBSCRIPTION) {
-                executionStrategy = executionContext.getSubscriptionStrategy();
-            } else {
-                executionStrategy = executionContext.getQueryStrategy();
-            }
+            ExecutionStrategy executionStrategy = executionContext.getStrategy(operation);
             if (logNotSafe.isDebugEnabled()) {
                 logNotSafe.debug("Executing '{}' query operation: '{}' using '{}' execution strategy", executionContext.getExecutionId(), operation, executionStrategy.getClass().getName());
             }
