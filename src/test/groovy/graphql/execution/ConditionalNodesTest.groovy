b91396bb8a13c06d2d209d8391052fee5b5b8742
Chris Schenk <cschenk@twitter.com>
2021-10-18 12:42:24 -0700

    Adds test coverage for ConditionalNodes allowing missing variables

--- src/test/groovy/graphql/execution/ConditionalNodesTest.groovy
+++ src/test/groovy/graphql/execution/ConditionalNodesTest.groovy
@@ -1,5 +1,6 @@
 package graphql.execution
 
+import graphql.AssertException
 import graphql.Directives
 import graphql.language.Argument
 import graphql.language.BooleanValue
@@ -32,5 +33,51 @@ class ConditionalNodesTest extends Specification {
         expect:
         conditionalNodes.shouldInclude(variables, [])
 
+    }
+
+    def "throws if no variable present for condition"() {
+        given:
+        def variables = new LinkedHashMap<String, Object>()
+        ConditionalNodes conditionalNodes = new ConditionalNodes()
+        conditionalNodes.valuesResolver = Mock(ValuesResolver)
+
+        def argument = Argument.newArgument("if", new BooleanValue(true)).build()
+        def directives = [Directive.newDirective().name("skip").arguments([argument]).build()]
+
+        conditionalNodes.valuesResolver.getArgumentValues(Directives.SkipDirective.getArguments(), [argument], variables) >> [:]
+
+        when:
+        conditionalNodes.shouldInclude(variables, directives)
+
+        then:
+        thrown AssertException
+    }
+
+    def "returns default value for condition if allowMissingVariables is true and variable missing"() {
+        given:
+        def variables = new LinkedHashMap<String, Object>()
+        ConditionalNodes conditionalNodes = new ConditionalNodes()
+        conditionalNodes.allowMissingVariables(true)
+        conditionalNodes.valuesResolver = Mock(ValuesResolver)
+
+        def argument = Argument.newArgument("if", new BooleanValue(true)).build()
+        def directives = [Directive.newDirective().name(directiveName).arguments([argument]).build()]
+
+        conditionalNodes.valuesResolver.getArgumentValues(Directives.SkipDirective.getArguments(), [argument], variables) >> condition
+
+        when:
+        def shouldInclude = conditionalNodes.shouldInclude(variables, directives)
+
+        then:
+        shouldInclude == expected
+
+        where:
+        directiveName | condition     | expected
+        "include"     | ["if": false] | false
+        "include"     | ["if": true]  | true
+        "include"     | [:]           | true
+        "skip"        | ["if": false] | true
+        "skip"        | ["if": true]  | false
+        "skip"        | [:]           | true
     }
 }
