4ba5410c123224fbc7ac515f43cc54d5d9128aa9
alex079 <>
2022-04-20 11:00:32 +0200

    rename test class

--- src/test/groovy/graphql/ComposedDataLoadersTest.groovy
+++ src/test/groovy/graphql/ComposedDataLoadersTest.groovy
@@ -13,7 +13,7 @@ import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
 import static graphql.schema.GraphQLObjectType.newObject
 import static java.util.concurrent.CompletableFuture.completedFuture
 
-class ChainedDataLoadersTest extends Specification {
+class ComposedDataLoadersTest extends Specification {
 
     private static final def QUERY = '{ level1field1 { level2field1 level2field2 } }'
 
@@ -23,38 +23,38 @@ class ChainedDataLoadersTest extends Specification {
             .register('loader', DataLoaderFactory.newDataLoader(LOADER))
             .build()
 
-    private static def dummyQuerySubtype = newObject()
+    private static final def DUMMY_QUERY_SUBTYPE = newObject()
             .name('Subtype')
             .field(newFieldDefinition()
                     .name('level2field1')
                     .type(GraphQLString)
                     .dataFetcher(environment -> {
-                        DataLoader<Integer, Integer> loader = REGISTRY.getDataLoader('loader')
+                        DataLoader<Integer, Integer> loader = environment.getDataLoader('loader')
                         loader.load(1).thenCompose(loader::load)
                     }))
             .field(newFieldDefinition()
                     .name('level2field2')
                     .type(GraphQLInt)
                     .dataFetcher(environment -> {
-                        DataLoader<Integer, Integer> loader = REGISTRY.getDataLoader('loader')
+                        DataLoader<Integer, Integer> loader = environment.getDataLoader('loader')
                         loader.load(5).thenCompose(loader::load)
                     }))
             .build()
 
-    private static def dummyQueryType = newObject()
+    private static final def DUMMY_QUERY_TYPE = newObject()
             .name('Query')
             .field(newFieldDefinition()
                     .name('level1field1')
-                    .type(dummyQuerySubtype)
+                    .type(DUMMY_QUERY_SUBTYPE)
                     .dataFetcher(environment -> {
-                        DataLoader<Integer, Integer> loader = REGISTRY.getDataLoader('loader')
+                        DataLoader<Integer, Integer> loader = environment.getDataLoader('loader')
                         loader.load(11).thenCompose(loader::load).thenApply {[:]}
                     })
             .build())
 
     def 'composed data loaders must complete to finish field fetching level'() {
         given:
-        def graphQL = GraphQL.newGraphQL(GraphQLSchema.newSchema().query(dummyQueryType).build()).build()
+        def graphQL = GraphQL.newGraphQL(GraphQLSchema.newSchema().query(DUMMY_QUERY_TYPE).build()).build()
         when:
         def result = graphQL.execute(ExecutionInput.newExecutionInput(QUERY).dataLoaderRegistry(REGISTRY).build()).data
         then:
