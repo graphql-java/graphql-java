dc93e0da458ae4abcdfe8fa90cbfc39996dcfb2d
Brad Baker <bbaker@atlassian.com>
2022-09-10 17:21:11 +1000

    WIP - lightweight data fetchers

--- src/test/java/benchmark/LightWeightPropertyFetchingBenchMark.java
+++ src/test/java/benchmark/LightWeightPropertyFetchingBenchMark.java
@@ -5,7 +5,6 @@ import graphql.GraphQL;
 import graphql.execution.ExecutionStepInfo;
 import graphql.execution.instrumentation.tracing.TracingInstrumentation;
 import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -22,20 +21,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
 /**
- * See https://github.com/openjdk/jmh/tree/master/jmh-samples/src/main/java/org/openjdk/jmh/samples/ for more samples
+ * See <a href="https://github.com/openjdk/jmh/tree/master/jmh-samples/src/main/java/org/openjdk/jmh/samples/">...</a> for more samples
  * on what you can do with JMH
  * <p>
- * You MUST have the JMH plugin for IDEA in place for this to work :  https://github.com/artyushov/idea-jmh-plugin
+ * You MUST have the JMH plugin for IDEA in place for this to work :  <a href="https://github.com/artyushov/idea-jmh-plugin">...</a>
  * <p>
  * Install it and then just hit "Run" on a certain benchmark method
  */
-@Warmup(iterations = 2, time = 5, batchSize = 3)
-@Measurement(iterations = 3, time = 10, batchSize = 4)
+@Warmup(iterations = 1, time = 2, batchSize = 3)
+@Measurement(iterations = 3, time = 5, batchSize = 3)
 public class LightWeightPropertyFetchingBenchMark {
 
 
@@ -81,11 +81,20 @@ public class LightWeightPropertyFetchingBenchMark {
         graphQL.execute(query);
     }
 
+    private static final CharacterDTO R2D2 = CharacterDTO.mkCharacter(0, 20, "r2d2", NUMBER_OF_FRIENDS);
+
+
+    @SuppressWarnings("unused")
     private static GraphQL buildGraphQL() {
         InputStream sdl = LightWeightPropertyFetchingBenchMark.class.getClassLoader().getResourceAsStream("starWarsSchema.graphqls");
-        TypeDefinitionRegistry definitionRegistry = new SchemaParser().parse(new InputStreamReader(sdl));
+        TypeDefinitionRegistry definitionRegistry = new SchemaParser().parse(new InputStreamReader(Objects.requireNonNull(sdl)));
 
-        DataFetcher<?> heroDataFetcher = environment -> CharacterDTO.mkCharacter(environment, "r2d2", NUMBER_OF_FRIENDS);
+        DataFetcher<?> heroDataFetcher = environment -> {
+            Object sideEffect = environment.getArgument("episode");
+            ExecutionStepInfo anotherSideEffect = environment.getExecutionStepInfo();
+
+            return R2D2;
+        };
 
         RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
                 .type(
@@ -126,12 +135,13 @@ public class LightWeightPropertyFetchingBenchMark {
             return friends;
         }
 
-        public static CharacterDTO mkCharacter(DataFetchingEnvironment environment, String name, int friendCount) {
-            Object sideEffect = environment.getArgument("episode");
-            ExecutionStepInfo anotherSideEffect = environment.getExecutionStepInfo();
+        public static CharacterDTO mkCharacter(int depth, int maxDepth, String name, int friendCount) {
+            if (depth > maxDepth) {
+                return null;
+            }
             List<CharacterDTO> friends = new ArrayList<>(friendCount);
             for (int i = 0; i < friendCount; i++) {
-                friends.add(mkCharacter(environment, "friend" + i, 0));
+                friends.add(mkCharacter(depth + 1, maxDepth, "friend" + i, 0));
             }
             return new CharacterDTO(name, friends);
         }
