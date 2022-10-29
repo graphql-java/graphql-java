bb4a3b2be565b457c5a8cd353af3351882372aa2
Aaron Paterson <apaterson@protonmail.com>
2022-10-28 21:38:38 -0600

    Parameterized introspection queries (#2993)
    * use ast builders and stub test
    
    * remove main and add test
    
    * restore jvm check
    
    * revert test spacing
    
    * fix test
    
    * revers build.gradle
    
    * use spec specifiedByURL capitalization
    
    * update another specifiedByURL test
    
    * refactor introspection query builder, restore and deprecate __Type.specifiedByUrl
    
    * test that AST printed introspection query is equivalent to original string

--- src/test/groovy/graphql/GraphQLTest.groovy
+++ src/test/groovy/graphql/GraphQLTest.groovy
@@ -1251,10 +1251,10 @@ many lines''']
         GraphQLSchema schema = TestUtil.schema('type Query {foo: MyScalar} scalar MyScalar @specifiedBy(url:"myUrl")')
 
         when:
-        def result = GraphQL.newGraphQL(schema).build().execute('{__type(name: "MyScalar") {name specifiedByUrl}}').getData()
+        def result = GraphQL.newGraphQL(schema).build().execute('{__type(name: "MyScalar") {name specifiedByURL}}').getData()
 
         then:
-        result == [__type: [name: "MyScalar", specifiedByUrl: "myUrl"]]
+        result == [__type: [name: "MyScalar", specifiedByURL: "myUrl"]]
     }
 
     def "test DFR and CF"() {
