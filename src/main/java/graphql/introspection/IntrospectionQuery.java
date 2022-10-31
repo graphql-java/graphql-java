71eccd499e72a0692860c556dadbb43a37f4b28e
Brad Baker <bbaker@atlassian.com>
2022-10-31 17:57:50 +1100

    Document the new IntrospectionQueryBuilder and tweaked it a little (#3003)

--- src/main/java/graphql/introspection/IntrospectionQuery.java
+++ src/main/java/graphql/introspection/IntrospectionQuery.java
@@ -4,5 +4,10 @@ import graphql.PublicApi;
 
 @PublicApi
 public interface IntrospectionQuery {
+    /**
+     * This is the default introspection query provided by graphql-java
+     *
+     * @see IntrospectionQueryBuilder for ways to customize the introspection query
+     */
     String INTROSPECTION_QUERY = IntrospectionQueryBuilder.build();
 }
