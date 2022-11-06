5c0c68ba15bc06f0783f010f73dc16665d79fada
Davide Angelocola <dfa1@users.noreply.github.com>
2022-11-06 22:36:11 +0100

    Fixes in TwitterBenchmark (#3006)
    * Remove unused imports and warnings from TwitterBenchmark
    
    * Configure ParserOptions for TwitterBenchmark
    
    * Fix javadoc of PersistedQueryCache

--- src/main/java/graphql/execution/preparsed/persisted/PersistedQueryCache.java
+++ src/main/java/graphql/execution/preparsed/persisted/PersistedQueryCache.java
@@ -19,14 +19,14 @@ public interface PersistedQueryCache {
      * If its present in cache then  it must return a PreparsedDocumentEntry where {@link graphql.execution.preparsed.PreparsedDocumentEntry#getDocument()}
      * is already parsed and validated.  This will be passed onto the graphql engine as is.
      * <p>
-     * If its a valid query id but its no present in cache, (cache miss) then you need to call back the "onCacheMiss" function with associated query text.
-     * This will be compiled and validated by the graphql engine and the a PreparsedDocumentEntry will be passed back ready for you to cache it.
+     * If it's a valid query id but its no present in cache, (cache miss) then you need to call back the "onCacheMiss" function with associated query text.
+     * This will be compiled and validated by the graphql engine and the PreparsedDocumentEntry will be passed back ready for you to cache it.
      * <p>
-     * If its not a valid query id then throw a {@link graphql.execution.preparsed.persisted.PersistedQueryNotFound} to indicate this.
+     * If it's not a valid query id then throw a {@link graphql.execution.preparsed.persisted.PersistedQueryNotFound} to indicate this.
      *
      * @param persistedQueryId the persisted query id
      * @param executionInput   the original execution input
-     * @param onCacheMiss      the call back should it be a valid query id but its not currently not in the cache
+     * @param onCacheMiss      the call back should it be a valid query id but it's not currently in the cache
      * @return a parsed and validated PreparsedDocumentEntry where {@link graphql.execution.preparsed.PreparsedDocumentEntry#getDocument()} is set
      * @throws graphql.execution.preparsed.persisted.PersistedQueryNotFound if the query id is not know at all and you have no query text
      *
@@ -42,14 +42,14 @@ public interface PersistedQueryCache {
      * If its present in cache then  it must return a PreparsedDocumentEntry where {@link graphql.execution.preparsed.PreparsedDocumentEntry#getDocument()}
      * is already parsed and validated.  This will be passed onto the graphql engine as is.
      * <p>
-     * If its a valid query id but its no present in cache, (cache miss) then you need to call back the "onCacheMiss" function with associated query text.
-     * This will be compiled and validated by the graphql engine and the a PreparsedDocumentEntry will be passed back ready for you to cache it.
+     * If it's a valid query id but its no present in cache, (cache miss) then you need to call back the "onCacheMiss" function with associated query text.
+     * This will be compiled and validated by the graphql engine and the PreparsedDocumentEntry will be passed back ready for you to cache it.
      * <p>
-     * If its not a valid query id then throw a {@link graphql.execution.preparsed.persisted.PersistedQueryNotFound} to indicate this.
+     * If it's not a valid query id then throw a {@link graphql.execution.preparsed.persisted.PersistedQueryNotFound} to indicate this.
      *
      * @param persistedQueryId the persisted query id
      * @param executionInput   the original execution input
-     * @param onCacheMiss      the call back should it be a valid query id but its not currently not in the cache
+     * @param onCacheMiss      the call back should it be a valid query id but it's not currently in the cache
      * @return a promise to parsed and validated {@link PreparsedDocumentEntry} where {@link graphql.execution.preparsed.PreparsedDocumentEntry#getDocument()} is set
      * @throws graphql.execution.preparsed.persisted.PersistedQueryNotFound if the query id is not know at all and you have no query text
      */
