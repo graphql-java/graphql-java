57eca2eb67d3f927873f396a9822b6195d4dcbb0
Federico Rispo <44871614+federicorispo@users.noreply.github.com>
2022-11-25 04:49:24 +0100

    Update ValidationError#toString to print the extentions field  (#3024)
    * chore: Update ValidationError#toString to print the extentions field
    
     - Updated toString method in ValidationError class to print the extensions field. If the field is null then an empty
       array will be printed `...extensions=[]`
     - Added a couple of UTs to test the ValidationError#toString method
    
    * chore: Remove message field in ValidationError and initialized the queryPath and extensions fields
    
    * Replace HashMap with ImmutableMap

---
+++ src/test/groovy/graphql/validation/ValidationErrorToString.groovy
@@ -0,0 +1,39 @@
+package graphql.validation
+
+import graphql.language.SourceLocation
+import spock.lang.Specification
+
+class ValidationErrorToString extends Specification {
+
+    def 'toString prints correctly ValidationError object when all fields are initialized'() {
+        given:
+        def sourceLocations = [new SourceLocation(5, 0), new SourceLocation(10, 1)]
+        def description = "Validation Error (UnknownType)"
+        def validationErrorClassification = ValidationErrorType.UnknownType
+        def queryPath = ["home", "address"]
+        def extensions = ["extension1": "first", "extension2": true, "extension3": 2]
+
+        when:
+        def validationError = ValidationError
+                .newValidationError()
+                .sourceLocations(sourceLocations)
+                .description(description)
+                .validationErrorType(validationErrorClassification)
+                .queryPath(queryPath)
+                .extensions(extensions)
+                .build()
+
+        then:
+        validationError.toString() == "ValidationError{validationErrorType=UnknownType, queryPath=[home, address], message=Validation Error (UnknownType), locations=[SourceLocation{line=5, column=0}, SourceLocation{line=10, column=1}], description='Validation Error (UnknownType)', extensions=[extension1=first, extension2=true, extension3=2]}"
+    }
+
+    def 'toString prints correctly ValidationError object when all fields are empty'() {
+       when:
+        def validationError = ValidationError
+                .newValidationError()
+                .build()
+
+        then:
+        validationError.toString() == "ValidationError{validationErrorType=null, queryPath=[], message=null, locations=[], description='null', extensions=[]}"
+    }
+}
