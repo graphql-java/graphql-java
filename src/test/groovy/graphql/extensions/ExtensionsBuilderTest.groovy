d1047c421fe4549cfd92d9d1b737a0e1ba83954d
Brad Baker <bbaker@atlassian.com>
2022-12-09 19:53:53 +1100

    Extensions Builder - more work - integration test

--- src/test/groovy/graphql/extensions/ExtensionsBuilderTest.groovy
+++ src/test/groovy/graphql/extensions/ExtensionsBuilderTest.groovy
@@ -1,10 +1,22 @@
 package graphql.extensions
 
+import graphql.ExecutionInput
 import graphql.ExecutionResult
+import graphql.TestUtil
+import graphql.execution.instrumentation.InstrumentationState
+import graphql.execution.instrumentation.SimplePerformantInstrumentation
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import graphql.schema.GraphQLTypeUtil
 import org.jetbrains.annotations.NotNull
 import spock.lang.Specification
 
+import java.util.concurrent.CompletableFuture
+
 import static graphql.extensions.ExtensionsBuilder.newExtensionsBuilder
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring
 
 class ExtensionsBuilderTest extends Specification {
 
@@ -73,5 +85,63 @@ class ExtensionsBuilderTest extends Specification {
                 .addValues([the: "end"]).buildExtensions()
         then:
         extensions == [the: "end"]
+    }
+
+    def "integration test that shows it working"() {
+        def sdl = """
+        type Query {
+            name : String!
+            street : String
+            id : ID!
+        }
+        """
+
+        def contextInstrumentation = new SimplePerformantInstrumentation() {
+            @Override
+            CompletableFuture<ExecutionResult> instrumentExecutionResult(ExecutionResult executionResult, InstrumentationExecutionParameters parameters, InstrumentationState state) {
+                ExtensionsBuilder extensionsBuilder = parameters.getGraphQLContext().get(ExtensionsBuilder.class)
+                def newEr = extensionsBuilder.setExtensions(executionResult)
+                return CompletableFuture.completedFuture(newEr)
+            }
+        }
+
+        DataFetcher df = { DataFetchingEnvironment env ->
+            ExtensionsBuilder extensionsBuilder = env.getGraphQlContext().get(ExtensionsBuilder.class)
+            def fieldMap = [:]
+            fieldMap.put(env.getFieldDefinition().name,GraphQLTypeUtil.simplePrint(env.getFieldDefinition().type))
+            extensionsBuilder.addValues([common: fieldMap])
+            extensionsBuilder.addValues(fieldMap)
+            return "ignored"
+        }
+
+        def ei = ExecutionInput.newExecutionInput("query q { name street id }")
+                .graphQLContext( { ctx -> ctx.put(ExtensionsBuilder.class, newExtensionsBuilder()) })
+                .build()
+
+
+        def graphQL = TestUtil.graphQL(sdl, newRuntimeWiring()
+                .type(newTypeWiring("Query").dataFetchers([
+                        name  : df,
+                        street: df,
+                        id    : df,
+                ])))
+                .instrumentation(contextInstrumentation)
+                .build()
+
+        when:
+        def er = graphQL.execute(ei)
+        then:
+        er.errors.isEmpty()
+        er.extensions == [
+                common: [
+                        name  : "String!",
+                        street: "String",
+                        id    : "ID!",
+                ],
+                // we break them out so we have common and not common entries
+                name  : "String!",
+                street: "String",
+                id    : "ID!",
+        ]
     }
 }
