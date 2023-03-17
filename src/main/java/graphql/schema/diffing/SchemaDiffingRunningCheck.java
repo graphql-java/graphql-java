53e84169b2ed9e8f6afb146704b09ec21ddb3b38
Franklin Wang <fwang@atlassian.com>
2023-03-17 16:24:10 +1300

    Remove check on Thread.interrupted

--- src/main/java/graphql/schema/diffing/SchemaDiffingRunningCheck.java
+++ src/main/java/graphql/schema/diffing/SchemaDiffingRunningCheck.java
@@ -6,9 +6,6 @@ class SchemaDiffingRunningCheck {
     private final AtomicBoolean wasStopped = new AtomicBoolean(false);
 
     void check() {
-        if (Thread.interrupted()) {
-            throw new SchemaDiffingCancelledException(true);
-        }
         if (wasStopped.get()) {
             throw new SchemaDiffingCancelledException(false);
         }
