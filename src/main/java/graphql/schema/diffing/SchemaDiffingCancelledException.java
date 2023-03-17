8fb6ffadd51da835f314aa0b55f90ba51e219cd1
Franklin Wang <fwang@atlassian.com>
2023-03-17 16:23:45 +1300

    Mark as internal

--- src/main/java/graphql/schema/diffing/SchemaDiffingCancelledException.java
+++ src/main/java/graphql/schema/diffing/SchemaDiffingCancelledException.java
@@ -1,5 +1,8 @@
 package graphql.schema.diffing;
 
+import graphql.Internal;
+
+@Internal
 public class SchemaDiffingCancelledException extends RuntimeException {
     SchemaDiffingCancelledException(boolean byInterrupt) {
         super("Schema diffing job was cancelled by " + (byInterrupt ? "thread interrupt" : "stop call"));
