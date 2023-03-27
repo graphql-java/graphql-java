3d1a2082112a5b3d1212af5da6fe0b35f06362e4
Brad Baker <bbaker@atlassian.com>
2023-03-27 14:51:24 +1100

    This has a listener to show what tests failed - @ignored

--- src/test/groovy/graphql/AlwaysFailsTest.groovy
+++ src/test/groovy/graphql/AlwaysFailsTest.groovy
@@ -1,11 +1,13 @@
 package graphql
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * This is only really useful for testing the gradle builds etc...
  * and in general would not be needed to test graphql-java
  */
+@Ignore
 class AlwaysFailsTest extends Specification{
 
     def "this test fails"() {
