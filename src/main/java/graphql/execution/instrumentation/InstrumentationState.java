d47b189e089a0b6b42b607bd808eeb39ff56d18e
Brad Baker <bbaker@atlassian.com>
2024-02-20 16:23:16 +1100

    javadoc problem

--- src/main/java/graphql/execution/instrumentation/InstrumentationState.java
+++ src/main/java/graphql/execution/instrumentation/InstrumentationState.java
@@ -1,12 +1,13 @@
 package graphql.execution.instrumentation;
 
 import graphql.PublicSpi;
+import graphql.execution.instrumentation.parameters.InstrumentationCreateStateParameters;
 
 /**
  * An {@link Instrumentation} implementation can create this as a stateful object that is then passed
  * to each instrumentation method, allowing state to be passed down with the request execution
  *
- * @see Instrumentation#createState(graphql.execution.instrumentation.parameters.InstrumentationCreateStateParameters)
+ * @see Instrumentation#createStateAsync(InstrumentationCreateStateParameters)
  */
 @PublicSpi
 public interface InstrumentationState {
