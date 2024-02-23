d778868655d875843a33796f2dd96e6ffb5c3982
dondonz <13839920+dondonz@users.noreply.github.com>
2024-02-23 18:22:11 +1100

    Add extensionDefinitions to schema builder

--- src/main/java/graphql/schema/GraphQLSchema.java
+++ src/main/java/graphql/schema/GraphQLSchema.java
@@ -641,6 +641,7 @@ public class GraphQLSchema {
                 .query(existingSchema.getQueryType())
                 .mutation(existingSchema.getMutationType())
                 .subscription(existingSchema.getSubscriptionType())
+                .extensionDefinitions(existingSchema.getExtensionDefinitions())
                 .introspectionSchemaType(existingSchema.getIntrospectionSchemaType())
                 .codeRegistry(existingSchema.getCodeRegistry())
                 .clearAdditionalTypes()
