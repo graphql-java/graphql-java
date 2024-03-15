40eae76e710fe123efdfa3c768a39a5f0ff16454
Brad Baker <bbaker@atlassian.com>
2024-03-16 08:24:20 +1100

    This provides and ability to disable Introspection - deprecate NoIntrospectionGraphqlFieldVisibility

--- src/main/java/graphql/schema/visibility/NoIntrospectionGraphqlFieldVisibility.java
+++ src/main/java/graphql/schema/visibility/NoIntrospectionGraphqlFieldVisibility.java
@@ -12,10 +12,15 @@ import static graphql.schema.visibility.BlockedFields.newBlock;
  * This field visibility will prevent Introspection queries from being performed.  Technically this puts your
  * system in contravention of <a href="https://spec.graphql.org/October2021/#sec-Introspection">the specification</a>
  * but some production systems want this lock down in place.
+ *
+ * @deprecated This is no longer the best way to prevent Introspection - {@link graphql.introspection.Introspection#enabledJvmWide(boolean)}
+ * can be used instead
  */
 @PublicApi
+@Deprecated(since = "2024-03-16")
 public class NoIntrospectionGraphqlFieldVisibility implements GraphqlFieldVisibility {
 
+    @Deprecated(since = "2024-03-16")
     public static NoIntrospectionGraphqlFieldVisibility NO_INTROSPECTION_FIELD_VISIBILITY = new NoIntrospectionGraphqlFieldVisibility();
 
 
