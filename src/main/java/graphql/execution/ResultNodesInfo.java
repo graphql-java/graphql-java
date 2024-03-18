80fcbb96be990b528bec306c9a9c2dc078b895fa
Andreas Marek <andimarek@fastmail.fm>
2024-03-18 13:33:53 +1000

    javadoc

--- src/main/java/graphql/execution/ResultNodesInfo.java
+++ src/main/java/graphql/execution/ResultNodesInfo.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * After each execution the GraphQLContext contains a ResultNodeInfo object under the key {@link ResultNodesInfo#RESULT_NODES_INFO}
  * <p>
  * The number of result can be limited (and should be for security reasons) by setting the maximum number of result nodes
- * in the GraphQLContext under the key {@link ResultNodesInfo#MAX_RESULT_NODES}
+ * in the GraphQLContext under the key {@link ResultNodesInfo#MAX_RESULT_NODES} of type Integer.
  * </p>
  */
 @PublicApi
@@ -36,7 +36,7 @@ public class ResultNodesInfo {
      * The number of result nodes created.
      * Note: this can be higher than max result nodes because
      * a each node that exceeds the number of max nodes is set to null,
-     * but still is a result node (which value null)
+     * but still is a result node (with value null)
      *
      * @return number of result nodes created
      */
