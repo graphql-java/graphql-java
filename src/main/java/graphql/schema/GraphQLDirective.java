479440ddf254fe2f7e410e9200ace1a7892edc47
James Bellenger <james.bellenger@airbnb.com>
2024-04-02 05:22:35 -0700

    fix java wildcard imports

--- src/main/java/graphql/schema/GraphQLDirective.java
+++ src/main/java/graphql/schema/GraphQLDirective.java
@@ -7,7 +7,11 @@ import graphql.language.DirectiveDefinition;
 import graphql.util.TraversalControl;
 import graphql.util.TraverserContext;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
