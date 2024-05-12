07dd80717a30d1da2a4e163ea567e5fc60959996
dondonz <13839920+dondonz@users.noreply.github.com>
2024-05-12 18:10:41 +1000

    Add validation for directive and field arguments

--- src/main/java/graphql/schema/validation/DeprecatedInputObjectAndArgumentsAreValid.java
+++ src/main/java/graphql/schema/validation/DeprecatedInputObjectAndArgumentsAreValid.java
@@ -2,6 +2,9 @@ package graphql.schema.validation;
 
 import graphql.Internal;
 import graphql.schema.GraphQLAppliedDirective;
+import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLInputObjectField;
 import graphql.schema.GraphQLInputObjectType;
 import graphql.schema.GraphQLSchemaElement;
@@ -28,8 +31,31 @@ public class DeprecatedInputObjectAndArgumentsAreValid extends GraphQLTypeVisito
         if (deprecatedDirective != null && GraphQLTypeUtil.isNonNull(inputObjectField.getType()) && !inputObjectField.hasSetDefaultValue()) {
             GraphQLInputObjectType inputObjectType = (GraphQLInputObjectType) context.getParentNode();
             SchemaValidationErrorCollector errorCollector = context.getVarFromParents(SchemaValidationErrorCollector.class);
-            String message = format("Required input field %s.%s cannot be deprecated.", inputObjectType.getName(), inputObjectField.getName());
+            String message = format("Required input field '%s.%s' cannot be deprecated.", inputObjectType.getName(), inputObjectField.getName());
             errorCollector.addError(new SchemaValidationError(SchemaValidationErrorType.RequiredInputFieldCannotBeDeprecated, message));
+        }
+        return TraversalControl.CONTINUE;
+    }
+
+    // An argument can appear as either a field argument or a directive argument. This visitor will visit both field arguments and directive arguments.
+    // An applied directive's argument cannot be deprecated.
+    @Override
+    public TraversalControl visitGraphQLArgument(GraphQLArgument argument, TraverserContext<GraphQLSchemaElement> context) {
+        // There can only be at most one @deprecated, because it is not a repeatable directive
+        GraphQLAppliedDirective deprecatedDirective = argument.getAppliedDirective("deprecated");
+
+        if (deprecatedDirective != null && GraphQLTypeUtil.isNonNull(argument.getType()) && !argument.hasSetDefaultValue()) {
+            if (context.getParentNode() instanceof GraphQLFieldDefinition) {
+                GraphQLFieldDefinition fieldDefinition = (GraphQLFieldDefinition) context.getParentNode();
+                SchemaValidationErrorCollector errorCollector = context.getVarFromParents(SchemaValidationErrorCollector.class);
+                String message = format("Required argument '%s' on field '%s' cannot be deprecated.", argument.getName(), fieldDefinition.getName());
+                errorCollector.addError(new SchemaValidationError(SchemaValidationErrorType.RequiredFieldArgumentCannotBeDeprecated, message));
+            } else if (context.getParentNode() instanceof GraphQLDirective) {
+                GraphQLDirective directive = (GraphQLDirective) context.getParentNode();
+                SchemaValidationErrorCollector errorCollector = context.getVarFromParents(SchemaValidationErrorCollector.class);
+                String message = format("Required argument '%s' on directive '%s' cannot be deprecated.", argument.getName(), directive.getName());
+                errorCollector.addError(new SchemaValidationError(SchemaValidationErrorType.RequiredDirectiveArgumentCannotBeDeprecated, message));
+            }
         }
         return TraversalControl.CONTINUE;
     }
