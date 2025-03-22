ee9570a5af23f390d23701ef6513d19e8544fd16
dondonz <13839920+dondonz@users.noreply.github.com>
2025-03-22 12:28:43 +1100

    Adjust tests to allow child datafetchers to be added again for the same type as long as they are not overlapping

--- src/test/groovy/graphql/schema/idl/RuntimeWiringTest.groovy
+++ src/test/groovy/graphql/schema/idl/RuntimeWiringTest.groovy
@@ -202,8 +202,7 @@ class RuntimeWiringTest extends Specification {
         when:
         RuntimeWiring.newRuntimeWiring()
                 .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("foo", DF1))
-                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("bar", DF1))
-
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("foo", DF1)) // Cannot redefine the same field's datafetcher
 
         then:
         def e1 = thrown(StrictModeWiringException)
@@ -243,6 +242,62 @@ class RuntimeWiringTest extends Specification {
         e5.message == "The type Foo has already has a default data fetcher defined"
     }
 
+    def "strict mode, on by default, permits a type to be defined more than once as long as elements are not overlapping"() {
+        DataFetcher DF1 = env -> "x"
+        DataFetcher DF2 = env -> "x"
+        TypeResolver TR1 = env -> null
+        EnumValuesProvider EVP1 = name -> null
+
+        when:
+        // Permit type wiring to be defined more than once, if child DataFetchers are for distinct fields
+        def runtimeWiring1 = RuntimeWiring.newRuntimeWiring()
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("foo", DF1))
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("bar", DF2))
+
+        then:
+        noExceptionThrown()
+        runtimeWiring1.getDataFetchers().get("Foo").get("foo") == DF1
+        runtimeWiring1.getDataFetchers().get("Foo").get("bar") == DF2
+
+        when:
+        // Only one type wiring is allowed per type, do not allow redefinition
+        RuntimeWiring.newRuntimeWiring()
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").typeResolver(TR1))
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").typeResolver(TR1))
+
+        then:
+        def e2 = thrown(StrictModeWiringException)
+        e2.message == "The type Foo already has a type resolver defined"
+
+        when:
+        // Only one enum values provider is allowed per type, do not allow redefinition
+        RuntimeWiring.newRuntimeWiring()
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").enumValues(EVP1))
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").enumValues(EVP1))
+
+        then:
+        def e3 = thrown(StrictModeWiringException)
+        e3.message == "The type Foo already has a enum provider defined"
+
+        when:
+        // Only one scalar wiring is allowed per scalar
+        RuntimeWiring.newRuntimeWiring()
+                .scalar(Scalars.GraphQLString)
+        then:
+        def e4 = thrown(StrictModeWiringException)
+        e4.message == "The scalar String is already defined"
+
+        when:
+        // Only one default data fetcher is allowed, do not allow redefinition
+        TypeRuntimeWiring.newTypeWiring("Foo")
+                .defaultDataFetcher(DF1)
+                .defaultDataFetcher(DF2)
+
+        then:
+        def e5 = thrown(StrictModeWiringException)
+        e5.message == "The type Foo has already has a default data fetcher defined"
+    }
+
     def "strict mode, if set to off, won't stop certain redefinitions"() {
         DataFetcher DF1 = env -> "x"
         DataFetcher DF2 = env -> "x"
@@ -253,7 +308,7 @@ class RuntimeWiringTest extends Specification {
         def runtimeWiring1 = RuntimeWiring.newRuntimeWiring()
                 .strictMode(false)
                 .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("foo", DF1))
-                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("bar", DF1))
+                .type(TypeRuntimeWiring.newTypeWiring("Foo").dataFetcher("foo", DF2))
                 .build()
 
         then:
