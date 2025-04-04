91f1967f5e3bfd21561e465b757b3d719b7b97a7
bbaker <bbaker@atlassian.com>
2025-04-04 15:26:38 +1100

    Removed cancel operation support

--- src/main/java/graphql/execution/EngineRunningObserver.java
+++ src/main/java/graphql/execution/EngineRunningObserver.java
@@ -1,6 +1,5 @@
 package graphql.execution;
 
-import graphql.ExecutionInput;
 import graphql.ExperimentalApi;
 import graphql.GraphQLContext;
 import org.jspecify.annotations.NullMarked;
@@ -9,8 +8,6 @@ import org.jspecify.annotations.NullMarked;
  * This class lets you observe the running state of the graphql-java engine.  As it processes and dispatches graphql fields,
  * the engine moves in and out of a running and not running state.  As it does this, the callback is called with information telling you the current
  * state.
- * <p>
- * If the engine is cancelled via {@link ExecutionInput#cancel()} then the observer will also be called to indicate that.
  */
 @ExperimentalApi
 @NullMarked
@@ -25,10 +22,6 @@ public interface EngineRunningObserver {
          * Represents that the engine code is asynchronously waiting for fetching to happen
          */
         NOT_RUNNING,
-        /**
-         * Represents that the engine code has been cancelled via {@link ExecutionInput#cancel()}
-         */
-        CANCELLED
     }
 
 
