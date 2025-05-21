3cafd37fd0ee01a25682e9580d6294cbb7c4a6cd
Felipe Reis <felipe.gdr@gmail.com>
2025-05-21 11:05:03 +1000

    Easy refactoring

--- src/main/java/graphql/util/querygenerator/QueryGeneratorFieldSelection.java
+++ src/main/java/graphql/util/querygenerator/QueryGeneratorFieldSelection.java
@@ -18,15 +18,17 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class QueryGeneratorFieldSelection {
     private final QueryGeneratorOptions options;
     private final GraphQLSchema schema;
 
-    private static final GraphQLObjectType emptyObjectType = GraphQLObjectType.newObject()
+    private static final GraphQLObjectType EMPTY_OBJECT_TYPE = GraphQLObjectType.newObject()
             .name("Empty")
             .build();
 
@@ -58,84 +60,12 @@ public class QueryGeneratorFieldSelection {
         fieldSelectionQueue.add(root);
 
         Set<FieldCoordinates> visited = new HashSet<>();
-        int totalFieldCount = 0;
+        AtomicInteger totalFieldCount = new AtomicInteger(0);
 
         while (!containersQueue.isEmpty()) {
-            List<GraphQLFieldsContainer> containers = containersQueue.poll();
-            FieldSelection fieldSelection = fieldSelectionQueue.poll();
+            processContainers(containersQueue, fieldSelectionQueue, visited, totalFieldCount);
 
-            for (GraphQLFieldsContainer container : containers) {
-
-                if (!options.getFilterFieldContainerPredicate().test(container)) {
-                    continue;
-                }
-
-                for (GraphQLFieldDefinition fieldDef : container.getFieldDefinitions()) {
-                    if (!options.getFilterFieldDefinitionPredicate().test(fieldDef)) {
-                        continue;
-                    }
-
-                    if (totalFieldCount >= options.getMaxFieldCount()) {
-                        break;
-                    }
-
-                    if (hasRequiredArgs(fieldDef)) {
-                        continue;
-                    }
-
-                    FieldCoordinates fieldCoordinates = FieldCoordinates.coordinates(container, fieldDef.getName());
-
-                    if (visited.contains(fieldCoordinates)) {
-                        continue;
-                    }
-
-                    GraphQLType unwrappedType = GraphQLTypeUtil.unwrapAll(fieldDef.getType());
-                    boolean isFieldContainer = unwrappedType instanceof GraphQLFieldsContainer;
-                    boolean isUnionType = unwrappedType instanceof GraphQLUnionType;
-                    boolean isInterfaceType = unwrappedType instanceof GraphQLInterfaceType;
-
-                    // TODO: This statement is kinda awful
-                    final FieldSelection newFieldSelection;
-
-                    if (isUnionType || isInterfaceType) {
-                        newFieldSelection = new FieldSelection(fieldDef.getName(), new HashMap<>(), true);
-                    } else if (isFieldContainer) {
-                        newFieldSelection = new FieldSelection(fieldDef.getName(), new HashMap<>(), false);
-                    } else {
-                        newFieldSelection = new FieldSelection(fieldDef.getName(), null, false);
-                    }
-
-
-                    fieldSelection.fieldsByContainer.computeIfAbsent(container.getName(), key -> new ArrayList<>()).add(newFieldSelection);
-
-                    fieldSelectionQueue.add(newFieldSelection);
-
-                    if (unwrappedType instanceof GraphQLInterfaceType) {
-                        GraphQLInterfaceType interfaceType = (GraphQLInterfaceType) unwrappedType;
-                        List<GraphQLFieldsContainer> possibleTypes = new ArrayList<>(schema.getImplementations(interfaceType));
-
-                        containersQueue.add(possibleTypes);
-                    } else if (isFieldContainer) {
-                        visited.add(fieldCoordinates);
-                        containersQueue.add(Collections.singletonList((GraphQLFieldsContainer) unwrappedType));
-                    } else if (isUnionType) {
-                        GraphQLUnionType unionType = (GraphQLUnionType) unwrappedType;
-                        List<GraphQLFieldsContainer> possibleTypes = unionType.getTypes().stream()
-                                .filter(possibleType -> possibleType instanceof GraphQLFieldsContainer)
-                                .map(possibleType -> (GraphQLFieldsContainer) possibleType)
-                                .collect(Collectors.toList());
-
-                        containersQueue.add(possibleTypes);
-                    } else {
-                        containersQueue.add(Collections.singletonList(emptyObjectType));
-                    }
-
-                    totalFieldCount++;
-                }
-            }
-
-
-            if (totalFieldCount >= options.getMaxFieldCount()) {
+            if (totalFieldCount.get() >= options.getMaxFieldCount()) {
                 break;
             }
         }
@@ -143,16 +73,110 @@ public class QueryGeneratorFieldSelection {
         return root;
     }
 
+    private void processContainers(Queue<List<GraphQLFieldsContainer>> containersQueue,
+                                   Queue<FieldSelection> fieldSelectionQueue,
+                                   Set<FieldCoordinates> visited,
+                                   AtomicInteger totalFieldCount) {
+        List<GraphQLFieldsContainer> containers = containersQueue.poll();
+        FieldSelection fieldSelection = fieldSelectionQueue.poll();
+
+        for (GraphQLFieldsContainer container : Objects.requireNonNull(containers)) {
+            if (!options.getFilterFieldContainerPredicate().test(container)) {
+                continue;
+            }
+
+            for (GraphQLFieldDefinition fieldDef : container.getFieldDefinitions()) {
+                if (!options.getFilterFieldDefinitionPredicate().test(fieldDef)) {
+                    continue;
+                }
+
+                if (totalFieldCount.get() >= options.getMaxFieldCount()) {
+                    break;
+                }
+
+                if (hasRequiredArgs(fieldDef)) {
+                    continue;
+                }
+
+                FieldCoordinates fieldCoordinates = FieldCoordinates.coordinates(container, fieldDef.getName());
+
+                if (visited.contains(fieldCoordinates)) {
+                    continue;
+                }
+
+                processField(
+                        container,
+                        fieldDef,
+                        Objects.requireNonNull(fieldSelection),
+                        containersQueue,
+                        fieldSelectionQueue,
+                        fieldCoordinates,
+                        visited,
+                        totalFieldCount
+                );
+            }
+        }
+    }
+
+    private void processField(GraphQLFieldsContainer container,
+                              GraphQLFieldDefinition fieldDef,
+                              FieldSelection fieldSelection,
+                              Queue<List<GraphQLFieldsContainer>> containersQueue,
+                              Queue<FieldSelection> fieldSelectionQueue,
+                              FieldCoordinates fieldCoordinates,
+                              Set<FieldCoordinates> visited,
+                              AtomicInteger totalFieldCount) {
+
+        GraphQLType unwrappedType = GraphQLTypeUtil.unwrapAll(fieldDef.getType());
+        FieldSelection newFieldSelection = getFieldSelection(fieldDef, unwrappedType);
+
+        fieldSelection.fieldsByContainer.computeIfAbsent(container.getName(), key -> new ArrayList<>()).add(newFieldSelection);
+
+        fieldSelectionQueue.add(newFieldSelection);
+
+        if (unwrappedType instanceof GraphQLInterfaceType) {
+            GraphQLInterfaceType interfaceType = (GraphQLInterfaceType) unwrappedType;
+            List<GraphQLFieldsContainer> possibleTypes = new ArrayList<>(schema.getImplementations(interfaceType));
+
+            containersQueue.add(possibleTypes);
+        } else if (unwrappedType instanceof GraphQLUnionType) {
+            GraphQLUnionType unionType = (GraphQLUnionType) unwrappedType;
+            List<GraphQLFieldsContainer> possibleTypes = unionType.getTypes().stream()
+                    .filter(possibleType -> possibleType instanceof GraphQLFieldsContainer)
+                    .map(possibleType -> (GraphQLFieldsContainer) possibleType)
+                    .collect(Collectors.toList());
+
+            containersQueue.add(possibleTypes);
+        } else if (unwrappedType instanceof GraphQLFieldsContainer) {
+            visited.add(fieldCoordinates);
+            containersQueue.add(Collections.singletonList((GraphQLFieldsContainer) unwrappedType));
+        } else {
+            containersQueue.add(Collections.singletonList(EMPTY_OBJECT_TYPE));
+        }
+
+        totalFieldCount.incrementAndGet();
+    }
+
+    private static FieldSelection getFieldSelection(GraphQLFieldDefinition fieldDef, GraphQLType unwrappedType) {
+        boolean typeNeedsClassifier = unwrappedType instanceof GraphQLUnionType || unwrappedType instanceof GraphQLInterfaceType;
+
+        // TODO: This statement is kinda awful
+        final FieldSelection newFieldSelection;
+
+        if (typeNeedsClassifier) {
+            newFieldSelection = new FieldSelection(fieldDef.getName(), new HashMap<>(), true);
+        } else if (unwrappedType instanceof GraphQLFieldsContainer) {
+            newFieldSelection = new FieldSelection(fieldDef.getName(), new HashMap<>(), false);
+        } else {
+            newFieldSelection = new FieldSelection(fieldDef.getName(), null, false);
+        }
+        return newFieldSelection;
+    }
+
     private boolean hasRequiredArgs(GraphQLFieldDefinition fieldDefinition) {
         // TODO: Maybe provide a hook to allow callers to resolve required arguments
         return fieldDefinition.getArguments().stream()
-                .anyMatch(arg -> {
-                    GraphQLInputType argType = arg.getType();
-                    boolean isMandatory = GraphQLTypeUtil.isNonNull(argType);
-                    boolean hasDefaultValue = arg.hasSetDefaultValue();
-
-                    return isMandatory && !hasDefaultValue;
-                });
+                .anyMatch(arg -> GraphQLTypeUtil.isNonNull(arg.getType()) && !arg.hasSetDefaultValue());
     }
 
     public static class FieldSelection {
