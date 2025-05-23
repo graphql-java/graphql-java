65e2c8474e83046021494da449df4c40361c6647
dondonz <13839920+dondonz@users.noreply.github.com>
2025-05-23 17:41:51 +1000

    Add ArchUnit rule to ban javax nullability annotations

---
+++ src/test/groovy/graphql/AnnotationUsageTest.groovy
@@ -0,0 +1,40 @@
+package graphql
+
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.EvaluationResult
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
+import spock.lang.Specification
+
+class AnnotationUsageTest extends Specification {
+
+    private static final JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("graphql")
+
+    def "should only use JSpecify nullability annotations"() {
+        given:
+        ArchRule dependencyRule = ArchRuleDefinition.noClasses()
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage(
+                        "javax.annotation",
+                        "org.jetbrains.annotations"
+                )
+                .because("We are using JSpecify nullability annotations only. Please change to use JSpecify.")
+
+        when:
+        EvaluationResult result = dependencyRule.evaluate(importedClasses)
+
+        then:
+        if (result.hasViolation()) {
+            println "We are using JSpecify nullability annotations only. Please change the following to use JSpecify instead:"
+            result.getFailureReport().getDetails().each { violation ->
+                println "- ${violation}"
+            }
+        }
+        !result.hasViolation()
+    }
+} 
