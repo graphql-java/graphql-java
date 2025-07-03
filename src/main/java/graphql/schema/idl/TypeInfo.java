e6027449fd75fdaf0a0bc800c87a6a2ffe063b48
Marc-Andre Giroux <mgiroux@netflix.com>
2025-07-03 10:29:22 -0400

    add javadoc

--- src/main/java/graphql/schema/idl/TypeInfo.java
+++ src/main/java/graphql/schema/idl/TypeInfo.java
@@ -26,6 +26,13 @@ public class TypeInfo {
         return new TypeInfo(type);
     }
 
+    /**
+     * Gets the type name of a [Type], unwrapping any lists or non-null decorations
+     *
+     * @param type the Type
+     *
+     * @return the inner TypeName for this type
+     */
     public static TypeName getTypeName(Type type) {
         while (!(type instanceof TypeName)) {
             if (type instanceof NonNullType) {
