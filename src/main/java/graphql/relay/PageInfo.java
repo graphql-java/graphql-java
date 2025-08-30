ee765746dca0454dbd6e64888a9e895274b2e7fe
dondonz <13839920+dondonz@users.noreply.github.com>
2025-08-30 15:32:00 +0200

    Annotate PageInfo

--- src/main/java/graphql/relay/PageInfo.java
+++ src/main/java/graphql/relay/PageInfo.java
@@ -1,6 +1,8 @@
 package graphql.relay;
 
 import graphql.PublicApi;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Represents pagination information in Relay about {@link graphql.relay.Edge edges} when used
@@ -9,17 +11,18 @@ import graphql.PublicApi;
  * See <a href="https://facebook.github.io/relay/graphql/connections.htm#sec-undefined.PageInfo">https://facebook.github.io/relay/graphql/connections.htm#sec-undefined.PageInfo</a>
  */
 @PublicApi
+@NullMarked
 public interface PageInfo {
 
     /**
      * @return cursor to the first edge, or null if this page is empty.
      */
-    ConnectionCursor getStartCursor();
+    @Nullable ConnectionCursor getStartCursor();
 
     /**
      * @return cursor to the last edge, or null if this page is empty.
      */
-    ConnectionCursor getEndCursor();
+    @Nullable ConnectionCursor getEndCursor();
 
     /**
      * @return true if and only if this page is not the first page. only meaningful when you gave the {@code last} argument.
