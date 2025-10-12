7598c55a9c2f41bda475350e34d1ffdcf4a74a23
Andreas Marek <andimarek@fastmail.fm>
2025-10-12 20:46:14 +1000

    more performance

--- src/main/java/graphql/schema/DataLoaderWithContext.java
+++ src/main/java/graphql/schema/DataLoaderWithContext.java
@@ -1,6 +1,7 @@
 package graphql.schema;
 
 import graphql.Internal;
+import graphql.execution.Async;
 import graphql.execution.incremental.AlternativeCallContext;
 import graphql.execution.instrumentation.dataloader.ExhaustedDataLoaderDispatchStrategy;
 import graphql.execution.instrumentation.dataloader.PerLevelDataLoaderDispatchStrategy;
@@ -10,7 +11,13 @@ import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+
+import static graphql.Assert.assertNotNull;
 
 @Internal
 @NullMarked
@@ -29,9 +36,54 @@ public class DataLoaderWithContext<K, V> extends DelegatingDataLoader<K, V> {
         // calling super.load() is important, because otherwise the data loader will sometimes called
         // later than the dispatch, which results in a hanging DL
         CompletableFuture<V> result = super.load(key, keyContext);
+        newDataLoaderInvocation();
+        return result;
+    }
+
+
+    @Override
+    public CompletableFuture<List<V>> loadMany(List<K> keys, List<Object> keyContexts) {
+        assertNotNull(keys);
+        assertNotNull(keyContexts);
+
+        CompletableFuture<List<V>> result;
+        synchronized (this) {
+            List<CompletableFuture<V>> collect = new ArrayList<>(keys.size());
+            for (int i = 0; i < keys.size(); i++) {
+                K key = keys.get(i);
+                Object keyContext = null;
+                if (i < keyContexts.size()) {
+                    keyContext = keyContexts.get(i);
+                }
+                collect.add(delegate.load(key, keyContext));
+            }
+            result = Async.allOf(collect);
+        }
+        newDataLoaderInvocation();
+        return result;
+    }
+
+    @Override
+    public CompletableFuture<Map<K, V>> loadMany(Map<K, ?> keysAndContexts) {
+        assertNotNull(keysAndContexts);
+
+        CompletableFuture<Map<K, V>> result;
+        synchronized (this) {
+            Map<K, CompletableFuture<V>> collect = new HashMap<>(keysAndContexts.size());
+            for (Map.Entry<K, ?> entry : keysAndContexts.entrySet()) {
+                K key = entry.getKey();
+                Object keyContext = entry.getValue();
+                collect.put(key, delegate.load(key, keyContext));
+            }
+            result = Async.allOf(collect);
+        }
+        newDataLoaderInvocation();
+        return result;
+    }
+
+    private void newDataLoaderInvocation() {
         DataFetchingEnvironmentImpl dfeImpl = (DataFetchingEnvironmentImpl) dfe;
         DataFetchingEnvironmentImpl.DFEInternalState dfeInternalState = (DataFetchingEnvironmentImpl.DFEInternalState) dfeImpl.toInternal();
-        dfeInternalState.getProfiler().dataLoaderUsed(dataLoaderName);
         if (dfeInternalState.getDataLoaderDispatchStrategy() instanceof PerLevelDataLoaderDispatchStrategy) {
             AlternativeCallContext alternativeCallContext = dfeInternalState.getDeferredCallContext();
             int level = dfeImpl.getLevel();
@@ -40,6 +92,9 @@ public class DataLoaderWithContext<K, V> extends DelegatingDataLoader<K, V> {
             AlternativeCallContext alternativeCallContext = dfeInternalState.getDeferredCallContext();
             ((ExhaustedDataLoaderDispatchStrategy) dfeInternalState.dataLoaderDispatchStrategy).newDataLoaderInvocation(alternativeCallContext);
         }
-        return result;
     }
+
+
+
+
 }
