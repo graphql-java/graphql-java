a87a8a30aa6d9b1ad11ac2a8e5b0148e5890232e
dondonz <13839920+dondonz@users.noreply.github.com>
2026-02-15 15:49:54 +1100

    Not nullable when Node is created

--- src/main/java/graphql/language/Description.java
+++ src/main/java/graphql/language/Description.java
@@ -9,17 +9,17 @@ import java.io.Serializable;
 @PublicApi
 @NullMarked
 public class Description implements Serializable {
-    public final @Nullable String content;
+    public final String content;
     public final @Nullable SourceLocation sourceLocation;
     public final boolean multiLine;
 
-    public Description(@Nullable String content, @Nullable SourceLocation sourceLocation, boolean multiLine) {
+    public Description(String content, @Nullable SourceLocation sourceLocation, boolean multiLine) {
         this.content = content;
         this.sourceLocation = sourceLocation;
         this.multiLine = multiLine;
     }
 
-    public @Nullable String getContent() {
+    public String getContent() {
         return content;
     }
 
